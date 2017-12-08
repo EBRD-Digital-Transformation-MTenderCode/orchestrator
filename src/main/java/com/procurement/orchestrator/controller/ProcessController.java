ff3d06615b1645c9fc9eac9051055a8a9a693591
andreykolchev <andreykolchev@gmail.com>
2017-12-08 20:11:39 +0200

    add test endpoint

--- src/main/java/com/procurement/orchestrator/controller/ProcessController.java
+++ src/main/java/com/procurement/orchestrator/controller/ProcessController.java
@@ -22,6 +22,9 @@ public class ProcessController {
 
     private final JsonUtil jsonUtil;
 
+    int count=0;
+
+
     public ProcessController(final ProcessService processService,
                              final OperationService operationService,
                              final JsonUtil jsonUtil) {
@@ -35,6 +38,44 @@ public class ProcessController {
                                                @RequestParam final String processType,
                                                @RequestParam final String platformId,
                                                @RequestBody final String jsonData) {
+        /**check/save operation data*/
+        operationService.processFirstOperationStep(transactionId, platformId, processType, jsonData);
+        /**start new process for current operation*/
+        processService.startProcess(processType, transactionId);
+        return new ResponseEntity<>(ResponseMessageType.OK.value(), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/testProcess", method = RequestMethod.POST)
+    public ResponseEntity<String> testProcess() {
+        count=count+1;
+        String transactionId = Integer.toString(count);
+        String platformId = "front";
+        String processType = "ein";
+        String jsonData = "{\"tender\":{\"id\":\"string\",\"title\":\"string\",\"description\":\"string\"," +
+            "\"classification\":{\"id\":\"string\",\"scheme\":\"CPV\",\"description\":\"string\"}}," +
+            "\"planning\":{\"budget\":{\"id\":\"string\"," +
+            "\"period\":{\"startDate\":\"2017-12-05T16:04:25.000000107Z\"," +
+            "\"endDate\":\"2017-12-05T16:04:25.000000107Z\"},\"amount\":{\"amount\":1.9,\"currency\":\"AED\"}}," +
+            "\"rationale\":\"string\"},\"relatedProcesses\":[{\"id\":\"string\",\"relationship\":[\"framework\"]," +
+            "\"title\":\"string\",\"scheme\":\"ocid\",\"identifier\":\"string\",\"uri\":\"string\"}]," +
+            "\"parties\":[{\"id\":\"string\",\"name\":\"string\",\"identifier\":{\"id\":\"string\"," +
+            "\"scheme\":\"string\",\"legalName\":\"string\",\"uri\":\"string\"}," +
+            "\"additionalIdentifiers\":[{\"id\":\"string\",\"scheme\":\"string\",\"legalName\":\"string\"," +
+            "\"uri\":\"string\"}],\"address\":{\"streetAddress\":\"string\",\"locality\":\"string\"," +
+            "\"region\":\"string\",\"postalCode\":\"string\",\"countryName\":\"string\"}," +
+            "\"contactPoint\":{\"name\":\"string\",\"email\":\"string\",\"telephone\":\"string\"," +
+            "\"faxNumber\":\"string\",\"url\":\"string\"},\"roles\":[\"buyer\"]," +
+            "\"details\":{\"typeOfBuyer\":\"BODY_PUBLIC\",\"mainGeneralActivity\":\"DEFENCE\"," +
+            "\"mainSectoralActivity\":\"AIRPORT_RELATED_ACTIVITIES\",\"isACentralPurchasingBody\":true," +
+            "\"NUTSCode\":\"string\",\"scale\":\"micro\"},\"buyerProfile\":\"string\"}],\"language\":\"string\"," +
+            "\"buyer\":{\"id\":\"string\",\"name\":\"string\",\"identifier\":{\"id\":\"string\"," +
+            "\"scheme\":\"string\",\"legalName\":\"string\",\"uri\":\"string\"}," +
+            "\"address\":{\"streetAddress\":\"string\",\"locality\":\"string\",\"region\":\"string\"," +
+            "\"postalCode\":\"string\",\"countryName\":\"string\"},\"additionalIdentifiers\":[{\"id\":\"string\"," +
+            "\"scheme\":\"string\",\"legalName\":\"string\",\"uri\":\"string\"}]," +
+            "\"contactPoint\":{\"name\":\"string\",\"email\":\"string\",\"telephone\":\"string\"," +
+            "\"faxNumber\":\"string\",\"url\":\"string\"}}}";
+
         /**check/save operation data*/
         operationService.processFirstOperationStep(transactionId, platformId, processType, jsonData);
         /**start new process for current operation*/
