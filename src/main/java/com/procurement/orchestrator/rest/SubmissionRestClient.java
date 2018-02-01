c9a137969486c21f8195ee0ac449fde5edfb1c78
andreykolchev <andreykolchev@gmail.com>
2018-02-01 18:03:11 +0200

    add new processes

--- src/main/java/com/procurement/orchestrator/rest/SubmissionRestClient.java
+++ src/main/java/com/procurement/orchestrator/rest/SubmissionRestClient.java
@@ -19,7 +19,7 @@ public interface SubmissionRestClient {
                                             @RequestParam("endDate") String endDate) throws Exception;
 
     @RequestMapping(path = "/period/save", method = RequestMethod.POST)
-    ResponseEntity<ResponseDto> savePeriod(@RequestParam("cpid") String cpid,
+    ResponseEntity<ResponseDto> savePeriod(@RequestParam("cpId") String cpid,
                                            @RequestParam("stage") String stage,
                                            @RequestParam("startDate") String startDate,
                                            @RequestParam("endDate") String endDate) throws Exception;
