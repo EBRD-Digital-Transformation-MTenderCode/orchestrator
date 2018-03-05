45dbdaec386f45a4453a8f19d71743b6a35dca56
andreykolchev <andreykolchev@gmail.com>
2018-03-05 17:56:50 +0200

    date format

--- src/main/java/com/procurement/orchestrator/utils/DateUtil.java
+++ src/main/java/com/procurement/orchestrator/utils/DateUtil.java
@@ -8,14 +8,24 @@ import java.time.format.DateTimeFormatterBuilder;
 import java.util.Date;
 import org.springframework.stereotype.Component;
 
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+
 @Component
 public class DateUtil {
 
     private final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
             .parseCaseInsensitive()
-            .append(DateTimeFormatter.ISO_LOCAL_DATE)
+            .append(ISO_LOCAL_DATE)
             .appendLiteral('T')
-            .append(DateTimeFormatter.ISO_LOCAL_TIME)
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendLiteral(':')
+            .appendValue(SECOND_OF_MINUTE, 2)
             .appendLiteral('Z')
             .toFormatter();
 
