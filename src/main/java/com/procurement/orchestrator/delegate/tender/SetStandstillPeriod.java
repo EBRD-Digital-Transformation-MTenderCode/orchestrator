ef68f5107baa75058689d6b18b070303a68f4672
andreykolchev <andreykolchev@gmail.com>
2018-03-07 18:04:51 +0200

    chron

--- src/main/java/com/procurement/orchestrator/delegate/tender/SetStandstillPeriod.java
+++ src/main/java/com/procurement/orchestrator/delegate/tender/SetStandstillPeriod.java
@@ -1,4 +1,4 @@
-package com.procurement.orchestrator.delegate.tender.access;
+package com.procurement.orchestrator.delegate.tender;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.cassandra.model.OperationStepEntity;
