1fb4c025ec9b0610e258c758aaf13d9f49222609
Oleh Herashchenko <joghua@gmail.com>
2018-04-05 19:05:25 +0300

    Create PN

--- src/main/java/com/procurement/orchestrator/rest/AccessRestClient.java
+++ src/main/java/com/procurement/orchestrator/rest/AccessRestClient.java
@@ -28,6 +28,14 @@ public interface AccessRestClient {
                                          @RequestParam("date") String dateTime,
                                          @RequestBody JsonNode jsonData) throws Exception;
 
+    @RequestMapping(path = "/pn", method = RequestMethod.POST)
+    ResponseEntity<ResponseDto> createPn(@RequestParam("stage") String stage,
+                                          @RequestParam("country") String country,
+                                          @RequestParam("pmd") String pmd,
+                                          @RequestParam("owner") String owner,
+                                          @RequestParam("date") String dateTime,
+                                          @RequestBody JsonNode jsonData) throws Exception;
+
     @RequestMapping(path = "/cn", method = RequestMethod.PUT)
     ResponseEntity<ResponseDto> updateCn(@RequestParam("identifier") String cpId,
                                          @RequestParam("token") String token,
