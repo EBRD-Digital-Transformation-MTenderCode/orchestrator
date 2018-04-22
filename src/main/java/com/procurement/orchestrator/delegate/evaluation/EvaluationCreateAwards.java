43d12f50f1a9c98b5ffecf58da67c951183efbf8
andreykolchev <andreykolchev@gmail.com>
2018-04-22 10:38:58 +0300

    refactor

---
+++ src/main/java/com/procurement/orchestrator/delegate/evaluation/EvaluationCreateAwards.java
@@ -0,0 +1,71 @@
+package com.procurement.orchestrator.delegate.evaluation;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.procurement.orchestrator.domain.Params;
+import com.procurement.orchestrator.domain.entity.OperationStepEntity;
+import com.procurement.orchestrator.rest.EvaluationRestClient;
+import com.procurement.orchestrator.rest.QualificationRestClient;
+import com.procurement.orchestrator.service.OperationService;
+import com.procurement.orchestrator.service.ProcessService;
+import com.procurement.orchestrator.utils.DateUtil;
+import com.procurement.orchestrator.utils.JsonUtil;
+import java.util.Objects;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EvaluationCreateAwards implements JavaDelegate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EvaluationCreateAwards.class);
+
+    private final EvaluationRestClient evaluationRestClient;
+    private final OperationService operationService;
+    private final ProcessService processService;
+    private final JsonUtil jsonUtil;
+
+    public EvaluationCreateAwards(final EvaluationRestClient evaluationRestClient,
+                                  final OperationService operationService,
+                                  final ProcessService processService,
+                                  final JsonUtil jsonUtil) {
+        this.evaluationRestClient = evaluationRestClient;
+        this.operationService = operationService;
+        this.processService = processService;
+        this.jsonUtil = jsonUtil;
+    }
+
+    @Override
+    public void execute(final DelegateExecution execution) throws Exception {
+        LOG.info(execution.getCurrentActivityName());
+        final String processId = execution.getProcessInstanceId();
+        final OperationStepEntity entity = operationService.getPreviousOperationStep(execution);
+        final Params params = jsonUtil.toObject(Params.class, entity.getJsonParams());
+        final String taskId = execution.getCurrentActivityId();
+        final JsonNode requestData = jsonUtil.toJsonNode(entity.getResponseData());
+        final JsonNode responseData = processService.processResponse(
+                evaluationRestClient.createAwards(
+                        params.getCpid(),
+                        params.getNewStage(),
+                        params.getOwner(),
+                        params.getCountry(),
+                        params.getPmd(),
+                        params.getStartDate(),
+                        requestData),
+                params,
+                processId,
+                taskId,
+                requestData);
+        if (Objects.nonNull(responseData)) {
+            operationService.saveOperationStep(
+                    execution,
+                    entity,
+                    processService.addAwardAccessToParams(params, responseData, processId),
+                    requestData,
+                    processService.addAwardData(requestData, responseData, processId));
+        }
+    }
+}
+
+
---
+++ src/main/java/com/procurement/orchestrator/rest/EvaluationRestClient.java
@@ -0,0 +1,45 @@
+package com.procurement.orchestrator.rest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.procurement.orchestrator.domain.response.ResponseDto;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "e-evaluation")
+public interface EvaluationRestClient {
+
+    @RequestMapping(path = "/evaluation", method = RequestMethod.POST)
+    ResponseEntity<ResponseDto> createAwards(@RequestParam("identifier") String cpId,
+                                             @RequestParam("stage") String stage,
+                                             @RequestParam("owner") String owner,
+                                             @RequestParam("country") String country,
+                                             @RequestParam("pmd") String pmd,
+                                             @RequestParam("startDate") String startDate,
+                                             @RequestBody JsonNode dataDto) throws Exception;
+
+    @RequestMapping(path = "/evaluation", method = RequestMethod.PUT)
+    ResponseEntity<ResponseDto> updateAward(@RequestParam("identifier") String cpId,
+                                            @RequestParam("stage") String stage,
+                                            @RequestParam("token") String token,
+                                            @RequestParam("owner") String owner,
+                                            @RequestBody JsonNode dataDto) throws Exception;
+
+    @RequestMapping(path = "/evaluation/period", method = RequestMethod.PUT)
+    ResponseEntity<ResponseDto> checkAwarded(@RequestParam("identifier") String cpId,
+                                             @RequestParam("stage") String stage,
+                                             @RequestParam("country") String country,
+                                             @RequestParam("pmd") String pmd,
+                                             @RequestParam("endPeriod") String endPeriod) throws Exception;
+
+    @RequestMapping(path = "/evaluation", method = RequestMethod.GET)
+    ResponseEntity<ResponseDto> getAwards(@RequestParam("identifier") String cpId,
+                                          @RequestParam("stage") String stage,
+                                          @RequestParam("country") String country,
+                                          @RequestParam("pmd") String pmd) throws Exception;
+
+
+}
