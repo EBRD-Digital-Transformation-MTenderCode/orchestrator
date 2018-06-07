c0d193f9d1d99cdbe6765b5f0e1154af688013d7
andreykolchev <andreykolchev@gmail.com>
2018-06-07 15:13:09 +0300

    rename endpoints from camelCase

--- src/main/java/com/procurement/orchestrator/config/kafka/KafkaProducerProperties.java
+++ src/main/java/com/procurement/orchestrator/config/kafka/KafkaProducerProperties.java
@@ -9,5 +9,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "kafka.producer")
 public class KafkaProducerProperties {
     private String internalBootstrap;
-    private String externalBootstrap;
 }
