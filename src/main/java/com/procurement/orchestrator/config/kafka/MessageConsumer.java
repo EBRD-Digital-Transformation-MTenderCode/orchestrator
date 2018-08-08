eed841144fb84941977c092f3a11f7428034a0c5
andreykolchev <andreykolchev@gmail.com>
2018-08-08 11:57:39 +0300

    rules

--- src/main/java/com/procurement/orchestrator/config/kafka/MessageConsumer.java
+++ src/main/java/com/procurement/orchestrator/config/kafka/MessageConsumer.java
@@ -44,6 +44,9 @@ public class MessageConsumer {
 
     @KafkaListener(topics = "chronograph-out")
     public void onReceiving(final String message,
+                            @Header(KafkaHeaders.OFFSET) final Integer offset,
+                            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) final int partition,
+                            @Header(KafkaHeaders.RECEIVED_TOPIC) final String topic,
                             @Header(KafkaHeaders.ACKNOWLEDGMENT) final Acknowledgment acknowledgment) {
 
         acknowledgment.acknowledge();
