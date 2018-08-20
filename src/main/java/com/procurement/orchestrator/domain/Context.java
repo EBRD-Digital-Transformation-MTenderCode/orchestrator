48d1ccd859ecdb49dd39d96963b34defc7fffdb5
andreykolchev <andreykolchev@gmail.com>
2018-08-20 16:32:43 +0300

    kafka response

--- src/main/java/com/procurement/orchestrator/domain/Context.java
+++ src/main/java/com/procurement/orchestrator/domain/Context.java
@@ -2,7 +2,6 @@ package com.procurement.orchestrator.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
