ceab2a87e35e889fe83bb102dd6c5a0686991ce3
andreykolchev <andreykolchev@gmail.com>
2018-08-27 00:01:06 +0300

    tender cancellation

--- src/main/java/com/procurement/orchestrator/controller/CancelController.java
+++ src/main/java/com/procurement/orchestrator/controller/CancelController.java
@@ -1,15 +1,17 @@
 package com.procurement.orchestrator.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
 import com.procurement.orchestrator.service.ProcessService;
 import com.procurement.orchestrator.service.RequestService;
 import com.procurement.orchestrator.utils.JsonUtil;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/cancel")
@@ -38,6 +40,20 @@ public class CancelController {
         final Context context = requestService.getContextForUpdate(authorization, operationId.toString(), cpid, ocid, token, "bidWithdrawn");
         context.setId(id.toString());
         requestService.saveRequestAndCheckOperation(context, jsonUtil.empty());
+        final Map<String, Object> variables = new HashMap<>();
+        processService.startProcess(context, variables);
+        return new ResponseEntity<>("ok", HttpStatus.ACCEPTED);
+    }
+
+    @RequestMapping(value = "/cn/{cpid}/{ocid}", method = RequestMethod.POST)
+    public ResponseEntity<String> tenderCancellation(@RequestHeader("Authorization") final String authorization,
+                                                     @RequestHeader("X-OPERATION-ID") final UUID operationId,
+                                                     @RequestHeader("X-TOKEN") final String token,
+                                                     @PathVariable("cpid") final String cpid,
+                                                     @PathVariable("ocid") final String ocid,
+                                                     @RequestBody final JsonNode data) {
+        final Context context = requestService.getContextForUpdate(authorization, operationId.toString(), cpid, ocid, token, "tenderCancellation");
+        requestService.saveRequestAndCheckOperation(context, data);
         final Map<String, Object> variables = new HashMap<>();
         processService.startProcess(context, variables);
         return new ResponseEntity<>("ok", HttpStatus.ACCEPTED);
