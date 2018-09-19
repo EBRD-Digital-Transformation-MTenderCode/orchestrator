264ad1f6af1535ab8086b41a66366a5f2dea46d2
andreykolchev <andreykolchev@gmail.com>
2018-09-19 14:09:34 +0300

    commands

---
+++ src/main/java/com/procurement/orchestrator/delegate/chronograph/ChronographScheduleEndEnquiryPeriod.java
@@ -0,0 +1,62 @@
+package com.procurement.orchestrator.delegate.chronograph;
+
+import com.datastax.driver.core.utils.UUIDs;
+import com.procurement.orchestrator.config.kafka.MessageProducer;
+import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.chronograph.ActionType;
+import com.procurement.orchestrator.domain.chronograph.ScheduleTask;
+import com.procurement.orchestrator.domain.entity.OperationStepEntity;
+import com.procurement.orchestrator.service.OperationService;
+import com.procurement.orchestrator.utils.DateUtil;
+import com.procurement.orchestrator.utils.JsonUtil;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChronographScheduleEndEnquiryPeriod implements JavaDelegate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChronographScheduleEndEnquiryPeriod.class);
+
+    private final MessageProducer messageProducer;
+    private final OperationService operationService;
+    private final JsonUtil jsonUtil;
+    private final DateUtil dateUtil;
+
+    public ChronographScheduleEndEnquiryPeriod(final MessageProducer messageProducer,
+                                               final OperationService operationService,
+                                               final JsonUtil jsonUtil,
+                                               final DateUtil dateUtil) {
+        this.messageProducer = messageProducer;
+        this.operationService = operationService;
+        this.jsonUtil = jsonUtil;
+        this.dateUtil = dateUtil;
+    }
+
+    @Override
+    public void execute(final DelegateExecution execution) {
+        LOG.info(execution.getCurrentActivityName());
+        final OperationStepEntity entity = operationService.getPreviousOperationStep(execution);
+        final Context context = jsonUtil.toObject(Context.class, entity.getContext());
+        /**set context for next process*/
+        final Context contextChronograph = new Context();
+        final String uuid = UUIDs.timeBased().toString();
+        contextChronograph.setCpid(context.getCpid());
+        contextChronograph.setProcessType("tenderPeriodEnd");
+        contextChronograph.setOperationId(uuid);
+        contextChronograph.setRequestId(uuid);
+        contextChronograph.setSetExtendedPeriod(null);
+
+        final ScheduleTask task = new ScheduleTask(
+                ActionType.SCHEDULE,
+                context.getCpid(),
+                "TENDERING",
+                dateUtil.stringToLocal(context.getEndDate()), /*launchTime*/
+                null, /*newLaunchTime*/
+                jsonUtil.toJson(contextChronograph));
+        messageProducer.sendToChronograph(task);
+        operationService.saveOperationStep(execution, entity, jsonUtil.toJsonNode(task));
+    }
+}
