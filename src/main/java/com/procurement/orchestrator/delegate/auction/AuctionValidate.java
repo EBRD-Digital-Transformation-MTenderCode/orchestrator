a2b1294e4a146e661651186275a20214bd1946b3
andreykolchev <andreykolchev@gmail.com>
2018-10-14 21:19:33 +0300

    errors

--- src/main/java/com/procurement/orchestrator/delegate/auction/AuctionValidate.java
+++ src/main/java/com/procurement/orchestrator/delegate/auction/AuctionValidate.java
@@ -1,41 +1,35 @@
 package com.procurement.orchestrator.delegate.auction;
 
-import com.procurement.orchestrator.rest.AuctionRestClient;
-import com.procurement.orchestrator.service.OperationService;
-import com.procurement.orchestrator.service.ProcessService;
-import com.procurement.orchestrator.utils.JsonUtil;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuctionValidate implements JavaDelegate {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AuctionValidate.class);
-
-    private final AuctionRestClient auctionRestClient;
-
-    private final OperationService operationService;
-
-    private final ProcessService processService;
-
-    private final JsonUtil jsonUtil;
-
-    public AuctionValidate(final AuctionRestClient auctionRestClient,
-                           final OperationService operationService,
-                           final ProcessService processService,
-                           final JsonUtil jsonUtil) {
-        this.auctionRestClient = auctionRestClient;
-        this.operationService = operationService;
-        this.processService = processService;
-        this.jsonUtil = jsonUtil;
-    }
+//    private static final Logger LOG = LoggerFactory.getLogger(AuctionValidate.class);
+//
+//    private final AuctionRestClient auctionRestClient;
+//
+//    private final OperationService operationService;
+//
+//    private final ProcessService processService;
+//
+//    private final JsonUtil jsonUtil;
+//
+//    public AuctionValidate(final AuctionRestClient auctionRestClient,
+//                           final OperationService operationService,
+//                           final ProcessService processService,
+//                           final JsonUtil jsonUtil) {
+//        this.auctionRestClient = auctionRestClient;
+//        this.operationService = operationService;
+//        this.processService = processService;
+//        this.jsonUtil = jsonUtil;
+//    }
 
     @Override
     public void execute(final DelegateExecution execution) throws Exception {
-        LOG.info(execution.getCurrentActivityName());
+//        LOG.info(execution.getCurrentActivityName());
 //        final OperationStepEntity entity = operationService.getPreviousOperationStep(execution);
 //        final JsonNode prevData = jsonUtil.toJsonNode(entity.getResponseData());
 //        final Context context = jsonUtil.toObject(Context.class, entity.getContext());
