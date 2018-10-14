7d9b101a296aa27ec651fbc3b7a179b40ed78a19
andreykolchev <andreykolchev@gmail.com>
2018-10-14 21:20:02 +0300

    errors

--- src/main/java/com/procurement/orchestrator/rest/AccessRestClient.java
+++ src/main/java/com/procurement/orchestrator/rest/AccessRestClient.java
@@ -1,17 +1,9 @@
 package com.procurement.orchestrator.rest;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.procurement.orchestrator.domain.dto.ResponseDto;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@FeignClient(name = "e-access")
+//@FeignClient(name = "e-access")
 public interface AccessRestClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/command")
-    ResponseEntity<ResponseDto> execute(@RequestBody JsonNode commandMessage) throws Exception;
+//    @RequestMapping(method = RequestMethod.POST, value = "/command")
+//    ResponseEntity<ResponseDto> execute(@RequestBody JsonNode commandMessage) throws Exception;
 
 }
