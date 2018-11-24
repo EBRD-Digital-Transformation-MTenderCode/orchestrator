ce16ca16fa32fbc365f727186b3480ebe1a8b2e0
andreykolchev <andreykolchev@gmail.com>
2018-11-24 18:15:59 +0200

    doc generator command message

--- src/main/java/com/procurement/orchestrator/controller/ContractController.java
+++ src/main/java/com/procurement/orchestrator/controller/ContractController.java
@@ -32,23 +32,6 @@ public class ContractController extends DoBaseController {
     }
 
     @RequestMapping(value = "/contract/{cpid}/{ocid}", method = RequestMethod.POST)
-    public ResponseEntity<String> updateAC(@RequestHeader("Authorization") final String authorization,
-                                           @RequestHeader("X-OPERATION-ID") final String operationId,
-                                           @RequestHeader("X-TOKEN") final String token,
-                                           @PathVariable("cpid") final String cpid,
-                                           @PathVariable("ocid") final String ocid,
-                                           @RequestBody final JsonNode data) {
-        requestService.validate(operationId, data);
-        final Context context = requestService.getContextForContractUpdate(authorization, operationId,
-                                                                           cpid, ocid, token, "updateAC");
-        requestService.saveRequestAndCheckOperation(context, data);
-        final Map<String, Object> variables = new HashMap<>();
-        variables.put("operationType", context.getOperationType());
-        processService.startProcess(context, variables);
-        return new ResponseEntity<>("ok", HttpStatus.ACCEPTED);
-    }
-
-    @RequestMapping(value = "/issue/contract/{cpid}/{ocid}", method = RequestMethod.POST)
     public ResponseEntity<String> issuingAC(@RequestHeader("Authorization") final String authorization,
                                            @RequestHeader("X-OPERATION-ID") final String operationId,
                                            @RequestHeader("X-TOKEN") final String token,
