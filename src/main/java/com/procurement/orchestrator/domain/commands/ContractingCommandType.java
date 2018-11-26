80aba10385e51d97d95a8cb63968c6d8e80564ec
andreykolchev <andreykolchev@gmail.com>
2018-11-26 20:28:19 +0200

    AC Signing by Supplier

--- src/main/java/com/procurement/orchestrator/domain/commands/ContractingCommandType.java
+++ src/main/java/com/procurement/orchestrator/domain/commands/ContractingCommandType.java
@@ -18,7 +18,9 @@ public enum ContractingCommandType {
     SET_ISSUED_STATUS_DETAILS("setIssuedStatusDetails"),
     FINAL_UPDATE("finalUpdateAC"),
     GET_RELATED_BID_ID("getRelatedBidId"),
-    PROCEED_SUPPLIER_RESPONSE("proceedSupplierResponse");
+    PROCEED_SUPPLIER_RESPONSE("proceedSupplierResponse"),
+    CONTRACT_VERIFICATION("contractVerification"),
+    PROCEED_TREASURY_RESPONSE("proceedTreasuryResponse");
 
     private static final Map<String, ContractingCommandType> CONSTANTS = new HashMap<>();
     private final String value;
