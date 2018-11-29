42dc964d31f5fed1134a9097b11f5cc7be00fec7
andreykolchev <andreykolchev@gmail.com>
2018-11-29 18:23:17 +0200

    ocid validation

--- src/main/java/com/procurement/orchestrator/delegate/notification/SendMessageToAuction.java
+++ src/main/java/com/procurement/orchestrator/delegate/notification/SendMessageToAuction.java
@@ -55,7 +55,8 @@ public class SendMessageToAuction implements JavaDelegate {
         final Context context = jsonUtil.toObject(Context.class, entity.getContext());
         final String processId = execution.getProcessInstanceId();
         final JsonNode rqData = processService.getAuctionLaunchData(jsonData, processId);
-        final CommandMessage commandMessage = notificationService.getCommandMessage(LAUNCH, context, rqData);
+        final JsonNode testRqData = getTestAuctionLaunchData(rqData);
+        final CommandMessage commandMessage = notificationService.getCommandMessage(LAUNCH, context, testRqData);
         messageProducer.sendToAuction(commandMessage);
         operationService.saveOperationStep(
                 execution,
