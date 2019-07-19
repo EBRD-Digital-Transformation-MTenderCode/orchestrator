3130504e9d290de1b8bd3702721949e61f1a75ce
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-19 14:13:38 +0300

    ES-1617 Added new methods to MDM REST Client.

--- src/main/java/com/procurement/orchestrator/rest/MdmRestClient.java
+++ src/main/java/com/procurement/orchestrator/rest/MdmRestClient.java
@@ -2,11 +2,10 @@ package com.procurement.orchestrator.rest;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.dto.command.ResponseDto;
+import feign.Param;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "e-mdm")
 public interface MdmRestClient {
@@ -14,4 +13,24 @@ public interface MdmRestClient {
     @RequestMapping(method = RequestMethod.POST, value = "/command")
     ResponseEntity<ResponseDto> execute(@RequestBody JsonNode commandMessage) throws Exception;
 
+    @GetMapping(path = "/addresses/countries/{countryId}", params = "lang")
+    ResponseEntity<String> getCountry(
+        @PathVariable("countryId") String countryId,
+        @Param("lang") String lang
+    ) throws Exception;
+
+    @GetMapping(path = "/addresses/countries/{countryId}/regions/{regionId}", params = "lang")
+    ResponseEntity<String> getRegion(
+        @PathVariable("countryId") String countryId,
+        @PathVariable("regionId") String regionId,
+        @Param("lang") String lang
+    ) throws Exception;
+
+    @GetMapping(path = "/addresses/countries/{countryId}/regions/{regionId}/localities/{localityId}", params = "lang")
+    ResponseEntity<String> getLocality(
+        @PathVariable("countryId") String countryId,
+        @PathVariable("regionId") String regionId,
+        @PathVariable("localityId") String localityId,
+        @Param("lang") String lang
+    ) throws Exception;
 }
