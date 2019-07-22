cc3172f7fdcc31a20c5ebb1ffee40995e0432e69
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-22 15:43:57 +0300

    Changed annotations for access methods of the MDM client.

--- src/main/java/com/procurement/orchestrator/rest/MdmRestClient.java
+++ src/main/java/com/procurement/orchestrator/rest/MdmRestClient.java
@@ -5,7 +5,10 @@ import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import feign.Param;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "e-mdm")
 public interface MdmRestClient {
@@ -13,20 +16,20 @@ public interface MdmRestClient {
     @RequestMapping(method = RequestMethod.POST, value = "/command")
     ResponseEntity<ResponseDto> execute(@RequestBody JsonNode commandMessage) throws Exception;
 
-    @GetMapping(path = "/addresses/countries/{countryId}", params = "lang")
+    @RequestMapping(method = RequestMethod.GET, path = "/addresses/countries/{countryId}", params = "lang")
     ResponseEntity<String> getCountry(
         @PathVariable("countryId") String countryId,
         @Param("lang") String lang
     ) throws Exception;
 
-    @GetMapping(path = "/addresses/countries/{countryId}/regions/{regionId}", params = "lang")
+    @RequestMapping(method = RequestMethod.GET, path = "/addresses/countries/{countryId}/regions/{regionId}", params = "lang")
     ResponseEntity<String> getRegion(
         @PathVariable("countryId") String countryId,
         @PathVariable("regionId") String regionId,
         @Param("lang") String lang
     ) throws Exception;
 
-    @GetMapping(path = "/addresses/countries/{countryId}/regions/{regionId}/localities/{localityId}", params = "lang")
+    @RequestMapping(method = RequestMethod.GET, path = "/addresses/countries/{countryId}/regions/{regionId}/localities/{localityId}", params = "lang")
     ResponseEntity<String> getLocality(
         @PathVariable("countryId") String countryId,
         @PathVariable("regionId") String regionId,
