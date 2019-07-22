aad224ba7f285330273eeac32236a0122d556bfa
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-22 13:16:59 +0300

    Added logging to exception handler.

--- src/main/java/com/procurement/orchestrator/controller/ControllerExceptionHandler.java
+++ src/main/java/com/procurement/orchestrator/controller/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import com.procurement.orchestrator.domain.ExternalException;
 import com.procurement.orchestrator.domain.PlatformError;
 import com.procurement.orchestrator.exception.EnumException;
 import com.procurement.orchestrator.exception.OperationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,30 +17,39 @@ import java.util.Set;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
     @ResponseBody
     @ExceptionHandler(OperationException.class)
     public ResponseEntity<ExternalException> handleOperationException(final OperationException e) {
-        return new ResponseEntity<>(getError(e.getMessage()), HttpStatus.BAD_REQUEST);
+        final String errorMessage = e.getMessage();
+        if(LOG.isWarnEnabled())
+            LOG.warn(errorMessage, e);
+        return new ResponseEntity<>(getError(errorMessage), HttpStatus.BAD_REQUEST);
     }
 
     @ResponseBody
     @ExceptionHandler(EnumException.class)
     public ResponseEntity<ExternalException> handleEnumException(final EnumException e) {
-        return new ResponseEntity<>(getError(e.getMessage()), HttpStatus.BAD_REQUEST);
+        final String errorMessage = e.getMessage();
+        if(LOG.isWarnEnabled())
+            LOG.warn(errorMessage, e);
+        return new ResponseEntity<>(getError(errorMessage), HttpStatus.BAD_REQUEST);
     }
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExternalException> handleException(final Exception e) {
-        return new ResponseEntity<>(getError(e.getMessage()), HttpStatus.BAD_REQUEST);
+        final String errorMessage = e.getMessage();
+        if(LOG.isWarnEnabled())
+            LOG.warn(errorMessage, e);
+        return new ResponseEntity<>(getError(errorMessage), HttpStatus.BAD_REQUEST);
     }
 
     private ExternalException getError(final String msg) {
         final Set<PlatformError> errors = new HashSet<>();
         errors.add(new PlatformError("400.00.00.00", msg));
-        final ExternalException externalException = new ExternalException(errors);
-        return externalException;
+        return new ExternalException(errors);
     }
 
 }
