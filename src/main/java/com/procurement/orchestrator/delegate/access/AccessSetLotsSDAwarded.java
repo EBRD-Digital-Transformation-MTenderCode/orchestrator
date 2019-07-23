dac73c09a63da0001ae5a5052df9acdadd97eb13
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-23 13:41:45 +0300

    Changed mechanism call eAccess service(added: DTO, logging)

--- src/main/java/com/procurement/orchestrator/delegate/access/AccessSetLotsSDAwarded.java
+++ src/main/java/com/procurement/orchestrator/delegate/access/AccessSetLotsSDAwarded.java
@@ -2,15 +2,19 @@ package com.procurement.orchestrator.delegate.access;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.AccessRestClient;
 import com.procurement.orchestrator.service.OperationService;
 import com.procurement.orchestrator.service.ProcessService;
 import com.procurement.orchestrator.utils.JsonUtil;
+import lombok.Getter;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.value.BooleanValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -48,19 +52,38 @@ public class AccessSetLotsSDAwarded implements JavaDelegate {
         final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityName();
-        final JsonNode commandMessage = processService.getCommandMessage(SET_LOTS_SD_AWARDED, context, jsonData);
-        JsonNode responseData = processService.processResponse(
-                accessRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
+
+        final String lotId = context.getId();
+        final Boolean lotAwarded = execution.<BooleanValue>getVariableTyped("lotAwarded").getValue();
+        final SetLotStatusRequest request = new SetLotStatusRequest(lotId, lotAwarded);
+        final JsonNode requestData = jsonUtil.toJsonNode(request);
+
+        final String operationId = context.getOperationId();
+        final JsonNode commandMessage = processService.getCommandMessage(SET_LOTS_SD_AWARDED, context, requestData);
+        LOG.debug("COMMAND TO SERVICE ({}): '{}'.", operationId, jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = accessRestClient.execute(commandMessage);
+        LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", operationId, jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", operationId, jsonUtil.toJsonOrEmpty(responseData));
+
         if (Objects.nonNull(responseData)) {
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    commandMessage,
-                    processService.addUpdatedLot(jsonData, responseData, processId));
+            final JsonNode step = processService.addUpdatedLot(jsonData, responseData, processId);
+            LOG.debug("STEP FOR SAVE ({}): '{}'.", operationId, jsonUtil.toJsonOrEmpty(step));
+
+            operationService.saveOperationStep(execution, entity, commandMessage, step);
+        }
+    }
+
+    @Getter
+    private static class SetLotStatusRequest {
+        private final String lotId;
+        private final boolean lotAwarded;
+
+        SetLotStatusRequest(String lotId, boolean lotAwarded) {
+            this.lotId = lotId;
+            this.lotAwarded = lotAwarded;
         }
     }
 }
