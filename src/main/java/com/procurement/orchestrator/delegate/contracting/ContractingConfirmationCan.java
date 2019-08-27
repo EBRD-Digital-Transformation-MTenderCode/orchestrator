174bac784f3ff0650ef79391782715db2e3e6626
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-08-27 14:42:11 +0300

    ES-1815 Added logging to 'ContractingConfirmationCan' delegate.

--- src/main/java/com/procurement/orchestrator/delegate/contracting/ContractingConfirmationCan.java
+++ src/main/java/com/procurement/orchestrator/delegate/contracting/ContractingConfirmationCan.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.contracting;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.ContractingRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,9 +12,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 import static com.procurement.orchestrator.domain.commands.ContractingCommandType.CONFIRMATION_CAN;
 
@@ -27,10 +27,12 @@ public class ContractingConfirmationCan implements JavaDelegate {
     private final ProcessService processService;
     private final JsonUtil jsonUtil;
 
-    public ContractingConfirmationCan(final ContractingRestClient contractingRestClient,
-                                      final OperationService operationService,
-                                      final ProcessService processService,
-                                      final JsonUtil jsonUtil) {
+    public ContractingConfirmationCan(
+        final ContractingRestClient contractingRestClient,
+        final OperationService operationService,
+        final ProcessService processService,
+        final JsonUtil jsonUtil
+    ) {
         this.contractingRestClient = contractingRestClient;
         this.operationService = operationService;
         this.processService = processService;
@@ -45,20 +47,21 @@ public class ContractingConfirmationCan implements JavaDelegate {
         final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
+
         final JsonNode commandMessage = processService.getCommandMessage(CONFIRMATION_CAN, context, jsonData);
-        JsonNode responseData = processService.processResponse(
-                contractingRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
-        if (Objects.nonNull(responseData)) {
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    context,
-                    commandMessage,
-                    responseData);
+        if (LOG.isDebugEnabled())
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = contractingRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+        if (responseData != null) {
+            operationService.saveOperationStep(execution, entity, context, commandMessage, responseData);
         }
     }
 }
