83758f3bd80b4eded6ff4e8f3d3801a4732c7533
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-08-28 11:51:37 +0300

    ES-1617 Changed mechanism creating MDMException.

--- src/main/java/com/procurement/orchestrator/exception/MDMException.java
+++ src/main/java/com/procurement/orchestrator/exception/MDMException.java
@@ -1,13 +1,39 @@
 package com.procurement.orchestrator.exception;
 
+import feign.Request;
+import feign.Response;
 import lombok.Getter;
+import org.apache.commons.io.IOUtils;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Getter
 public class MDMException extends RuntimeException {
     private final String responseBody;
 
-    public MDMException(final String responseBody, Exception exception) {
-        super(exception);
+    public static MDMException create(final Response response) {
+        final Request request = response.request();
+        final String requestMethod = request.method().toUpperCase();
+        final String requestUrl = response.request().url();
+        final String requestBody = new String(response.request().body());
+
+        final int responseStatus = response.status();
+        final String responseBody = getResponseBody(response.body());
+
+        final String message = "Request[" + requestMethod + " " + requestUrl + ", body: '" + requestBody + "'], Response[Status: '" + responseStatus + "', body: '" + responseBody + "']";
+        return new MDMException(responseBody, message);
+    }
+
+    private static String getResponseBody(final Response.Body body) {
+        try {
+            return IOUtils.toString(body.asInputStream(), UTF_8);
+        } catch (Exception exception) {
+            return "Error getting body of response.";
+        }
+    }
+
+    public MDMException(final String responseBody, final String message) {
+        super(message);
         this.responseBody = responseBody;
     }
 }
