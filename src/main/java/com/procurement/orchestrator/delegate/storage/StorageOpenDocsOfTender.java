8fedc74c4da1bb2ea738ae996a925ae63f9acdec
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-08-29 16:17:33 +0300

    ES-1788 Added logging to 'StorageOpenDocsOfTender' delegate.

--- src/main/java/com/procurement/orchestrator/delegate/storage/StorageOpenDocsOfTender.java
+++ src/main/java/com/procurement/orchestrator/delegate/storage/StorageOpenDocsOfTender.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.storage;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.StorageRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,9 +12,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 import static com.procurement.orchestrator.domain.commands.StorageCommandType.PUBLISH;
 
@@ -30,10 +30,12 @@ public class StorageOpenDocsOfTender implements JavaDelegate {
 
     private final JsonUtil jsonUtil;
 
-    public StorageOpenDocsOfTender(final StorageRestClient storageRestClient,
-                                   final OperationService operationService,
-                                   final ProcessService processService,
-                                   final JsonUtil jsonUtil) {
+    public StorageOpenDocsOfTender(
+        final StorageRestClient storageRestClient,
+        final OperationService operationService,
+        final ProcessService processService,
+        final JsonUtil jsonUtil
+    ) {
         this.storageRestClient = storageRestClient;
         this.operationService = operationService;
         this.processService = processService;
@@ -48,22 +50,27 @@ public class StorageOpenDocsOfTender implements JavaDelegate {
         final Context context = jsonUtil.toObject(Context.class, entity.getContext());
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
-        final JsonNode documents = processService.getDocumentsOfTender(jsonData, processId);
-        if (Objects.nonNull(documents)) {
-            final JsonNode commandMessage = processService.getCommandMessage(PUBLISH, context, documents);
-            JsonNode responseData = processService.processResponse(
-                    storageRestClient.execute(commandMessage),
-                    context,
-                    processId,
-                    taskId,
-                    commandMessage);
 
-            if (Objects.nonNull(responseData)) {
-                operationService.saveOperationStep(
-                        execution,
-                        entity,
-                        commandMessage,
-                        processService.setDocumentsOfTender(jsonData, responseData, processId));
+        final JsonNode documents = processService.getDocumentsOfTender(jsonData, processId);
+        if (documents != null) {
+            final JsonNode commandMessage = processService.getCommandMessage(PUBLISH, context, documents);
+            if (LOG.isDebugEnabled())
+                LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+            final ResponseEntity<ResponseDto> response = storageRestClient.execute(commandMessage);
+            if (LOG.isDebugEnabled())
+                LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+            final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+            if (LOG.isDebugEnabled())
+                LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+            if (responseData != null) {
+                final JsonNode step = processService.setDocumentsOfTender(jsonData, responseData, processId);
+                if (LOG.isDebugEnabled())
+                    LOG.debug("STEP FOR SAVE ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(step));
+
+                operationService.saveOperationStep(execution, entity, commandMessage, step);
             }
         }
     }
