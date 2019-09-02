d42d8e3e456f8cad19bfa0355475cf659655e0ca
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-09-02 16:48:55 +0300

    ES-1847 Added logging to 'EvaluationAwardsCancellation' delegate.

--- src/main/java/com/procurement/orchestrator/delegate/evaluation/EvaluationAwardsCancellation.java
+++ src/main/java/com/procurement/orchestrator/delegate/evaluation/EvaluationAwardsCancellation.java
@@ -1,10 +1,12 @@
 package com.procurement.orchestrator.delegate.evaluation;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.Outcome;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.EvaluationRestClient;
-import com.procurement.orchestrator.service.NotificationService;
 import com.procurement.orchestrator.service.OperationService;
 import com.procurement.orchestrator.service.ProcessService;
 import com.procurement.orchestrator.utils.JsonUtil;
@@ -12,9 +14,11 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.procurement.orchestrator.domain.commands.EvaluationCommandType.AWARDS_CANCELLATION;
 
@@ -24,18 +28,17 @@ public class EvaluationAwardsCancellation implements JavaDelegate {
     private static final Logger LOG = LoggerFactory.getLogger(EvaluationAwardsCancellation.class);
 
     private final EvaluationRestClient evaluationRestClient;
-    private final NotificationService notificationService;
     private final OperationService operationService;
     private final ProcessService processService;
     private final JsonUtil jsonUtil;
 
-    public EvaluationAwardsCancellation(final EvaluationRestClient evaluationRestClient,
-                                        final NotificationService notificationService,
-                                        final OperationService operationService,
-                                        final ProcessService processService,
-                                        final JsonUtil jsonUtil) {
+    public EvaluationAwardsCancellation(
+        final EvaluationRestClient evaluationRestClient,
+        final OperationService operationService,
+        final ProcessService processService,
+        final JsonUtil jsonUtil
+    ) {
         this.evaluationRestClient = evaluationRestClient;
-        this.notificationService = notificationService;
         this.operationService = operationService;
         this.processService = processService;
         this.jsonUtil = jsonUtil;
@@ -50,22 +53,56 @@ public class EvaluationAwardsCancellation implements JavaDelegate {
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
         final JsonNode lots = processService.getLots(jsonData, processId);
+
         final JsonNode commandMessage = processService.getCommandMessage(AWARDS_CANCELLATION, context, lots);
-        final JsonNode responseData = processService.processResponse(
-                evaluationRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
-        if (Objects.nonNull(responseData)) {
+        LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = evaluationRestClient.execute(commandMessage);
+        LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+        if (responseData != null) {
             context.setPhase("empty");
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    notificationService.addAwardOutcomeToContext(context, responseData, processId),
-                    commandMessage,
-                    processService.addAwards(jsonData, responseData, processId));
+
+            final Context modifiedContext = addAwardOutcomeToContext(context, responseData);
+            if (LOG.isDebugEnabled())
+                LOG.debug("CONTEXT FOR SAVE (" + context.getOperationId() + "): '" + jsonUtil.toJsonOrEmpty(modifiedContext) + "'.");
+
+
+            final JsonNode step = processService.addAwards(jsonData, responseData, processId);
+            LOG.debug("STEP FOR SAVE ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(step));
+
+            operationService.saveOperationStep(execution, entity, modifiedContext, commandMessage, step);
         }
     }
-}
 
+    private Context addAwardOutcomeToContext(final Context context, final JsonNode responseData) {
+        final Set<Outcome> outcomes;
+        if (context.getOutcomes() != null) {
+            outcomes = context.getOutcomes();
+        } else {
+            outcomes = new HashSet<>();
+        }
+
+        final ArrayNode awardsNode = (ArrayNode) responseData.get("awards");
+        for (final JsonNode awardNode : awardsNode) {
+            if (awardNode.has("token")) {
+                final String token = awardNode.get("token").asText();
+                final String status = awardNode.get("status").asText();
+                final String id = awardNode.get("id").asText();
+
+                if ("unsuccessful".equals(status)) {
+                    outcomes.add(new Outcome(id, null, "awards"));
+                } else {
+                    outcomes.add(new Outcome(id, token, "awards"));
+                }
+            }
+
+        }
+
+        context.setOutcomes(outcomes);
+        return context;
+    }
+}
