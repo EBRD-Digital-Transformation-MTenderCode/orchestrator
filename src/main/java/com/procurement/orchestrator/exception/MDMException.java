df18e17a1d7845864561a2f325e71b1a85719163
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-09-10 13:54:27 +0300

    ES-1855 Changed processing body of response from MDM when error.

--- src/main/java/com/procurement/orchestrator/exception/MDMException.java
+++ src/main/java/com/procurement/orchestrator/exception/MDMException.java
@@ -14,19 +14,18 @@ public class MDMException extends RuntimeException {
     public static MDMException create(final Response response) {
         final Request request = response.request();
         final String requestMethod = request.method().toUpperCase();
-        final String requestUrl = response.request().url();
-        final byte[] body = response.request().body();
-        final String requestBody;
-        if (body == null)
-            requestBody = "None";
-        else
-            requestBody = new String(body);
-
+        final String requestUrl = request.url();
+        final String requestBody = getRequestBody(request);
         final int responseStatus = response.status();
         final String responseBody = getResponseBody(response.body());
 
         final String message = "Request[" + requestMethod + " " + requestUrl + ", body: '" + requestBody + "'], Response[Status: '" + responseStatus + "', body: '" + responseBody + "']";
         return new MDMException(responseBody, message);
+    }
+
+    private static String getRequestBody(final Request request) {
+        final byte[] body = request.body();
+        return (request.body() == null || body.length == 0) ? "None" : new String(body);
     }
 
     private static String getResponseBody(final Response.Body body) {
