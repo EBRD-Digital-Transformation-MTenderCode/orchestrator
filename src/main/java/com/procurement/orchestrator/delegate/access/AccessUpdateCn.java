46445ec15402bc07c49270cad761930708ed1a04
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-02 17:37:29 +0200

    ES-2091 Added logging to 'AccessUpdateCn' delegate.

--- src/main/java/com/procurement/orchestrator/delegate/access/AccessUpdateCn.java
+++ src/main/java/com/procurement/orchestrator/delegate/access/AccessUpdateCn.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.access;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.AccessRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,9 +12,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 import static com.procurement.orchestrator.domain.commands.AccessCommandType.UPDATE_CN;
 
@@ -21,16 +21,19 @@ import static com.procurement.orchestrator.domain.commands.AccessCommandType.UPD
 public class AccessUpdateCn implements JavaDelegate {
 
     private static final Logger LOG = LoggerFactory.getLogger(AccessUpdateCn.class);
+    private static final String VARIABLE_NAME = "isLotsChanged";
 
     private final AccessRestClient accessRestClient;
     private final OperationService operationService;
     private final ProcessService processService;
     private final JsonUtil jsonUtil;
 
-    public AccessUpdateCn(final AccessRestClient accessRestClient,
-                          final OperationService operationService,
-                          final ProcessService processService,
-                          final JsonUtil jsonUtil) {
+    public AccessUpdateCn(
+        final AccessRestClient accessRestClient,
+        final OperationService operationService,
+        final ProcessService processService,
+        final JsonUtil jsonUtil
+    ) {
         this.accessRestClient = accessRestClient;
         this.operationService = operationService;
         this.processService = processService;
@@ -46,22 +49,28 @@ public class AccessUpdateCn implements JavaDelegate {
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
         final JsonNode requestData = processService.getAccessData(jsonData, processId);
+
         final JsonNode commandMessage = processService.getCommandMessage(UPDATE_CN, context, requestData);
-        execution.setVariable("isLotsChanged", false);
-        JsonNode responseData = processService.processResponse(
-                accessRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
-        if (Objects.nonNull(responseData)) {
-            execution.setVariable("isLotsChanged", processService.getBoolean("isLotsChanged", responseData, processId));
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    context,
-                    commandMessage,
-                    responseData);
+        if (LOG.isDebugEnabled())
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = accessRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        execution.setVariable(VARIABLE_NAME, false);
+        LOG.debug("COMMAND ({}) IN CONTEXT PUT THE VARIABLE '{}' WITH THE VALUE '{}'.", context.getOperationId(), VARIABLE_NAME, false);
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+        if (responseData != null) {
+            final Boolean isLotsChanged = processService.getBoolean(VARIABLE_NAME, responseData, processId);
+            execution.setVariable(VARIABLE_NAME, isLotsChanged);
+            LOG.debug("COMMAND ({}) IN CONTEXT PUT THE VARIABLE '{}' WITH THE VALUE '{}'.", context.getOperationId(), VARIABLE_NAME, isLotsChanged);
+
+            operationService.saveOperationStep(execution, entity, context, commandMessage, responseData);
         }
     }
 }
