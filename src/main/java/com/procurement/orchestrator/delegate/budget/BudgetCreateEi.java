7e8d82a6f0b850442057d8be7f5625a28a982908
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-06 10:49:08 +0200

    ES-2147 Added logging to 'BudgetCreateEi' delegate.

--- src/main/java/com/procurement/orchestrator/delegate/budget/BudgetCreateEi.java
+++ src/main/java/com/procurement/orchestrator/delegate/budget/BudgetCreateEi.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.budget;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.BudgetRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,9 +12,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 import static com.procurement.orchestrator.domain.commands.BudgetCommandType.CREATE_EI;
 
@@ -27,10 +27,12 @@ public class BudgetCreateEi implements JavaDelegate {
     private final ProcessService processService;
     private final JsonUtil jsonUtil;
 
-    public BudgetCreateEi(final BudgetRestClient budgetRestClient,
-                          final OperationService operationService,
-                          final ProcessService processService,
-                          final JsonUtil jsonUtil) {
+    public BudgetCreateEi(
+        final BudgetRestClient budgetRestClient,
+        final OperationService operationService,
+        final ProcessService processService,
+        final JsonUtil jsonUtil
+    ) {
         this.budgetRestClient = budgetRestClient;
         this.operationService = operationService;
         this.processService = processService;
@@ -45,20 +47,25 @@ public class BudgetCreateEi implements JavaDelegate {
         final JsonNode requestData = jsonUtil.toJsonNode(entity.getResponseData());
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
+
         final JsonNode commandMessage = processService.getCommandMessage(CREATE_EI, context, requestData);
-        JsonNode responseData = processService.processResponse(
-                budgetRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
-        if (Objects.nonNull(responseData)) {
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    addDataToContext(context, responseData, processId),
-                    commandMessage,
-                    responseData);
+        if (LOG.isDebugEnabled())
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = budgetRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+        if (responseData != null) {
+            final Context updatedContext = addDataToContext(context, responseData, processId);
+            if (LOG.isDebugEnabled())
+                LOG.debug("COMMAND ({}) UPDATED CONTEXT (added ocid and token to outcome): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(updatedContext));
+
+            operationService.saveOperationStep(execution, entity, updatedContext, commandMessage, responseData);
         }
     }
 
