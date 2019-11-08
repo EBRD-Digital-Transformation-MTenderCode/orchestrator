0bd39d71f4eafa6ebdcb1b9049f5cdab7e984b07
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-08 16:06:29 +0200

    ES-2179 Added logging to 'ClarificationCheckPeriod' delegate.

--- src/main/java/com/procurement/orchestrator/delegate/clarification/ClarificationCheckPeriod.java
+++ src/main/java/com/procurement/orchestrator/delegate/clarification/ClarificationCheckPeriod.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.clarification;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.ClarificationRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,9 +12,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 import static com.procurement.orchestrator.domain.commands.ClarificationCommandType.CHECK_PERIOD;
 
@@ -21,19 +21,20 @@ import static com.procurement.orchestrator.domain.commands.ClarificationCommandT
 public class ClarificationCheckPeriod implements JavaDelegate {
 
     private static final Logger LOG = LoggerFactory.getLogger(ClarificationCheckPeriod.class);
+    private static final String ATTRIBUTE_NAME = "isEnquiryPeriodChanged";
+    private static final String VARIABLE_NAME = "isEnquiryPeriodChanged";
 
     private final ClarificationRestClient clarificationRestClient;
-
     private final OperationService operationService;
-
     private final ProcessService processService;
-
     private final JsonUtil jsonUtil;
 
-    public ClarificationCheckPeriod(final ClarificationRestClient clarificationRestClient,
-                                    final OperationService operationService,
-                                    final ProcessService processService,
-                                    final JsonUtil jsonUtil) {
+    public ClarificationCheckPeriod(
+        final ClarificationRestClient clarificationRestClient,
+        final OperationService operationService,
+        final ProcessService processService,
+        final JsonUtil jsonUtil
+    ) {
         this.clarificationRestClient = clarificationRestClient;
         this.operationService = operationService;
         this.processService = processService;
@@ -49,20 +50,30 @@ public class ClarificationCheckPeriod implements JavaDelegate {
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
         final JsonNode enquiryPeriod = processService.getEnquiryPeriod(jsonData, processId);
+
         final JsonNode commandMessage = processService.getCommandMessage(CHECK_PERIOD, context, enquiryPeriod);
-        JsonNode responseData = processService.processResponse(
-                clarificationRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
-        if (Objects.nonNull(responseData)) {
-            execution.setVariable("isEnquiryPeriodChanged", processService.getBoolean("isEnquiryPeriodChanged", responseData, processId));
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    commandMessage,
-                    processService.setCheckEnquiryPeriod(jsonData, responseData, processId));
+        if (LOG.isDebugEnabled())
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = clarificationRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+        if (responseData != null) {
+            final Boolean isEnquiryPeriodChanged = processService.getBoolean(ATTRIBUTE_NAME, responseData, processId);
+            execution.setVariable(VARIABLE_NAME, isEnquiryPeriodChanged);
+            if (LOG.isDebugEnabled())
+                LOG.debug("COMMAND ({}) IN CONTEXT PUT THE VARIABLE '{}' WITH THE VALUE '{}'.", context.getOperationId(), VARIABLE_NAME, isEnquiryPeriodChanged);
+
+            final JsonNode step = processService.setCheckEnquiryPeriod(jsonData, responseData, processId);
+            if (LOG.isDebugEnabled())
+                LOG.debug("STEP FOR SAVE ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(step));
+
+            operationService.saveOperationStep(execution, entity, commandMessage, step);
         }
     }
 }
