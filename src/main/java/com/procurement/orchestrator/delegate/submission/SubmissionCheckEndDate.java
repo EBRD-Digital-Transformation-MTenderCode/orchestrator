d548f464edc8509cbca3f628511a31db74a425d8
Evgen Kopach <zkopach@gmail.com>
2019-11-15 15:55:11 +0200

    ES-2169 Added logging for delegate 'SubmissionCheckEndDate '

--- src/main/java/com/procurement/orchestrator/delegate/submission/SubmissionCheckEndDate.java
+++ src/main/java/com/procurement/orchestrator/delegate/submission/SubmissionCheckEndDate.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.submission;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.SubmissionRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,6 +12,7 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -47,21 +49,28 @@ public class SubmissionCheckEndDate implements JavaDelegate {
         final Context context = jsonUtil.toObject(Context.class, entity.getContext());
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
+
         final JsonNode commandMessage = processService.getCommandMessage(CHECK_PERIOD_END_DATE, context, jsonUtil.empty());
-        JsonNode responseData = processService.processResponse(
-                submissionRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = submissionRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
         if (Objects.nonNull(responseData)) {
             final Boolean isTenderPeriodExpired = processService.getBoolean("isTenderPeriodExpired", responseData, processId);
             execution.setVariable("isTenderPeriodExpired", isTenderPeriodExpired);
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    commandMessage,
-                    processService.addTenderTenderPeriod(jsonUtil.empty(), responseData, processId));
+
+            final JsonNode step = processService.addTenderTenderPeriod(jsonUtil.empty(), responseData, processId);
+            if (LOG.isDebugEnabled())
+                LOG.debug("STEP FOR SAVE ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(step));
+
+            operationService.saveOperationStep(execution, entity, commandMessage, step);
         }
     }
 }
