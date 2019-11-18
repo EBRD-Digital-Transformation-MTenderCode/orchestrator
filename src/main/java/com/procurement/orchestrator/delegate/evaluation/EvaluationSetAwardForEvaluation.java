4dda367b4a0285036c4b9de613a16c9d33d40cc3
Evgen Kopach <zkopach@gmail.com>
2019-11-18 12:16:57 +0200

    ES-2169 Create delegate 'EvaluationSetAwardForEvaluation'

---
+++ src/main/java/com/procurement/orchestrator/delegate/evaluation/EvaluationSetAwardForEvaluation.java
@@ -0,0 +1,89 @@
+package com.procurement.orchestrator.delegate.evaluation;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
+import com.procurement.orchestrator.domain.entity.OperationStepEntity;
+import com.procurement.orchestrator.rest.EvaluationRestClient;
+import com.procurement.orchestrator.service.OperationService;
+import com.procurement.orchestrator.service.ProcessService;
+import com.procurement.orchestrator.utils.JsonUtil;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import static com.procurement.orchestrator.domain.commands.EvaluationCommandType.SET_AWARD_FOR_EVALUATION;
+
+@Component
+public class EvaluationSetAwardForEvaluation implements JavaDelegate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EvaluationSetAwardForEvaluation.class);
+
+    private final EvaluationRestClient evaluationRestClient;
+    private final OperationService operationService;
+    private final ProcessService processService;
+    private final JsonUtil jsonUtil;
+
+    public EvaluationSetAwardForEvaluation(
+        final EvaluationRestClient evaluationRestClient,
+        final OperationService operationService,
+        final ProcessService processService,
+        final JsonUtil jsonUtil
+    ) {
+        this.evaluationRestClient = evaluationRestClient;
+        this.operationService = operationService;
+        this.processService = processService;
+        this.jsonUtil = jsonUtil;
+    }
+
+    @Override
+    public void execute(final DelegateExecution execution) throws Exception {
+        LOG.info(execution.getCurrentActivityId());
+        final OperationStepEntity entity = operationService.getPreviousOperationStep(execution);
+        final Context context = jsonUtil.toObject(Context.class, entity.getContext());
+        final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
+        final String taskId = execution.getCurrentActivityId();
+        final String processId = execution.getProcessInstanceId();
+
+        final JsonNode commandMessage = processService.getCommandMessage(SET_AWARD_FOR_EVALUATION, context, jsonData);
+        if (LOG.isDebugEnabled())
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = evaluationRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+        if (responseData != null) {
+            final JsonNode step = addAwards(context.getOperationId(), jsonData, responseData, processId);
+            if (LOG.isDebugEnabled())
+                LOG.debug("STEP FOR SAVE ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(step));
+
+            operationService.saveOperationStep(execution, entity, commandMessage, step);
+        }
+    }
+
+    private JsonNode addAwards(
+        final String operationId,
+        final JsonNode jsonData,
+        final JsonNode responseData,
+        final String processId
+    ) {
+        try {
+            final ObjectNode mainNode = (ObjectNode) jsonData;
+            mainNode.set("awards", responseData.get("awards"));
+            return jsonData;
+        } catch (Exception e) {
+            LOG.error("COMMAND (" + operationId + "): Could not add awards.", e);
+            processService.terminateProcess(processId, e.getMessage());
+            return null;
+        }
+    }
+}
