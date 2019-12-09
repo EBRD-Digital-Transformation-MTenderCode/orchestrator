6129a59c4d48e3879a9653ec6fbd7e410ee5b594
Evgen Kopach <zkopach@gmail.com>
2019-12-09 12:50:11 +0200

    ES-2314 Changed process name with only 'evaluateAward' (removed 'awardByBid')

--- src/main/java/com/procurement/orchestrator/controller/TenderController.java
+++ src/main/java/com/procurement/orchestrator/controller/TenderController.java
@@ -213,12 +213,7 @@ public class TenderController extends DoBaseController {
                                              @PathVariable("id") final String id,
                                              @RequestBody final JsonNode data) {
         requestService.validate(operationId, data);
-        final Context prevContext = requestService.getContext(cpid);
-        final String process;
-        if("NP".equals(prevContext.getStage()))
-            process = "evaluateAward";
-        else
-            process = "awardByBid";
+        final String process = "evaluateAward";
 
         final Context context = requestService.getContextForUpdate(authorization, operationId, cpid, ocid, token, process);
         context.setId(id);
