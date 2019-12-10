047d8355b1e459689e5d3cf5c6eac18ca73319b4
Evgen Kopach <zkopach@gmail.com>
2019-12-10 19:11:06 +0200

    ES-2374 Put lots to 'data' instead of 'data.tender'

--- src/main/java/com/procurement/orchestrator/delegate/access/AccessGetLotsAuction.java
+++ src/main/java/com/procurement/orchestrator/delegate/access/AccessGetLotsAuction.java
@@ -1,7 +1,10 @@
 package com.procurement.orchestrator.delegate.access;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.AccessRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,6 +14,7 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -45,21 +49,42 @@ public class AccessGetLotsAuction implements JavaDelegate {
         final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
+
         final JsonNode commandMessage = processService.getCommandMessage(GET_LOTS_AUCTION, context, jsonUtil.empty());
-        JsonNode responseData = processService.processResponse(
-                accessRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
+        LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = accessRestClient.execute(commandMessage);
+        LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
         if (Objects.nonNull(responseData)) {
             context.setAwardCriteria(processService.getText("awardCriteria", responseData, processId));
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    context,
-                    commandMessage,
-                    processService.setTender(jsonData, responseData, processId));
+
+            final JsonNode step = setTenderAndLots(jsonData, responseData, processId);
+            LOG.debug("STEP FOR SAVE ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(step));
+
+            operationService.saveOperationStep(execution, entity, context, commandMessage, step);
+        }
+    }
+
+    private JsonNode setTenderAndLots(final JsonNode jsonData, final JsonNode response, final String processId) {
+        try {
+            final ObjectNode tenderNode = (ObjectNode) response.get("tender");
+            final ArrayNode lotsNode = (ArrayNode) tenderNode.get("lots");
+            final ObjectNode mainNode = (ObjectNode) jsonData;
+
+            if (lotsNode.size() != 0) {
+                mainNode.putArray("lots").addAll(lotsNode);
+            }
+            tenderNode.remove("lots");
+            mainNode.set("tender", tenderNode);
+
+            return jsonData;
+        } catch (Exception e) {
+            processService.terminateProcess(processId, e.getMessage());
+            return null;
         }
     }
 }
