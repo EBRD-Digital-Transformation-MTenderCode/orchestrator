9099be749e04ef18ca462bde1cdcdbec5661f495
katherine <riodael@gmail.com>
2019-12-12 11:11:17 +0200

    ES-2384 Added logging, deleted addAwardOutcomeToContext step and award data saving.

--- src/main/java/com/procurement/orchestrator/delegate/evaluation/EvaluationCreateAwardsAuctionEnd.java
+++ src/main/java/com/procurement/orchestrator/delegate/evaluation/EvaluationCreateAwardsAuctionEnd.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.evaluation;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.EvaluationRestClient;
 import com.procurement.orchestrator.service.NotificationService;
@@ -12,6 +13,7 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -49,20 +51,21 @@ public class EvaluationCreateAwardsAuctionEnd implements JavaDelegate {
         final Context context = jsonUtil.toObject(Context.class, entity.getContext());
         final String taskId = execution.getCurrentActivityId();
         final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
+
         final JsonNode commandMessage = processService.getCommandMessage(CREATE_AWARDS_AUCTION_END, context, jsonData);
-        final JsonNode responseData = processService.processResponse(
-                evaluationRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
+        if (LOG.isDebugEnabled())
+        LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = evaluationRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
         if (Objects.nonNull(responseData)) {
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    notificationService.addAwardOutcomeToContext(context, responseData, processId),
-                    commandMessage,
-                    processService.addAwardData(jsonData, responseData, processId));
+            operationService.saveOperationStep(execution, entity, context, commandMessage);
         }
     }
 }
