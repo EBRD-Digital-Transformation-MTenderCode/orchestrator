f8856fa266fc2b9f899880a43e3e933bdfe2900f
Evgen Kopach <zkopach@gmail.com>
2020-01-17 14:54:00 +0200

    ES-2469 Changed http method for Mdm (/organization/schemes/) request. Added getting countries from suppliers

--- src/main/java/com/procurement/orchestrator/delegate/reference/OrganizationSchemeForSupplier.java
+++ src/main/java/com/procurement/orchestrator/delegate/reference/OrganizationSchemeForSupplier.java
@@ -1,18 +1,25 @@
 package com.procurement.orchestrator.delegate.reference;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.procurement.orchestrator.domain.Context;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.MdmRestClient;
 import com.procurement.orchestrator.service.OperationService;
 import com.procurement.orchestrator.utils.JsonUtil;
+import lombok.Getter;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Component
 public class OrganizationSchemeForSupplier implements JavaDelegate {
@@ -37,11 +44,13 @@ public class OrganizationSchemeForSupplier implements JavaDelegate {
         LOG.info(execution.getCurrentActivityId());
 
         final OperationStepEntity entity = operationService.getPreviousOperationStep(execution);
-        final Context context = jsonUtil.toObject(Context.class, entity.getContext());
         final JsonNode updatedData = jsonUtil.toJsonNode(entity.getResponseData());
 
-        final String countryId = context.getCountry();
-        final JsonNode schemes = getOrganizationSchemesData(countryId);
+        final ArrayNode suppliers = (ArrayNode) (updatedData.get("award").get("suppliers"));
+        final List<String> countriesIds = getSuppliersCountries(suppliers);
+        final Countries countries = new Countries(countriesIds);
+
+        final JsonNode schemes = getOrganizationSchemesData(countries);
 
         final ObjectNode mdm;
         if (updatedData.has("mdm"))
@@ -56,11 +65,32 @@ public class OrganizationSchemeForSupplier implements JavaDelegate {
         operationService.saveOperationStep(execution, entity, prevData, updatedData);
     }
 
-    private JsonNode getOrganizationSchemesData(final String countryId) {
-        LOG.debug("Get data of schemes by country: {}.", countryId);
-        ResponseEntity<String> response = mdmRestClient.getOrganizationSchemes(countryId);
+    private JsonNode getOrganizationSchemesData(final Countries countries) {
+        LOG.debug("Get data of schemes by country: {}.", countries);
+        ResponseEntity<String> response = mdmRestClient.getOrganizationSchemes(countries);
         final String schemesData = response.getBody();
-        LOG.debug("Received data of schemes by country: {} - '{}'.", countryId, schemesData);
+        LOG.debug("Received data of schemes by country: {} - '{}'.", countries, schemesData);
         return jsonUtil.toJsonNode(schemesData).get("data").get("elements");
     }
+
+    private List<String> getSuppliersCountries(final ArrayNode suppliers) {
+        return StreamSupport.stream(suppliers.spliterator(), false)
+                .map(supplier -> supplier.get("address")
+                        .get("addressDetails")
+                        .get("country")
+                        .get("id").asText())
+                .collect(Collectors.toList());
+    }
+
+    @Getter
+    public static class Countries{
+        @JsonProperty("countries")
+        private final List<String> countries;
+
+        Countries(final List<String> countries) {
+            Objects.requireNonNull(countries);
+            this.countries = countries;
+        }
+    }
+
 }
