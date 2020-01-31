df9bcfb0fad044f92b55696be596337e854ad53d
Evgen Kopach <zkopach@gmail.com>
2020-01-31 12:37:19 +0200

    ES-2506 Added handler for 'processAcRejection' for sending message to kafka

--- src/main/java/com/procurement/orchestrator/service/NotificationServiceImpl.java
+++ src/main/java/com/procurement/orchestrator/service/NotificationServiceImpl.java
@@ -462,6 +462,11 @@ public class NotificationServiceImpl implements NotificationService {
                 data.setUrl(getTenderUri(context.getCpid(), context.getOcid()));
                 break;
             }
+            case PROCESS_AC_REJECTION: {
+                data.setOcid(context.getCpid());
+                data.setUrl(getTenderUri(context.getCpid(), context.getOcid()));
+                break;
+            }
             case DO_AWARD_CONSIDERATION: {
                 data.setOcid(context.getCpid());
                 data.setUrl(getTenderUri(context.getCpid(), context.getOcid()));
