597e675a25366ab2ab24f4bd9bbd58249f2900c9
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-03-31 12:10:00 +0300

    ES-2630 Changed implementation 'AwardId' type.

--- src/main/kotlin/com/procurement/orchestrator/domain/model/award/AwardId.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/award/AwardId.kt
@@ -1,5 +1,34 @@
 package com.procurement.orchestrator.domain.model.award
 
+import com.fasterxml.jackson.annotation.JsonValue
+import com.procurement.orchestrator.domain.model.UUID_PATTERN
+import com.procurement.orchestrator.domain.model.isUUID
+import java.io.Serializable
 import java.util.*
 
-typealias AwardId = UUID
+class AwardId private constructor(private val value: String) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        return if (this !== other)
+            other is AwardId
+                && this.value == other.value
+        else
+            true
+    }
+
+    override fun hashCode(): Int = value.hashCode()
+
+    @JsonValue
+    override fun toString(): String = value
+
+    companion object {
+        val pattern: String
+            get() = UUID_PATTERN
+
+        fun validation(text: String): Boolean = text.isUUID()
+
+        fun tryCreateOrNull(text: String): AwardId? = if (validation(text)) AwardId(text) else null
+
+        fun generate(): AwardId = AwardId(UUID.randomUUID().toString())
+    }
+}
