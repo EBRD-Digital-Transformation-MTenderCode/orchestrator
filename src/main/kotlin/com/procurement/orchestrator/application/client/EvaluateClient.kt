a0b9be059f7095ca9cbcaa3387ed1073f89695c7
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-04-01 15:52:58 +0300

    ES-2622 Added new method 'checkRelatedTenderer' to 'EvaluateClient'.

--- src/main/kotlin/com/procurement/orchestrator/application/client/EvaluateClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/client/EvaluateClient.kt
@@ -4,11 +4,14 @@ import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
 import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CheckAccessToAwardAction
+import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CheckRelatedTendererAction
 import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.GetAwardStateByIdsAction
 
 interface EvaluateClient {
 
     suspend fun checkAccessToAward(params: CheckAccessToAwardAction.Params): Result<Reply<Unit>, Fail.Incident>
+
+    suspend fun checkRelatedTenderer(params: CheckRelatedTendererAction.Params): Result<Reply<Unit>, Fail.Incident>
 
     suspend fun getAwardStateByIds(params: GetAwardStateByIdsAction.Params): Result<Reply<GetAwardStateByIdsAction.Result>, Fail.Incident>
 }
