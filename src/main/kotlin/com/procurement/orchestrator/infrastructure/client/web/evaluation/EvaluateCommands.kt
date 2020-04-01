c718902c245bffc175cee4f8bcdc635fdbf4de04
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-04-01 09:32:21 +0300

    ES-2618 Added CheckAccessToAward command for Evaluate.

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/evaluation/EvaluateCommands.kt
@@ -0,0 +1,8 @@
+package com.procurement.orchestrator.infrastructure.client.web.evaluation
+
+import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CheckAccessToAwardAction
+
+object EvaluateCommands {
+
+    object CheckAccessToAward : CheckAccessToAwardAction()
+}
