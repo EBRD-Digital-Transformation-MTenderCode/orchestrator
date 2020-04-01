d0720de125eb7cb2d53b7e353b7f820f438fa4a5
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-04-01 09:35:10 +0300

    ES-2618 Implemented 'EvaluateCheckAccessToAward' delegate for 'CheckAccessToAward' command.

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/evaluate/EvaluateCheckAccessToAwardDelegate.kt
@@ -0,0 +1,77 @@
+package com.procurement.orchestrator.infrastructure.bpms.delegate.evaluate
+
+import com.procurement.orchestrator.application.client.EvaluateClient
+import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
+import com.procurement.orchestrator.application.service.Logger
+import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.MaybeFail
+import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.domain.functional.Result.Companion.failure
+import com.procurement.orchestrator.domain.functional.Result.Companion.success
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.tender.Tender
+import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
+import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
+import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
+import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CheckAccessToAwardAction
+import org.springframework.stereotype.Component
+
+@Component
+class EvaluateCheckAccessToAwardDelegate(
+    logger: Logger,
+    private val evaluateClient: EvaluateClient,
+    operationStepRepository: OperationStepRepository,
+    transform: Transform
+) : AbstractExternalDelegate<Unit, Unit>(
+    logger = logger,
+    transform = transform,
+    operationStepRepository = operationStepRepository
+) {
+
+    override fun parameters(parameterContainer: ParameterContainer): Result<Unit, Fail.Incident.Bpmn.Parameter> =
+        success(Unit)
+
+    override suspend fun execute(
+        context: CamundaGlobalContext,
+        parameters: Unit
+    ): Result<Reply<Unit>, Fail.Incident> {
+
+        val processInfo = context.processInfo
+        val cpid: Cpid = processInfo.cpid
+        val ocid: Ocid = processInfo.ocid
+
+        val tender: Tender = context.tender
+            ?: return failure(Fail.Incident.Bpe(description = "The global context does not contain a 'Tender' object."))
+
+        val awards = context.awards
+            .takeIf { it.isNotEmpty() }
+            ?: return failure(Fail.Incident.Bpe(description = "The global context does not contain a 'Awards' object."))
+        if (awards.size != 1)
+            return failure(
+                Fail.Incident.Bpmn.Context.UnConsistency(
+                    name = "awards",
+                    description = "It was expected that the attribute 'awards' would have only one value. In fact, the attribute has ${awards.size} meanings."
+                )
+            )
+
+        val award = awards[0]
+        return evaluateClient.checkAccessToAward(
+            params = CheckAccessToAwardAction.Params(
+                cpid = cpid,
+                ocid = ocid,
+                awardId = award.id,
+                token = tender.token,
+                owner = tender.owner
+            )
+        )
+    }
+
+    override fun updateGlobalContext(
+        context: CamundaGlobalContext,
+        parameters: Unit,
+        data: Unit
+    ): MaybeFail<Fail.Incident.Bpmn> = MaybeFail.none()
+}
