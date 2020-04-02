58b3866443ce59c7cee0f5c6d9f7e1b070597e7b
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-04-02 15:20:02 +0300

    ES-2617 Added interface 'Serializable' to 'RequirementResponseValue' type.

--- src/main/kotlin/com/procurement/orchestrator/domain/model/requirement/RequirementResponseValue.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/requirement/RequirementResponseValue.kt
@@ -1,10 +1,11 @@
 package com.procurement.orchestrator.domain.model.requirement
 
+import java.io.Serializable
 import java.math.BigDecimal
 
-sealed class RequirementResponseValue {
-    data class AsBoolean(val value: Boolean) : RequirementResponseValue()
-    data class AsString(val value: String) : RequirementResponseValue()
-    data class AsNumber(val value: BigDecimal) : RequirementResponseValue()
-    data class AsInteger(val value: Long) : RequirementResponseValue()
+sealed class RequirementResponseValue : Serializable {
+    data class AsBoolean(val value: Boolean) : RequirementResponseValue(), Serializable
+    data class AsString(val value: String) : RequirementResponseValue(), Serializable
+    data class AsNumber(val value: BigDecimal) : RequirementResponseValue(), Serializable
+    data class AsInteger(val value: Long) : RequirementResponseValue(), Serializable
 }
