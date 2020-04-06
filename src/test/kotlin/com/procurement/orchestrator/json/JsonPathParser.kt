a690ee1e76a3a4d1c6d6b3c2bf51481a0b0c81b1
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-04-06 19:04:21 +0300

    ES-2838 Refactoring 'JsonPathParser' (Parsing an array as a root attribute).

--- src/test/kotlin/com/procurement/orchestrator/json/JsonPathParser.kt
+++ src/test/kotlin/com/procurement/orchestrator/json/JsonPathParser.kt
@@ -8,43 +8,45 @@ object JsonPathParser {
     fun parse(json: String): Map<String, String> {
         val nodes = json.toNode()
         val valueByPath = mutableMapOf<String, String>()
-        parsing(nodes, "$", valueByPath)
+        parsing(nodes, "root", "$", valueByPath)
         return valueByPath
     }
 
-    private fun parsing(nodes: JsonNode, prefix: String, valueByPath: MutableMap<String, String>) {
-        for ((name, node) in nodes.fields()) {
-            when {
-                node.isObject -> parsing(node, "$prefix['$name']", valueByPath)
-                node.isArray -> {
-                    if (node.size() > 0) {
-                        val innerNode = node[0]
-                        when (innerNode.nodeType) {
-                            JsonNodeType.ARRAY,
-                            JsonNodeType.OBJECT -> node.forEachIndexed { index, jsonNode ->
-                                parsing(jsonNode, "$prefix['$name'][$index]", valueByPath)
-                            }
+    private fun parsing(node: JsonNode, name: String, prefix: String, valueByPath: MutableMap<String, String>) {
+        when {
+            node.isObject -> {
+                for ((fieldName, currentNode) in node.fields()) {
+                    parsing(currentNode, fieldName, "$prefix['$fieldName']", valueByPath)
+                }
+            }
+            node.isArray -> {
+                if (node.size() > 0) {
+                    val innerNode = node[0]
+                    when (innerNode.nodeType) {
+                        JsonNodeType.ARRAY,
+                        JsonNodeType.OBJECT -> node.forEachIndexed { index, jsonNode ->
+                            parsing(jsonNode, name, "$prefix['$name'][$index]", valueByPath)
+                        }
 
-                            else -> {
-                                for (index in 0 until node.size()) {
-                                    val path = "$prefix['$name'][$index]"
-                                    val value: String = parseLeaf(node[index], path)
-                                    valueByPath[path] = value
-                                }
+                        else -> {
+                            for (index in 0 until node.size()) {
+                                val path = "$prefix['$name'][$index]"
+                                val value: String = parsePrimitive(node[index], path)
+                                valueByPath[path] = value
                             }
                         }
                     }
                 }
-                else -> {
-                    val path = "$prefix['$name']"
-                    val value: String = parseLeaf(node, path)
-                    valueByPath[path] = value
-                }
+            }
+            else -> {
+                val path = "$prefix['$name']"
+                val value: String = parsePrimitive(node, path)
+                valueByPath[path] = value
             }
         }
     }
 
-    private fun parseLeaf(node: JsonNode, path: String): String {
+    private fun parsePrimitive(node: JsonNode, path: String): String {
         return when (node.nodeType) {
             JsonNodeType.BOOLEAN -> if (node.asBoolean()) "true" else "false"
             JsonNodeType.NUMBER -> node.numberValue().toString()
