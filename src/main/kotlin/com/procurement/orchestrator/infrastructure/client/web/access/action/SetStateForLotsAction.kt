3c95daf0138e5cd6df3efb4f2ff56c575e08c898
katherine <kate.shaidurova@gmail.com>
2020-04-09 12:21:23 +0300

    ES-2841 Added SetStateForLotsAction

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/action/SetStateForLotsAction.kt
@@ -0,0 +1,39 @@
+package com.procurement.orchestrator.infrastructure.client.web.access.action
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.application.service.FunctionalAction
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.lot.LotId
+import com.procurement.orchestrator.domain.model.lot.LotStatus
+import com.procurement.orchestrator.domain.model.lot.LotStatusDetails
+import com.procurement.orchestrator.infrastructure.client.web.Target
+import com.procurement.orchestrator.infrastructure.model.Version
+import java.io.Serializable
+
+abstract class SetStateForLotsAction :
+    FunctionalAction<SetStateForLotsAction.Params, SetStateForLotsAction.Result> {
+    override val version: Version = Version.parse("2.0.0")
+    override val name: String = "setStateForLots"
+    override val target: Target<Result> = Target.plural()
+
+    class Params(
+        @param:JsonProperty("cpid") @field:JsonProperty("cpid") val cpid: Cpid,
+        @param:JsonProperty("ocid") @field:JsonProperty("ocid") val ocid: Ocid,
+        @param:JsonProperty("lots") @field:JsonProperty("lots") val lots: List<Lot>
+    ) {
+        data class Lot(
+            @param:JsonProperty("id") @field:JsonProperty("id") val id: LotId.Permanent,
+            @param:JsonProperty("status") @field:JsonProperty("status") val status: LotStatus,
+            @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: LotStatusDetails
+        )
+    }
+
+    class Result(values: List<Lot>) : List<Result.Lot> by values, Serializable {
+        class Lot(
+            @param:JsonProperty("id") @field:JsonProperty("id") val id: LotId.Permanent,
+            @param:JsonProperty("status") @field:JsonProperty("status") val status: LotStatus,
+            @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: LotStatusDetails
+        ) : Serializable
+    }
+}
