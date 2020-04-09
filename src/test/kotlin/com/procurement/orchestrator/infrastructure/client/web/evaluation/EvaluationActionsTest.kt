a6fcd649d599f0ac33e47ca92b9c7c8ab59757aa
Evgen Kopach <zkopach@gmail.com>
2020-04-09 18:16:56 +0300

    ES-2843 Added tests for 'CreateUnsuccessfulAwards' delegate

--- src/test/kotlin/com/procurement/orchestrator/infrastructure/client/web/evaluation/EvaluationActionsTest.kt
+++ src/test/kotlin/com/procurement/orchestrator/infrastructure/client/web/evaluation/EvaluationActionsTest.kt
@@ -3,6 +3,7 @@ package com.procurement.orchestrator.infrastructure.client.web.evaluation
 import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CheckAccessToAwardAction
 import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CheckRelatedTendererAction
 import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CreateRequirementResponseAction
+import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.CreateUnsuccessfulAwardsAction
 import com.procurement.orchestrator.infrastructure.client.web.evaluation.action.GetAwardStateByIdsAction
 import com.procurement.orchestrator.json.testingBindingAndMapping
 import org.junit.jupiter.api.Nested
@@ -89,4 +90,26 @@ class EvaluationActionsTest {
             }
         }
     }
+
+    @Nested
+    inner class CreateCreateUnsuccessfulAwards {
+
+        @Nested
+        inner class Params {
+            @Test
+            fun fully() {
+                testingBindingAndMapping<CreateUnsuccessfulAwardsAction.Params>("json/client/evaluation/create_unsuccessful_awards_params_full.json")
+            }
+        }
+
+        @Nested
+        inner class Result {
+            @Test
+            fun fully() {
+                testingBindingAndMapping<CreateUnsuccessfulAwardsAction.Result>("json/client/evaluation/create_unsuccessful_awards_result.json")
+            }
+        }
+    }
+
 }
+
