43afffefa50496aed42e9ca2a1fba3c8cc183fd7
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-04-15 17:37:41 +0300

    ES-2914 removed 'required2' mapping test from ContractingActionsTest

--- src/test/kotlin/com/procurement/orchestrator/infrastructure/client/web/contracting/action/ContractingActionsTest.kt
+++ src/test/kotlin/com/procurement/orchestrator/infrastructure/client/web/contracting/action/ContractingActionsTest.kt
@@ -20,11 +20,6 @@ class ContractingActionsTest {
             fun required1() {
                 testingBindingAndMapping<FindCANIdsAction.Params>("json/client/contracting/find_can_ids_params_1.json")
             }
-
-           /* @Test
-            fun required2() {
-                testingBindingAndMapping<ValidateRequirementResponseAction.Params>("json/client/dossier/validate_requirement_response_params_required_2.json")
-            }*/
         }
     }
 }
