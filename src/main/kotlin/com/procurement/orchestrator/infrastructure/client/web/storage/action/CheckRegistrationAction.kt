01564ef94bdd78f88621efb1ddab19517ad0ac27
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-04-20 15:00:09 +0300

    ES-3007 removed from command 'storageCheckRegistration' 'datePublished' attribute

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/storage/action/CheckRegistrationAction.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/storage/action/CheckRegistrationAction.kt
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.orchestrator.application.service.ProceduralAction
 import com.procurement.orchestrator.domain.model.document.DocumentId
 import com.procurement.orchestrator.infrastructure.model.Version
-import java.time.LocalDateTime
 
 abstract class CheckRegistrationAction : ProceduralAction<CheckRegistrationAction.Params> {
     override val version: Version = Version.parse("2.0.0")
     override val name: String = "checkRegistration"
 
     class Params(
-        @field:JsonProperty("datePublished") @param:JsonProperty("datePublished") val datePublished: LocalDateTime,
         @field:JsonProperty("documentIds") @param:JsonProperty("documentIds") val ids: List<DocumentId>
     )
 }
