92c813f66681fe14168ed462bf6aa8351862e4ae
Evgen Kopach <zkopach@gmail.com>
2020-04-22 14:28:00 +0300

    ES-3015 Removed data consistency checking. Added branching based on 'location' value

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/access/AccessSetStateForLotsDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/access/AccessSetStateForLotsDelegate.kt
@@ -1,11 +1,14 @@
 package com.procurement.orchestrator.infrastructure.bpms.delegate.access
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 import com.procurement.orchestrator.application.CommandId
 import com.procurement.orchestrator.application.client.AccessClient
 import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
 import com.procurement.orchestrator.application.model.context.extension.tryGetTender
 import com.procurement.orchestrator.application.service.Logger
 import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.EnumElementProvider
 import com.procurement.orchestrator.domain.EnumElementProvider.Companion.keysAsStrings
 import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.MaybeFail
@@ -14,7 +17,6 @@ import com.procurement.orchestrator.domain.functional.Result.Companion.failure
 import com.procurement.orchestrator.domain.model.lot.LotId
 import com.procurement.orchestrator.domain.model.lot.LotStatus
 import com.procurement.orchestrator.domain.model.lot.LotStatusDetails
-import com.procurement.orchestrator.domain.util.extension.toSetBy
 import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
 import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
 import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
@@ -36,7 +38,7 @@ class AccessSetStateForLotsDelegate(
     companion object {
         private const val PARAMETER_NAME_STATUS = "status"
         private const val PARAMETER_NAME_STATUS_DETAILS = "statusDetails"
-        private const val LOT = "lot"
+        private const val PARAMETER_NAME_LOCATION = "location"
     }
 
     override fun parameters(parameterContainer: ParameterContainer): Result<Parameters, Fail.Incident.Bpmn.Parameter> {
@@ -65,7 +67,19 @@ class AccessSetStateForLotsDelegate(
                     )
             }
 
-        return Result.success(Parameters(status = status, statusDetails = statusDetails))
+        val location: Location = parameterContainer.getString(PARAMETER_NAME_STATUS)
+            .orForwardFail { fail -> return fail }
+            .let { location ->
+                Location.orNull(location)
+                    ?: return failure(
+                        Fail.Incident.Bpmn.Parameter.UnknownValue(
+                            name = PARAMETER_NAME_LOCATION,
+                            actualValue = location,
+                            expectedValues = Location.allowedElements.keysAsStrings()
+                        )
+                    )
+            }
+        return Result.success(Parameters(status = status, statusDetails = statusDetails, location = location))
     }
 
     override suspend fun execute(
@@ -78,19 +92,38 @@ class AccessSetStateForLotsDelegate(
         val tender = context.tryGetTender()
             .orForwardFail { fail -> return fail }
 
+        val lots = when (parameters.location) {
+            Location.TENDER           -> tender.lots
+                .map { lot ->
+                    SetStateForLotsAction.Params.Lot(
+                        id = lot.id.toString(),
+                        status = parameters.status,
+                        statusDetails = parameters.statusDetails
+                    )
+                }
+            Location.TENDER_AMENDMENT -> tender.amendments
+                .map { amendment ->
+                    val entityId = amendment.relatedItem
+                        ?: return failure(
+                            Fail.Incident.Bpms.Context.Missing(
+                                name = "relatedItem",
+                                path = "tender.amendments[${amendment.id}].relatedItem"
+                            )
+                        )
+                    SetStateForLotsAction.Params.Lot(
+                        id = entityId,
+                        status = parameters.status,
+                        statusDetails = parameters.statusDetails
+                    )
+                }
+        }
+
         return accessClient.setStateForLots(
             id = commandId,
             params = SetStateForLotsAction.Params(
                 cpid = processInfo.cpid,
                 ocid = processInfo.ocid,
-                lots = tender.lots
-                    .map { lot ->
-                        SetStateForLotsAction.Params.Lot(
-                            id = lot.id as LotId.Permanent,
-                            statusDetails = parameters.statusDetails,
-                            status = parameters.status
-                        )
-                    }
+                lots = lots
             )
         )
     }
@@ -103,35 +136,7 @@ class AccessSetStateForLotsDelegate(
         val tender = context.tryGetTender()
             .orReturnFail { return MaybeFail.fail(it) }
 
-        val duplicates = data.groupingBy { it.id }
-            .eachCount()
-            .filter { it.value > 1 }
-
-        if (duplicates.isNotEmpty())
-            return MaybeFail.fail(
-                Fail.Incident.Response.Validation.DuplicateEntity(
-                    id = duplicates.keys.joinToString(), name = LOT
-                )
-            )
-
         val receivedLotByIds: Map<LotId, SetStateForLotsAction.Result.Lot> = data.associateBy { it.id }
-        val receivedLotsIds = receivedLotByIds.keys
-        val tenderLotsIds = tender.lots.toSetBy { it.id }
-
-        val unknownLots = receivedLotsIds.minus(tenderLotsIds)
-        if (unknownLots.isNotEmpty())
-            return MaybeFail.fail(
-                Fail.Incident.Response.Validation.UnknownEntity(
-                    id = unknownLots.joinToString(), name = LOT
-                )
-            )
-        val missingLots = tenderLotsIds.minus(receivedLotsIds)
-        if (missingLots.isNotEmpty())
-            return MaybeFail.fail(
-                Fail.Incident.Response.Validation.MissingExpectedEntity(
-                    id = missingLots.joinToString(), name = LOT
-                )
-            )
 
         val updatedLots = tender.lots
             .map { lot ->
@@ -150,5 +155,20 @@ class AccessSetStateForLotsDelegate(
         return MaybeFail.none()
     }
 
-    class Parameters(val status: LotStatus, val statusDetails: LotStatusDetails)
+    class Parameters(val status: LotStatus, val statusDetails: LotStatusDetails, val location: Location)
+
+    enum class Location(@JsonValue override val key: String) : EnumElementProvider.Key {
+
+        TENDER("tender"),
+        TENDER_AMENDMENT("tender.amendment");
+
+        override fun toString(): String = key
+
+        companion object : EnumElementProvider<Location>(info = info()) {
+
+            @JvmStatic
+            @JsonCreator
+            fun creator(name: String) = Location.orThrow(name)
+        }
+    }
 }
