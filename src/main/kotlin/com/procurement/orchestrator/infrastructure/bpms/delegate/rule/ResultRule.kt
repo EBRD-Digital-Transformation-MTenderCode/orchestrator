bbe5c374e2b0adcdf1dd918baf486ce36b435435
katherine <kate.shaidurova@gmail.com>
2020-04-22 13:46:32 +0300

    ES-2959 Refactored ResultRule to use CollectionExtension methods

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/rule/ResultRule.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/rule/ResultRule.kt
@@ -3,26 +3,26 @@ package com.procurement.orchestrator.infrastructure.bpms.delegate.rule
 import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.ValidationResult
 import com.procurement.orchestrator.domain.functional.ValidationRule
+import com.procurement.orchestrator.domain.util.extension.getDuplicate
+import com.procurement.orchestrator.domain.util.extension.getMissingElements
+import com.procurement.orchestrator.domain.util.extension.getUnknownElements
 
-fun <T : List<Any>> duplicatesRule(attributeName: String): ValidationRule<T, Fail.Incident.Response.Validation.DuplicateEntity> =
-    ValidationRule { valuesToCheck: T ->
-        val duplicates = valuesToCheck.groupingBy { it }
-            .eachCount()
-            .filter { it.value > 1 }
-
-        if (duplicates.isNotEmpty())
+fun <T : Collection<Any>> duplicatesRule(attributeName: String): ValidationRule<T, Fail.Incident.Response.Validation.DuplicateEntity> =
+    ValidationRule { received: T ->
+        val duplicate = received.getDuplicate { it }
+        if (duplicate != null)
             ValidationResult.error(
                 Fail.Incident.Response.Validation.DuplicateEntity(
-                    id = duplicates.keys.joinToString(), name = attributeName
+                    id = duplicate.toString(), name = attributeName
                 )
             )
         else
             ValidationResult.ok()
     }
 
-fun <T : Set<Any>> unknownEntityRule(expectedValues: T, attributeName: String) =
-    ValidationRule { valuesToCheck: T ->
-        val unknownValues = valuesToCheck.minus(expectedValues)
+fun <T : Collection<Any>> unknownEntityRule(expectedValues: T, attributeName: String) =
+    ValidationRule { received: T ->
+        val unknownValues = getUnknownElements(received = received, known = expectedValues)
         if (unknownValues.isNotEmpty())
             ValidationResult.error(
                 Fail.Incident.Response.Validation.UnknownEntity(
@@ -33,9 +33,9 @@ fun <T : Set<Any>> unknownEntityRule(expectedValues: T, attributeName: String) =
             ValidationResult.ok()
     }
 
-fun <T : Set<Any>> missingEntityRule(expectedValues: T, attributeName: String) =
-    ValidationRule { valuesToCheck: T ->
-        val missingValues = expectedValues.minus(valuesToCheck)
+fun <T : Collection<Any>> missingEntityRule(expectedValues: T, attributeName: String) =
+    ValidationRule { received: T ->
+        val missingValues = getMissingElements(received = received, known = expectedValues)
         if (missingValues.isNotEmpty())
             ValidationResult.error(
                 Fail.Incident.Response.Validation.MissingExpectedEntity(
