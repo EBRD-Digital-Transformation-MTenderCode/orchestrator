1a1d517fc7e15ee556da340013ab2e41deea65d3
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-04-23 16:33:46 +0300

    ES-3058 Implemented controller for '/cancel/amendment' endpoint.

---
+++ src/main/kotlin/com/procurement/orchestrator/application/service/cancellation/CancellationAmendment.kt
@@ -0,0 +1,27 @@
+package com.procurement.orchestrator.application.service.cancellation
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.application.model.OperationId
+import com.procurement.orchestrator.application.model.Owner
+import com.procurement.orchestrator.application.model.PlatformId
+import com.procurement.orchestrator.application.model.Token
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.amendment.AmendmentId
+
+object CancellationAmendment {
+
+    class Request(
+        val operationId: OperationId,
+        val platformId: PlatformId,
+        val context: Context
+    ) {
+        class Context(
+            @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: Cpid,
+            @field:JsonProperty("ocid") @param:JsonProperty("ocid") val ocid: Ocid,
+            @field:JsonProperty("token") @param:JsonProperty("token") val token: Token,
+            @field:JsonProperty("owner") @param:JsonProperty("owner") val owner: Owner,
+            @field:JsonProperty("amendmentId") @param:JsonProperty("amendmentId") val amendmentId: AmendmentId
+        )
+    }
+}
