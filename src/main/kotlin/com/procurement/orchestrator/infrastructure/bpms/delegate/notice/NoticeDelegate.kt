ae5dd22c41846606e21b636837b803a77f60fea7
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-04-28 17:50:46 +0300

    ES-3107 Changed 'Notice' delegate (inherited from AbstractSeqExternalDelegate).

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/notice/NoticeDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/notice/NoticeDelegate.kt
@@ -6,10 +6,10 @@ import com.procurement.orchestrator.application.model.context.CamundaGlobalConte
 import com.procurement.orchestrator.application.service.Logger
 import com.procurement.orchestrator.application.service.Transform
 import com.procurement.orchestrator.domain.fail.Fail
-import com.procurement.orchestrator.domain.functional.MaybeFail
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.domain.functional.Result.Companion.success
-import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
+import com.procurement.orchestrator.domain.functional.asSuccess
+import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractSeqExternalDelegate
 import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
 import com.procurement.orchestrator.infrastructure.bpms.model.queue.QueueNoticeTask
 import com.procurement.orchestrator.infrastructure.bpms.model.queue.grouping
@@ -20,6 +20,7 @@ import com.procurement.orchestrator.infrastructure.client.reply.Reply
 import com.procurement.orchestrator.infrastructure.client.web.notice.action.CreateRecordAction
 import com.procurement.orchestrator.infrastructure.client.web.notice.action.UpdateRecordAction
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 
 @Component
 class NoticeDelegate(
@@ -28,67 +29,72 @@ class NoticeDelegate(
     operationStepRepository: OperationStepRepository,
     private val noticeQueueRepository: NoticeQueueRepository,
     private val transform: Transform
-) : AbstractExternalDelegate<Unit, Unit>(
+) : AbstractSeqExternalDelegate<Unit, NoticeDelegate.Task, Unit>(
     logger = logger,
     transform = transform,
     operationStepRepository = operationStepRepository
 ) {
 
-    override fun parameters(parameterContainer: ParameterContainer): Result<Unit, Fail.Incident.Bpmn.Parameter> =
+    override fun prepareParameters(parameterContainer: ParameterContainer): Result<Unit, Fail.Incident.Bpmn.Parameter> =
         success(Unit)
 
-    override suspend fun execute(
-        commandId: CommandId,
+    override fun prepareSeq(
         context: CamundaGlobalContext,
         parameters: Unit
-    ): Result<Reply<Unit>, Fail.Incident> {
-
+    ): Result<List<Task>, Fail.Incident> {
         val requestInfo = context.requestInfo
         val operationId = requestInfo.operationId
 
-        val groupedTask = noticeQueueRepository.load(operationId = operationId)
+        return noticeQueueRepository.load(operationId = operationId)
             .orForwardFail { fail -> return fail }
             .grouping()
-
-        groupedTask.forEach { (id, tasks) ->
-            val salt = id.ocid.toString()
-            val commandIdWithSalt = commandId + salt
-            when (id.action) {
-                QueueNoticeTask.Action.UPDATE_RECORD -> {
-                    val data = tasks.merge(transform)
-                        .orForwardFail { fail -> return fail }
-                    val params = UpdateRecordAction.Params(date = requestInfo.timestamp, data = data)
-                    val reply = noticeClient.updateRecord(id = commandIdWithSalt, params = params)
-                        .orForwardFail { fail -> return fail }
-
-                    when (reply) {
-                        is Reply.Success -> Unit
-                        is Reply.Errors -> return success(reply)
-                        is Reply.Incident -> return success(reply)
+            .map { (id, tasks) ->
+                tasks.merge(transform)
+                    .orForwardFail { fail -> return fail }
+                    .let { data ->
+                        Task(
+                            action = id.action,
+                            salt = id.ocid.toString(),
+                            date = requestInfo.timestamp,
+                            data = data
+                        )
                     }
-                }
-                QueueNoticeTask.Action.CREATE_RECORD -> {
-                    val data = tasks.merge(transform)
-                        .orForwardFail { fail -> return fail }
-                    val params = CreateRecordAction.Params(date = requestInfo.timestamp, data = data)
-                    val reply = noticeClient.createRecord(id = commandIdWithSalt, params = params)
-                        .orForwardFail { fail -> return fail }
+            }
+            .asSuccess()
+    }
 
-                    when (reply) {
-                        is Reply.Success -> Unit
-                        is Reply.Errors -> return success(reply)
-                        is Reply.Incident -> return success(reply)
-                    }
-                }
+    override suspend fun call(
+        baseCommandId: CommandId,
+        element: Task
+    ): Result<Reply<Unit>, Fail.Incident> {
+
+        val commandIdWithSalt = baseCommandId + element.salt
+        val reply = when (element.action) {
+            QueueNoticeTask.Action.UPDATE_RECORD -> {
+                val params = UpdateRecordAction.Params(date = element.date, data = element.data)
+                noticeClient.updateRecord(id = commandIdWithSalt, params = params)
+                    .orForwardFail { fail -> return fail }
+            }
+
+            QueueNoticeTask.Action.CREATE_RECORD -> {
+                val params = CreateRecordAction.Params(date = element.date, data = element.data)
+                noticeClient.createRecord(id = commandIdWithSalt, params = params)
+                    .orForwardFail { fail -> return fail }
             }
         }
 
-        return success(Reply.None)
+        return when (reply) {
+            is Reply.None -> success(Reply.None)
+            is Reply.Success -> success(Reply.None)
+            is Reply.Errors -> success(reply)
+            is Reply.Incident -> success(reply)
+        }
     }
 
-    override fun updateGlobalContext(
-        context: CamundaGlobalContext,
-        parameters: Unit,
-        data: Unit
-    ): MaybeFail<Fail.Incident.Bpmn> = MaybeFail.none()
+    class Task(
+        val action: QueueNoticeTask.Action,
+        val salt: String,
+        val date: LocalDateTime,
+        val data: String
+    )
 }
