6d75ce16808e8fda0b4d3256ee8fd2699e7857b1
Evgen Kopach <zkopach@gmail.com>
2020-05-11 18:07:35 +0300

    ES-3224 Added command 'createSubmission' to Qualification commands list

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/qualification/QualificationCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/qualification/QualificationCommands.kt
@@ -1,8 +1,11 @@
 package com.procurement.orchestrator.infrastructure.client.web.qualification
 
 import com.procurement.orchestrator.infrastructure.client.web.qualification.action.CheckPeriodAction
+import com.procurement.orchestrator.infrastructure.client.web.qualification.action.CreateSubmissionAction
 
 object QualificationCommands {
+
+    object CreateSubmission : CreateSubmissionAction()
 
     object CheckPeriod : CheckPeriodAction()
 }
