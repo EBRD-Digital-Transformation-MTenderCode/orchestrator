b1c0c0794f407fb74fdade608cefe450c480717d
Evgen Kopach <zkopach@gmail.com>
2020-05-11 14:21:21 +0300

    ES-3223 Changed object structure of MainEconomicActivity.kt

--- src/main/kotlin/com/procurement/orchestrator/domain/model/organization/datail/MainEconomicActivity.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/organization/datail/MainEconomicActivity.kt
@@ -1,3 +1,17 @@
 package com.procurement.orchestrator.domain.model.organization.datail
 
-typealias MainEconomicActivity = String
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class MainEconomicActivity(
+    @field:JsonProperty("description") @param:JsonProperty("description") val description: String,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("scheme") @param:JsonProperty("scheme") val scheme: String?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("id") @param:JsonProperty("id") val id: String?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("uri") @param:JsonProperty("uri") val uri: String?
+)
