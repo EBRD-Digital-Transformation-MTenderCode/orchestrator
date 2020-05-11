eb06231fe9cb3a5d7337791e106186967ea08f3d
Evgen Kopach <zkopach@gmail.com>
2020-05-11 14:22:45 +0300

    ES-3223 Added 'validateSubmission' command to Qualification commands list

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/qualification/QualificationCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/qualification/QualificationCommands.kt
@@ -1,8 +1,11 @@
 package com.procurement.orchestrator.infrastructure.client.web.qualification
 
 import com.procurement.orchestrator.infrastructure.client.web.qualification.action.CheckPeriodAction
+import com.procurement.orchestrator.infrastructure.client.web.qualification.action.ValidateSubmissionAction
 
 object QualificationCommands {
 
     object CheckPeriod : CheckPeriodAction()
+
+    object ValidateSubmission : ValidateSubmissionAction()
 }
