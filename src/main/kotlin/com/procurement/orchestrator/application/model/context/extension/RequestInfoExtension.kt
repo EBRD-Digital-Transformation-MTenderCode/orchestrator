c69a1f1fc624cf877a2bd94a75485e330cd75d8c
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-05-12 14:09:08 +0300

    ES-3234 added RequestInfoExtension

---
+++ src/main/kotlin/com/procurement/orchestrator/application/model/context/extension/RequestInfoExtension.kt
@@ -0,0 +1,18 @@
+package com.procurement.orchestrator.application.model.context.extension
+
+import com.procurement.orchestrator.application.model.Token
+import com.procurement.orchestrator.application.model.context.members.RequestInfo
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.domain.functional.asFailure
+import com.procurement.orchestrator.domain.functional.asSuccess
+
+private const val TOKEN_ATTRIBUTE_NAME = "token"
+
+fun RequestInfo.tryGetToken(): Result<Token, Fail.Incident.Bpms.Context.Missing> {
+    val token = this.token
+        ?: return Fail.Incident.Bpms.Context.Missing(name = TOKEN_ATTRIBUTE_NAME)
+            .asFailure()
+    return token.asSuccess()
+}
+
