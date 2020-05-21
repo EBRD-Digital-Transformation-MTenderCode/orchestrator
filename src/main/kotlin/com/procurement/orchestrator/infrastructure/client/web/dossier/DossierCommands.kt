d85dc2e97af635f6b2eec45054fc99b815863bf8
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-05-21 12:21:09 +0300

    ES-3302 Add new 'GetSubmissionPeriodEndDate' action and 'GetSubmissionPeriodEndDate' command for 'Dossier' service.

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/dossier/DossierCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/dossier/DossierCommands.kt
@@ -1,8 +1,11 @@
 package com.procurement.orchestrator.infrastructure.client.web.dossier
 
+import com.procurement.orchestrator.infrastructure.client.web.dossier.action.GetSubmissionPeriodEndDateAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.ValidateRequirementResponseAction
 
 object DossierCommands {
+
+    object GetSubmissionPeriodEndDate : GetSubmissionPeriodEndDateAction()
 
     object ValidateRequirementResponse : ValidateRequirementResponseAction()
 }
