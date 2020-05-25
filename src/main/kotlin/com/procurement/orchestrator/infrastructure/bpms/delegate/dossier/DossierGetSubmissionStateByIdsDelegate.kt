e7fe93548e2a47e960b204ca4cbebf6660cf8463
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-05-25 16:07:57 +0300

    ES-3236 updated 'DossierGetSubmissionStateByIdsDelegate' to new delegate logic

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/dossier/DossierGetSubmissionStateByIdsDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/dossier/DossierGetSubmissionStateByIdsDelegate.kt
@@ -4,10 +4,12 @@ import com.procurement.orchestrator.application.CommandId
 import com.procurement.orchestrator.application.client.DossierClient
 import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
 import com.procurement.orchestrator.application.model.context.extension.getDetailsIfNotEmpty
+import com.procurement.orchestrator.application.model.context.extension.tryGetSubmissions
 import com.procurement.orchestrator.application.service.Logger
 import com.procurement.orchestrator.application.service.Transform
 import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.MaybeFail
+import com.procurement.orchestrator.domain.functional.Option
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.domain.model.submission.Details
 import com.procurement.orchestrator.domain.model.submission.Submissions
@@ -15,6 +17,7 @@ import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExterna
 import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
 import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.dossier.DossierCommands
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.GetSubmissionStateByIdsAction
 import org.springframework.stereotype.Component
 
@@ -40,7 +43,9 @@ class DossierGetSubmissionStateByIdsDelegate(
 
         val processInfo = context.processInfo
 
-        val submissions = context.submissions.getDetailsIfNotEmpty()
+        val submissions = context.tryGetSubmissions()
+            .orForwardFail { fail -> return fail }
+            .getDetailsIfNotEmpty()
             .orForwardFail { fail -> return fail }
 
         return dossierClient.getSubmissionStateByIds(
@@ -56,12 +61,21 @@ class DossierGetSubmissionStateByIdsDelegate(
     override fun updateGlobalContext(
         context: CamundaGlobalContext,
         parameters: Unit,
-        data: GetSubmissionStateByIdsAction.Result
+        result: Option<GetSubmissionStateByIdsAction.Result>
     ): MaybeFail<Fail.Incident> {
 
-        val submissions = context.submissions
+        val submissions = context.tryGetSubmissions()
+            .orReturnFail { fail -> return MaybeFail.fail(fail)  }
             .getDetailsIfNotEmpty()
             .orReturnFail { fail -> return MaybeFail.fail(fail) }
+
+        val data = result.orNull
+            ?: return MaybeFail.fail(
+                Fail.Incident.Response.Empty(
+                    service = "eDossier",
+                    action = DossierCommands.GetSubmissionStateByIds
+                )
+            )
 
         val receivedSubmissionsByIds = data.associateBy { it.id }
 
