cb22bc45fe473274c03716d92957d09e75322a85
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-05-25 19:03:21 +0300

    ES-3303 Added new types 'MainEconomicActivityScheme' & 'MainEconomicActivityId'.

--- src/main/kotlin/com/procurement/orchestrator/domain/model/organization/datail/MainEconomicActivity.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/organization/datail/MainEconomicActivity.kt
@@ -10,10 +10,10 @@ data class MainEconomicActivity(
     @field:JsonProperty("description") @param:JsonProperty("description") val description: String,
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:JsonProperty("scheme") @param:JsonProperty("scheme") val scheme: String?,
+    @field:JsonProperty("scheme") @param:JsonProperty("scheme") val scheme: MainEconomicActivityScheme?,
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:JsonProperty("id") @param:JsonProperty("id") val id: String?,
+    @field:JsonProperty("id") @param:JsonProperty("id") val id: MainEconomicActivityId?,
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @field:JsonProperty("uri") @param:JsonProperty("uri") val uri: String?
