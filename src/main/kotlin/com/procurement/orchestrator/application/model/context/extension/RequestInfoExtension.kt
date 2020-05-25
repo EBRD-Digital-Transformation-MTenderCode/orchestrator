d72756ec67e802ed6b2d8037ee6f48d8ec4863fe
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-05-25 15:32:20 +0300

    ES-3234 improved token's way in 'RequestInfoExtension'

--- src/main/kotlin/com/procurement/orchestrator/application/model/context/extension/RequestInfoExtension.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/model/context/extension/RequestInfoExtension.kt
@@ -4,15 +4,12 @@ import com.procurement.orchestrator.application.model.Token
 import com.procurement.orchestrator.application.model.context.members.RequestInfo
 import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.Result
-import com.procurement.orchestrator.domain.functional.asFailure
-import com.procurement.orchestrator.domain.functional.asSuccess
+import com.procurement.orchestrator.domain.functional.Result.Companion.failure
+import com.procurement.orchestrator.domain.functional.Result.Companion.success
 
-private const val TOKEN_ATTRIBUTE_NAME = "requestInfo.token"
+private const val PATH = "requestInfo"
+private const val TOKEN_ATTRIBUTE_NAME = "token"
 
-fun RequestInfo.tryGetToken(): Result<Token, Fail.Incident.Bpms.Context.Missing> {
-    val token = this.token
-        ?: return Fail.Incident.Bpms.Context.Missing(name = TOKEN_ATTRIBUTE_NAME)
-            .asFailure()
-    return token.asSuccess()
-}
-
+fun RequestInfo.tryGetToken(): Result<Token, Fail.Incident.Bpms.Context.Missing> = this.token
+    ?.let { success(it) }
+    ?: failure(Fail.Incident.Bpms.Context.Missing(name = TOKEN_ATTRIBUTE_NAME, path = PATH))
