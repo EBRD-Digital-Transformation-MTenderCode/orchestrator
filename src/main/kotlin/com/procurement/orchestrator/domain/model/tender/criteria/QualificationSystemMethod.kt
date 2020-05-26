c79628fc34ca01c29e68ea2a67ff58abf15f1993
Evgen Kopach <zkopach@gmail.com>
2020-05-26 13:42:37 +0300

    ES-3353 Created QualificationSystemMethod enum

---
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/tender/criteria/QualificationSystemMethod.kt
@@ -0,0 +1,20 @@
+package com.procurement.orchestrator.domain.model.tender.criteria
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+import com.procurement.orchestrator.domain.EnumElementProvider
+
+enum class QualificationSystemMethod(@JsonValue override val key: String) : EnumElementProvider.Key {
+
+    automated("automated"),
+    manual("manual");
+
+    override fun toString(): String = key
+
+    companion object : EnumElementProvider<QualificationSystemMethod>(info = info()) {
+
+        @JvmStatic
+        @JsonCreator
+        fun creator(name: String) = orThrow(name)
+    }
+}
