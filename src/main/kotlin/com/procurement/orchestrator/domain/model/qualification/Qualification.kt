5c00bd76eeda1245fed984f8a2da61de094c659a
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-05-28 16:15:23 +0300

    ES-3316 added Qualification domain model, QualificationStatus, QualificationStatusDetails, QualificationId, Qualifications

---
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/qualification/Qualification.kt
@@ -0,0 +1,40 @@
+package com.procurement.orchestrator.domain.model.qualification
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.domain.model.IdentifiableObject
+import com.procurement.orchestrator.domain.model.or
+import com.procurement.orchestrator.domain.model.submission.SubmissionId
+import java.io.Serializable
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+data class Qualification(
+    @field:JsonProperty("id") @param:JsonProperty("id") val id: QualificationId,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("status") @param:JsonProperty("status") val status: QualificationStatus? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("statusDetails") @param:JsonProperty("statusDetails") val statusDetails: QualificationStatusDetails? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("relatedSubmission") @param:JsonProperty("relatedSubmission") val relatedSubmission: SubmissionId? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("date") @param:JsonProperty("date") val date: LocalDateTime? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("scoring") @param:JsonProperty("scoring") val scoring: BigDecimal? = null
+
+) : IdentifiableObject<Qualification>, Serializable {
+
+    override fun updateBy(src: Qualification): Qualification = Qualification(
+        id = id,
+        date = src.date or date,
+        status = src.status or status,
+        statusDetails = src.statusDetails or statusDetails,
+        relatedSubmission = src.relatedSubmission or relatedSubmission,
+        scoring = src.scoring or scoring
+    )
+}
