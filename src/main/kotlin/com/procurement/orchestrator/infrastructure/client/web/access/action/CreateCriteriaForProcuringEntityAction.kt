b0e879b435cbb92b49984bbd21cd421c044953b3
Evgen Kopach <zkopach@gmail.com>
2020-06-02 11:35:09 +0300

    ES-3400 Created Params & Result DTO for 'createCriteriaForProcuringEntity' command

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/action/CreateCriteriaForProcuringEntityAction.kt
@@ -0,0 +1,86 @@
+package com.procurement.orchestrator.infrastructure.client.web.access.action
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.procurement.orchestrator.application.service.FunctionalAction
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.requirement.RequirementId
+import com.procurement.orchestrator.domain.model.tender.criteria.CriteriaSource
+import com.procurement.orchestrator.domain.model.tender.criteria.CriterionId
+import com.procurement.orchestrator.domain.model.tender.criteria.requirement.Requirement
+import com.procurement.orchestrator.domain.model.tender.criteria.requirement.RequirementGroupId
+import com.procurement.orchestrator.infrastructure.bind.criteria.requirement.RequirementDeserializer
+import com.procurement.orchestrator.infrastructure.bind.criteria.requirement.RequirementSerializer
+import com.procurement.orchestrator.infrastructure.client.web.Target
+import com.procurement.orchestrator.infrastructure.model.Version
+import java.io.Serializable
+
+abstract class CreateCriteriaForProcuringEntityAction : FunctionalAction<CreateCriteriaForProcuringEntityAction.Params, CreateCriteriaForProcuringEntityAction.Result> {
+    override val version: Version = Version.parse("2.0.0")
+    override val name: String = "createCriteriaForProcuringEntity"
+    override val target: Target<Result> = Target.plural()
+
+    class Params(
+        @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: Cpid,
+        @field:JsonProperty("ocid") @param:JsonProperty("ocid") val ocid: Ocid,
+        @field:JsonProperty("criteria") @param:JsonProperty("criteria") val criteria: List<Criterion>
+    ) {
+        data class Criterion(
+            @field:JsonProperty("id") @param:JsonProperty("id") val id: CriterionId,
+            @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
+
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
+
+            @JsonInclude(JsonInclude.Include.NON_EMPTY)
+            @field:JsonProperty("requirementGroups") @param:JsonProperty("requirementGroups") val requirementGroups: List<RequirementGroup>
+        ) {
+            data class RequirementGroup(
+                @field:JsonProperty("id") @param:JsonProperty("id") val id: RequirementGroupId,
+
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
+
+                @field:JsonProperty("requirements") @param:JsonProperty("requirements") val requirements: List<Requirement>
+            ) {
+                data class Requirement(
+                    @field:JsonProperty("id") @param:JsonProperty("id") val id: RequirementId,
+                    @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
+                    
+                    @JsonInclude(JsonInclude.Include.NON_NULL)
+                    @field:JsonProperty("description") @param:JsonProperty("description") val description: String?
+                )
+            }
+        }
+    }
+
+    class Result(values: List<Criterion>) : List<Result.Criterion> by values, Serializable {
+
+        data class Criterion(
+            @field:JsonProperty("id") @param:JsonProperty("id") val id: CriterionId,
+            @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
+            @field:JsonProperty("source") @param:JsonProperty("source") val source: CriteriaSource,
+
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
+
+            @JsonInclude(JsonInclude.Include.NON_EMPTY)
+            @field:JsonProperty("requirementGroups") @param:JsonProperty("requirementGroups") val requirementGroups: List<RequirementGroup>
+        ) : Serializable {
+
+            data class RequirementGroup(
+                @field:JsonProperty("id") @param:JsonProperty("id") val id: RequirementGroupId,
+
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
+
+                @JsonDeserialize(using = RequirementDeserializer::class)
+                @JsonSerialize(using = RequirementSerializer::class)
+                @field:JsonProperty("requirements") @param:JsonProperty("requirements") val requirements: List<Requirement>
+            ) : Serializable
+        }
+    }
+}
