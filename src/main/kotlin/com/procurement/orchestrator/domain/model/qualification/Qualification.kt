be9fba4d75ff618af0d394a82841416852668d73
yevhenii <yevheniiskoryi@gmail.com>
2020-06-03 16:50:03 +0300

    ES-3316 added 'Scoring' class with de/serializer

--- src/main/kotlin/com/procurement/orchestrator/domain/model/qualification/Qualification.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/qualification/Qualification.kt
@@ -3,10 +3,10 @@ package com.procurement.orchestrator.domain.model.qualification
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.orchestrator.domain.model.IdentifiableObject
+import com.procurement.orchestrator.domain.model.measure.Scoring
 import com.procurement.orchestrator.domain.model.or
 import com.procurement.orchestrator.domain.model.submission.SubmissionId
 import java.io.Serializable
-import java.math.BigDecimal
 import java.time.LocalDateTime
 
 data class Qualification(
@@ -25,7 +25,8 @@ data class Qualification(
     @field:JsonProperty("date") @param:JsonProperty("date") val date: LocalDateTime? = null,
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:JsonProperty("scoring") @param:JsonProperty("scoring") val scoring: BigDecimal? = null
+
+    @field:JsonProperty("scoring") @param:JsonProperty("scoring") val scoring: Scoring? = null
 
 ) : IdentifiableObject<Qualification>, Serializable {
 
