e37d5354de433a4bc8ac2d7525094a09260bf15b
Evgen Kopach <zkopach@gmail.com>
2020-06-09 12:43:29 +0300

    ES-3430 Created Params, Result, Error DTO for 'getCountry' command

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/mdm/action/EnrichCountryAction.kt
@@ -0,0 +1,33 @@
+package com.procurement.orchestrator.infrastructure.client.web.mdm.action
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.io.Serializable
+
+abstract class EnrichCountryAction {
+
+    class Params(
+        @field:JsonProperty("lang") @param:JsonProperty("lang") val lang: String,
+        @field:JsonProperty("scheme") @param:JsonProperty("scheme") val scheme: String,
+        @field:JsonProperty("countyId") @param:JsonProperty("countyId") val countyId: String
+    )
+
+    class Result(
+        @field:JsonProperty("data") @param:JsonProperty("data") val data: Data
+    ) : Serializable {
+        data class Data(
+            @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
+            @field:JsonProperty("scheme") @param:JsonProperty("scheme") val scheme: String,
+            @field:JsonProperty("description") @param:JsonProperty("description") val description: String,
+            @field:JsonProperty("uri") @param:JsonProperty("uri") val uri: String
+        ): Serializable
+    }
+
+    class ResponseError(
+        @field:JsonProperty("errors") @param:JsonProperty("errors") val errors: List<Error>
+    ) : Serializable {
+        data class Error(
+            @field:JsonProperty("code") @param:JsonProperty("code") val code: String,
+            @field:JsonProperty("description") @param:JsonProperty("description") val description: String
+        ): Serializable
+    }
+}
