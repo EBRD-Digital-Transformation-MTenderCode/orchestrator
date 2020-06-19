f7772daf0672ce59fb5707774e1711d6b8643a6c
katherine <kate.shaidurova@gmail.com>
2020-06-19 12:39:29 +0300

    ES-3567 Added QUALIFICATION_DECLARE_NON_CONFLICT_OF_INTEREST branch handling to RevisionCreateAmendmentDelegate.kt

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/revision/RevisionCreateAmendmentDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/revision/RevisionCreateAmendmentDelegate.kt
@@ -66,7 +66,8 @@ class RevisionCreateAmendmentDelegate(
             OperationTypeProcess.DECLARE_NON_CONFLICT_OF_INTEREST,
             OperationTypeProcess.TENDER_OR_LOT_AMENDMENT_CANCELLATION,
             OperationTypeProcess.TENDER_OR_LOT_AMENDMENT_CONFIRMATION,
-            OperationTypeProcess.WITHDRAW_SUBMISSION ->
+            OperationTypeProcess.WITHDRAW_SUBMISSION,
+            OperationTypeProcess.QUALIFICATION_DECLARE_NON_CONFLICT_OF_INTEREST->
                 return failure(Fail.Incident.Bpe(description = "Operation type: '${processInfo.operationType.key}' in this delegate do not implemented."))
         }
 
