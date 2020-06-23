0c8f0741719f3627e397e1e77082bdb77ae93b1f
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-06-23 12:58:46 +0300

    ES-3373 Implemented new logic for chronograph message consumer.

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/message/chronograph/ChronographMessageConsumer.kt
@@ -0,0 +1,103 @@
+package com.procurement.orchestrator.infrastructure.message.chronograph
+
+import com.procurement.orchestrator.application.model.OperationId
+import com.procurement.orchestrator.application.model.Owner
+import com.procurement.orchestrator.application.model.PlatformId
+import com.procurement.orchestrator.application.model.RequestId
+import com.procurement.orchestrator.application.service.ChronographEvent
+import com.procurement.orchestrator.application.service.ProcessLauncher
+import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.Context
+import com.procurement.orchestrator.domain.extension.date.parseLocalDateTime
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.MaybeFail
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.infrastructure.web.controller.parseCpid
+import com.procurement.orchestrator.infrastructure.web.controller.parseSingleStageOcid
+import com.procurement.orchestrator.service.ProcessService
+import com.procurement.orchestrator.service.RequestService
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.handler.annotation.Header
+import java.time.LocalDateTime
+
+class ChronographMessageConsumer(
+    private val processService: ProcessService,
+    private val requestService: RequestService,
+    private val transform: Transform,
+    private val processLauncher: ProcessLauncher
+) {
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ChronographMessageConsumer::class.java)
+    }
+
+    @KafkaListener(topics = ["chronograph-out"])
+    fun onChronograph(
+        message: String,
+        @Header(KafkaHeaders.ACKNOWLEDGMENT) acknowledgment: Acknowledgment
+    ) {
+        try {
+            LOG.info("Received a message from the Chronograph ($message).")
+            val response = transform.tryDeserialization(message, ChronographMessage::class.java)
+                .orThrow { it.exception }
+
+            if ("NOTIFICATION" == response.status.toUpperCase()) {
+                if ("SUBMISSION" == response.data.phase.toUpperCase())
+                    processingNew(response.data)
+                else
+                    processingOld(response.data)
+            }
+            acknowledgment.acknowledge()
+        } catch (e: Exception) {
+            LOG.error("Error while processing the message from the Chronograph ($message).", e)
+        }
+    }
+
+    private fun processingOld(data: ChronographMessage.Data): MaybeFail<Fail> {
+        val contextChronograph = transform.tryDeserialization(data.metadata, Context::class.java)
+            .orReturnFail { return MaybeFail.fail(it) }
+
+        val prevContext: Context = requestService.getContext(contextChronograph.cpid)
+        val context = requestService.checkRulesAndProcessContext(
+            prevContext,
+            contextChronograph.processType,
+            contextChronograph.requestId
+        )
+        requestService.saveRequestAndCheckOperation(context, null)
+        val variables = mapOf(
+            "operationType" to context.operationType
+        )
+        processService.startProcess(context, variables)
+
+        return MaybeFail.none()
+    }
+
+    private fun processingNew(data: ChronographMessage.Data): MaybeFail<Fail> {
+        val payload = transform.tryDeserialization(data.metadata, ChronographMessage.Metadata::class.java)
+            .orReturnFail { return MaybeFail.fail(it) }
+        val cpid: Cpid = parseCpid(payload.cpid)
+            .orReturnFail { return MaybeFail.fail(it) }
+        val ocid: Ocid = parseSingleStageOcid(payload.ocid)
+            .orReturnFail { return MaybeFail.fail(it) }
+        val operationId: OperationId = OperationId.tryCreateOrNull(payload.operationId)!!
+        val requestId: RequestId = RequestId.tryCreateOrNull(payload.requestId)!!
+        val owner: Owner = Owner.tryCreateOrNull(payload.owner)!!
+        val platformId: PlatformId = owner
+        val timestamp: LocalDateTime = data.launchTime.parseLocalDateTime()
+
+        val event = ChronographEvent(
+            operationId = operationId,
+            platformId = platformId,
+            cpid = cpid,
+            ocid = ocid,
+            owner = owner,
+            timestamp = timestamp,
+            processName = payload.processType,
+            requestId = requestId
+        )
+        return processLauncher.launch(event)
+    }
+}
