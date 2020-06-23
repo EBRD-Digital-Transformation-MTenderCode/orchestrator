672226e84349b85afc57429922fb319a942cdfe7
katherine <kate.shaidurova@gmail.com>
2020-06-23 19:37:47 +0300

    ES-3342 Fixed imports in DossierClient.kt

--- src/main/kotlin/com/procurement/orchestrator/application/client/DossierClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/client/DossierClient.kt
@@ -7,8 +7,6 @@ import com.procurement.orchestrator.infrastructure.client.reply.Reply
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CheckAccessToSubmissionAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CheckPeriodAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CreateSubmissionAction
-import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CheckPeriodAction
-import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CreateSubmissionAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.FindSubmissionsForOpeningAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.GetOrganizationsAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.GetSubmissionPeriodEndDateAction
