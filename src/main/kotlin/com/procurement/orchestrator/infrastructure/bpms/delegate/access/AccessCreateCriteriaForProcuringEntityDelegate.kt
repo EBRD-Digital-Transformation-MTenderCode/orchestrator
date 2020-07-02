85aac488ffe617011e6f5fc67376a0f21e79618c
yevhenii <yevheniiskoryi@gmail.com>
2020-07-02 11:35:44 +0300

    ES-3668 extended 'CreateCriteriaForProcuringEntityAction.Params' on OperationType

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/access/AccessCreateCriteriaForProcuringEntityDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/access/AccessCreateCriteriaForProcuringEntityDelegate.kt
@@ -56,7 +56,8 @@ class AccessCreateCriteriaForProcuringEntityDelegate(
             params = Params(
                 cpid = processInfo.cpid,
                 ocid = processInfo.ocid,
-                criteria = criteriaFromContext
+                criteria = criteriaFromContext,
+                operationType = processInfo.operationType
             )
         )
     }
