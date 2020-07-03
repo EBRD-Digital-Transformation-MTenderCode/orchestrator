d43a3b5b33280e43851e4b34f585beb7cec8026b
katherine <kate.shaidurova@gmail.com>
2020-07-03 13:00:53 +0300

    ES-3674 Renamed methods and variables that contained qualificationDocuments to qualificationReqRsDocuments in StorageCheckRegistrationDelegate.kt

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/storage/StorageCheckRegistrationDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/storage/StorageCheckRegistrationDelegate.kt
@@ -109,15 +109,15 @@ class StorageCheckRegistrationDelegate(
             getSubmissionsCandidateDocumentsIds(context, entities)
                 .orForwardFail { fail -> return fail }
 
-        val qualificationDocuments: List<DocumentId> =
-            getQualificationDocumentsIds(context, entities)
+        val qualificationReqRsDocuments: List<DocumentId> =
+            getQualificationReqRsDocumentsIds(context, entities)
                 .orForwardFail { fail -> return fail }
 
         val documentIds: List<DocumentId> = tenderDocuments.plus(amendmentDocuments)
             .plus(awardRequirementResponseDocuments)
             .plus(submissionsDocuments)
             .plus(submissionCandidateDocuments)
-            .plus(qualificationDocuments)
+            .plus(qualificationReqRsDocuments)
 
         if (documentIds.isEmpty())
             return success(Reply.None)
@@ -309,16 +309,16 @@ class StorageCheckRegistrationDelegate(
             .map { document -> document.id }
             .asSuccess()
 
-    private fun getQualificationDocumentsIds(
+    private fun getQualificationReqRsDocumentsIds(
         context: CamundaGlobalContext, entities: Map<EntityKey, EntityValue>
     ): Result<List<DocumentId>, Fail.Incident> =
         when (entities[EntityKey.QUALIFICATION_REQUIREMENT_RESPONSE]) {
-            EntityValue.OPTIONAL -> getQualificationDocumentsIdsOptional(context)
-            EntityValue.REQUIRED -> getQualificationDocumentsIdsRequired(context)
+            EntityValue.OPTIONAL -> getQualificationReqRsDocumentsIdsOptional(context)
+            EntityValue.REQUIRED -> getQualificationReqRsDocumentsIdsRequired(context)
             null -> emptyList<DocumentId>().asSuccess()
         }
 
-    private fun getQualificationDocumentsIdsOptional(context: CamundaGlobalContext): Result<List<DocumentId>, Fail.Incident> =
+    private fun getQualificationReqRsDocumentsIdsOptional(context: CamundaGlobalContext): Result<List<DocumentId>, Fail.Incident> =
         context.qualifications
             .asSequence()
             .flatMap { qualification -> qualification.requirementResponses.asSequence() }
@@ -328,7 +328,7 @@ class StorageCheckRegistrationDelegate(
             .toList()
             .asSuccess()
 
-    private fun getQualificationDocumentsIdsRequired(context: CamundaGlobalContext): Result<List<DocumentId>, Fail.Incident> =
+    private fun getQualificationReqRsDocumentsIdsRequired(context: CamundaGlobalContext): Result<List<DocumentId>, Fail.Incident> =
         context.getQualificationIfOnlyOne()
             .orForwardFail { fail -> return fail }
             .getRequirementResponseIfOnlyOne()
