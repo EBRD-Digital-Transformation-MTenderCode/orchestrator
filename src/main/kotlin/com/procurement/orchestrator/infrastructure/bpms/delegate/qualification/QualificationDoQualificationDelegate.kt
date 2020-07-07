1cb0c2e3ad9624b84d0a3d4cd7435f2531cf36a2
katherine <kate.shaidurova@gmail.com>
2020-07-07 13:16:22 +0300

    ES-3676 Replaced MaybeFail.none() with MaybeFail.fail() in case result is null for QualificationDoQualificationDelegate.kt

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/qualification/QualificationDoQualificationDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/qualification/QualificationDoQualificationDelegate.kt
@@ -23,7 +23,9 @@ import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExterna
 import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
 import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.qualification.QualificationCommands
 import com.procurement.orchestrator.infrastructure.client.web.qualification.action.DoQualificationAction
+import com.procurement.orchestrator.infrastructure.configuration.property.ExternalServiceName
 import org.springframework.stereotype.Component
 
 @Component
@@ -83,7 +85,12 @@ class QualificationDoQualificationDelegate(
     ): MaybeFail<Fail.Incident> {
 
         val data = result.orNull
-            ?: return MaybeFail.none()
+            ?: return MaybeFail.fail(
+                Fail.Incident.Response.Empty(
+                    service = ExternalServiceName.QUALIFICATION,
+                    action = QualificationCommands.CreateQualification
+                )
+            )
 
         val qualifications = data.qualifications
             ?.map { qualification ->
