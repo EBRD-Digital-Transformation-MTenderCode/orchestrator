6656e3ba9865a9330fc8041a58917599cc64d06d
yevhenii <yevheniiskoryi@gmail.com>
2020-07-09 11:37:10 +0300

    ES-3733 created delegate QualificationAnalizeQualificationForInvitationDelegate

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/qualification/QualificationAnalizeQualificationForInvitationDelegate.kt
@@ -0,0 +1,50 @@
+package com.procurement.orchestrator.infrastructure.bpms.delegate.qualification
+
+import com.procurement.orchestrator.application.CommandId
+import com.procurement.orchestrator.application.client.QualificationClient
+import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
+import com.procurement.orchestrator.application.service.Logger
+import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
+import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
+import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
+import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.qualification.action.AnalizeQualificationForInvitationAction
+import org.springframework.stereotype.Component
+
+@Component
+class QualificationAnalizeQualificationForInvitationDelegate(
+    logger: Logger,
+    private val client: QualificationClient,
+    operationStepRepository: OperationStepRepository,
+    transform: Transform
+) : AbstractExternalDelegate<Unit, Unit>(
+    logger = logger,
+    transform = transform,
+    operationStepRepository = operationStepRepository
+) {
+    override fun parameters(parameterContainer: ParameterContainer): Result<Unit, Fail.Incident.Bpmn.Parameter> =
+        Result.success(Unit)
+
+    override suspend fun execute(
+        commandId: CommandId,
+        context: CamundaGlobalContext,
+        parameters: Unit
+    ): Result<Reply<Unit>, Fail.Incident> {
+
+        val processInfo = context.processInfo
+        val requestInfo = context.requestInfo
+
+        return client.analizeQualificationForInvitation(
+            id = commandId,
+            params = AnalizeQualificationForInvitationAction.Params(
+                cpid = processInfo.cpid,
+                ocid = processInfo.ocid,
+                country = requestInfo.country,
+                pmd =processInfo.pmd
+            )
+        )
+    }
+}
