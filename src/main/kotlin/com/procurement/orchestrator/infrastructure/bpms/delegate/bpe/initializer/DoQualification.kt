07a52a87ad42f69fbdfd21b810557fa4ad0756e4
Evgen Kopach <zkopach@gmail.com>
2020-07-09 16:30:46 +0300

    ES-3737 Put qualification attributes to 'qualification' object in platform request dto

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/bpe/initializer/DoQualification.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/bpe/initializer/DoQualification.kt
@@ -8,19 +8,20 @@ object DoQualification {
     class Request {
 
         class Payload(
-
-            @field:JsonProperty("statusDetails") @param:JsonProperty("statusDetails") val statusDetails: String?,
-
-            @JsonInclude(JsonInclude.Include.NON_NULL)
-            @field:JsonProperty("internalId") @param:JsonProperty("internalId") val internalId: String?,
-
-            @JsonInclude(JsonInclude.Include.NON_NULL)
-            @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
-
-            @JsonInclude(JsonInclude.Include.NON_EMPTY)
-            @field:JsonProperty("documents") @param:JsonProperty("documents") val documents: List<Document> = emptyList()
-
+            @field:JsonProperty("qualification") @param:JsonProperty("qualification") val qualification: Qualification
         ) {
+            data class Qualification(
+                @field:JsonProperty("statusDetails") @param:JsonProperty("statusDetails") val statusDetails: String,
+
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @field:JsonProperty("internalId") @param:JsonProperty("internalId") val internalId: String?,
+
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
+
+                @JsonInclude(JsonInclude.Include.NON_EMPTY)
+                @field:JsonProperty("documents") @param:JsonProperty("documents") val documents: List<Document> = emptyList()
+            )
 
             data class Document(
                 @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
@@ -28,7 +29,7 @@ object DoQualification {
                 @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
 
                 @JsonInclude(JsonInclude.Include.NON_NULL)
-                @field:JsonProperty("description") @param:JsonProperty("description") val description: String? = null
+                @field:JsonProperty("description") @param:JsonProperty("description") val description: String?
             )
         }
     }
