a463f87135f44284bdb22c4d7c61a5ad84326a8d
katherine <kate.shaidurova@gmail.com>
2020-07-15 16:44:15 +0300

    ES-3757 Added SubmissionCommands.kt

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/submission/SubmissionCommands.kt
@@ -0,0 +1,8 @@
+package com.procurement.orchestrator.infrastructure.client.web.submission
+
+import com.procurement.orchestrator.infrastructure.client.web.submission.action.DoInvitationsAction
+
+object SubmissionCommands {
+
+    object DoInvitations : DoInvitationsAction()
+}
