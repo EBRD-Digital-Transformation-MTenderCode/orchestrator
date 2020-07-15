3738cd04c7f59bdda2f4ea44f252842b1bc1165b
katherine <kate.shaidurova@gmail.com>
2020-07-15 16:51:51 +0300

    ES-3726 Removed redundant updateGlobalContext override in QualificationCheckQualificationPeriodDelegate.kt

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/qualification/QualificationCheckQualificationPeriodDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/qualification/QualificationCheckQualificationPeriodDelegate.kt
@@ -6,8 +6,6 @@ import com.procurement.orchestrator.application.model.context.CamundaGlobalConte
 import com.procurement.orchestrator.application.service.Logger
 import com.procurement.orchestrator.application.service.Transform
 import com.procurement.orchestrator.domain.fail.Fail
-import com.procurement.orchestrator.domain.functional.MaybeFail
-import com.procurement.orchestrator.domain.functional.Option
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.domain.functional.Result.Companion.success
 import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
@@ -49,10 +47,4 @@ class QualificationCheckQualificationPeriodDelegate(
             )
         )
     }
-
-    override fun updateGlobalContext(
-        context: CamundaGlobalContext,
-        parameters: Unit,
-        result: Option<Unit>
-    ): MaybeFail<Fail.Incident> = MaybeFail.none()
 }
