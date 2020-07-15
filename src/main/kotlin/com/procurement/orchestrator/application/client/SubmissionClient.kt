5ae8053231dcefce4cd3bc9214f39463ec6d5b1c
katherine <kate.shaidurova@gmail.com>
2020-07-15 16:44:05 +0300

    ES-3757 Added SubmissionClient.kt and HttpSubmissionClient.kt

---
+++ src/main/kotlin/com/procurement/orchestrator/application/client/SubmissionClient.kt
@@ -0,0 +1,15 @@
+package com.procurement.orchestrator.application.client
+
+import com.procurement.orchestrator.application.CommandId
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.submission.action.DoInvitationsAction
+
+interface SubmissionClient {
+
+    suspend fun doInvitations(
+        id: CommandId,
+        params: DoInvitationsAction.Params
+    ): Result<Reply<DoInvitationsAction.Result>, Fail.Incident>
+}
