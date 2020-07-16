c4e0e9c6b7eecea2c2520bdf8131496ace4ca528
katherine <kate.shaidurova@gmail.com>
2020-07-16 16:16:44 +0300

    ES-3795 Moved ValidateTenderPeriodAction.kt to dossier package

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/dossier/action/ValidateTenderPeriodAction.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/dossier/action/ValidateTenderPeriodAction.kt
@@ -1,4 +1,4 @@
-package com.procurement.orchestrator.infrastructure.client.web.access.action
+package com.procurement.orchestrator.infrastructure.client.web.dossier.action
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
