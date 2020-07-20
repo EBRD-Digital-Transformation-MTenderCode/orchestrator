ac24b69ff5e17f720b7572329768ffb999ebdf18
katherine <kate.shaidurova@gmail.com>
2020-07-20 14:17:41 +0300

    ES-3797 Added SubmissionSetTenderPeriodDelegate.kt

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/submission/SubmissionSetTenderPeriodDelegate.kt
@@ -0,0 +1,92 @@
+package com.procurement.orchestrator.infrastructure.bpms.delegate.submission
+
+import com.procurement.orchestrator.application.CommandId
+import com.procurement.orchestrator.application.client.SubmissionClient
+import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
+import com.procurement.orchestrator.application.model.context.extension.tryGetTender
+import com.procurement.orchestrator.application.service.Logger
+import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.MaybeFail
+import com.procurement.orchestrator.domain.functional.Option
+import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.domain.model.period.Period
+import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
+import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
+import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
+import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.submission.SubmissionCommands
+import com.procurement.orchestrator.infrastructure.client.web.submission.action.SetTenderPeriodAction
+import com.procurement.orchestrator.infrastructure.configuration.property.ExternalServiceName
+import org.springframework.stereotype.Component
+
+@Component
+class SubmissionSetTenderPeriodDelegate(
+    logger: Logger,
+    private val submissionClient: SubmissionClient,
+    operationStepRepository: OperationStepRepository,
+    transform: Transform
+) : AbstractExternalDelegate<Unit, SetTenderPeriodAction.Result>(
+    logger = logger,
+    transform = transform,
+    operationStepRepository = operationStepRepository
+) {
+    override fun parameters(parameterContainer: ParameterContainer): Result<Unit, Fail.Incident.Bpmn.Parameter> =
+        Result.success(Unit)
+
+    override suspend fun execute(
+        commandId: CommandId,
+        context: CamundaGlobalContext,
+        parameters: Unit
+    ): Result<Reply<SetTenderPeriodAction.Result>, Fail.Incident> {
+
+        val processInfo = context.processInfo
+        val requestInfo = context.requestInfo
+
+        val endDate = context.tender?.tenderPeriod?.endDate
+
+
+        return submissionClient.setTenderPeriodAction(
+            id = commandId,
+            params = SetTenderPeriodAction.Params(
+                cpid = processInfo.cpid,
+                ocid = processInfo.ocid,
+                date = requestInfo.timestamp,
+                tender = SetTenderPeriodAction.Params.Tender(
+                    SetTenderPeriodAction.Params.Tender.TenderPeriod(endDate)
+                )
+            )
+        )
+    }
+
+    override fun updateGlobalContext(
+        context: CamundaGlobalContext,
+        parameters: Unit,
+        result: Option<SetTenderPeriodAction.Result>
+    ): MaybeFail<Fail.Incident> {
+
+        val data = result.orNull
+            ?: return MaybeFail.fail(
+                Fail.Incident.Response.Empty(
+                    service = ExternalServiceName.SUBMISSION,
+                    action = SubmissionCommands.SetTenderPeriod
+                )
+            )
+
+        val receivedTenderPeriod = data.tender.tenderPeriod
+
+        val tender = context.tryGetTender()
+            .orReturnFail { return MaybeFail.fail(it) }
+
+        val tenderUpdated = tender.copy(
+            tenderPeriod = Period(
+                startDate = receivedTenderPeriod.startDate,
+                endDate = receivedTenderPeriod.endDate
+            )
+        )
+
+        context.tender = tenderUpdated
+
+        return MaybeFail.none()
+    }
+}
