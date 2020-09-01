0e32806cd155d30e09de2d06a5462a4af9ba7d5d
katherine <kate.shaidurova@gmail.com>
2020-09-01 14:33:03 +0300

    ES-4109 Fixed RelatedProcess.kt

--- src/main/kotlin/com/procurement/orchestrator/domain/model/contract/RelatedProcess.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/contract/RelatedProcess.kt
@@ -8,36 +8,34 @@ import com.procurement.orchestrator.domain.model.or
 import java.io.Serializable
 
 data class RelatedProcess(
-    @field:JsonProperty("scheme") @param:JsonProperty("scheme") val scheme: RelatedProcessScheme,
     @field:JsonProperty("id") @param:JsonProperty("id") val id: RelatedProcessId,
 
     @field:JsonInclude(JsonInclude.Include.NON_EMPTY)
     @field:JsonProperty("relationship") @param:JsonProperty("relationship") val relationship: RelatedProcessTypes = RelatedProcessTypes(),
 
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("scheme") @param:JsonProperty("scheme") val scheme: RelatedProcessScheme? = null,
+
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
     @field:JsonProperty("identifier") @param:JsonProperty("identifier") val identifier: RelatedProcessIdentifier? = null,
 
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
     @field:JsonProperty("uri") @param:JsonProperty("uri") val uri: String? = null
+
 ) : IdentifiableObject<RelatedProcess>, Serializable {
 
     override fun equals(other: Any?): Boolean = if (this === other)
         true
     else
         other is RelatedProcess
-            && this.scheme == other.scheme
             && this.id == other.id
 
-    override fun hashCode(): Int {
-        var result = scheme.hashCode()
-        result = 31 * result + id.hashCode()
-        return result
-    }
+    override fun hashCode(): Int = id.hashCode()
 
     override fun updateBy(src: RelatedProcess) = RelatedProcess(
-        scheme = scheme,
         id = id,
         relationship = relationship combineBy src.relationship,
+        scheme = src.scheme or scheme,
         identifier = src.identifier or identifier,
         uri = src.uri or uri
     )
