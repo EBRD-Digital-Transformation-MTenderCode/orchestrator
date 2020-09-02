6e45291fdbedc38a5a3c8ae96ac0af00ad93f87d
katherine <kate.shaidurova@gmail.com>
2020-09-02 17:12:59 +0300

    ES-4129 Added outsourcingPn() to AccessClient.kt

--- src/main/kotlin/com/procurement/orchestrator/application/client/AccessClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/client/AccessClient.kt
@@ -15,6 +15,7 @@ import com.procurement.orchestrator.infrastructure.client.web.access.action.GetL
 import com.procurement.orchestrator.infrastructure.client.web.access.action.GetOrganizationAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.GetQualificationCriteriaAndMethodAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.GetTenderStateAction
+import com.procurement.orchestrator.infrastructure.client.web.access.action.OutsourcingPnAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.ResponderProcessingAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.SetStateForLotsAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.SetStateForTenderAction
@@ -103,4 +104,9 @@ interface AccessClient {
         id: CommandId,
         params: FindAuctionsAction.Params
     ): Result<Reply<FindAuctionsAction.Result>, Fail.Incident>
+
+    suspend fun outsourcingPn(
+        id: CommandId,
+        params: OutsourcingPnAction.Params
+    ): Result<Reply<OutsourcingPnAction.Result>, Fail.Incident>
 }
--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/HttpAccessClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/HttpAccessClient.kt
@@ -17,6 +17,7 @@ import com.procurement.orchestrator.infrastructure.client.web.access.action.GetL
 import com.procurement.orchestrator.infrastructure.client.web.access.action.GetOrganizationAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.GetQualificationCriteriaAndMethodAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.GetTenderStateAction
+import com.procurement.orchestrator.infrastructure.client.web.access.action.OutsourcingPnAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.ResponderProcessingAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.SetStateForLotsAction
 import com.procurement.orchestrator.infrastructure.client.web.access.action.SetStateForTenderAction
@@ -168,5 +169,14 @@ class HttpAccessClient(private val webClient: WebClient, properties: ComponentPr
         url = url,
         command = AccessCommands.FindAuctions.build(id = id, params = params),
         target = AccessCommands.FindAuctions.target
+    )
+
+    override suspend fun outsourcingPn(
+        id: CommandId,
+        params: OutsourcingPnAction.Params
+    ): Result<Reply<OutsourcingPnAction.Result>, Fail.Incident> = webClient.call(
+        url = url,
+        command = AccessCommands.OutsourcingPn.build(id = id, params = params),
+        target = AccessCommands.OutsourcingPn.target
     )
 }
