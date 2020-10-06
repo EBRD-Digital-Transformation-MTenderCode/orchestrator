201516577c7f5b8292250993b672c8b88c49ccb5
katherine <kate.shaidurova@gmail.com>
2020-10-06 16:54:44 +0300

    ES-4407 Added delegate

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/access/AccessCalculateApValue.kt
@@ -0,0 +1,86 @@
+package com.procurement.orchestrator.infrastructure.bpms.delegate.access
+
+import com.procurement.orchestrator.application.CommandId
+import com.procurement.orchestrator.application.client.AccessClient
+import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
+import com.procurement.orchestrator.application.service.Logger
+import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.MaybeFail
+import com.procurement.orchestrator.domain.functional.Option
+import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.domain.functional.Result.Companion.success
+import com.procurement.orchestrator.domain.model.tender.Tender
+import com.procurement.orchestrator.domain.model.value.Value
+import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
+import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
+import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
+import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.access.AccessCommands
+import com.procurement.orchestrator.infrastructure.client.web.access.action.CalculateApValueAction
+import com.procurement.orchestrator.infrastructure.configuration.property.ExternalServiceName
+import org.springframework.stereotype.Component
+
+@Component
+class AccessCalculateApValue(
+    logger: Logger,
+    private val accessClient: AccessClient,
+    operationStepRepository: OperationStepRepository,
+    transform: Transform
+) : AbstractExternalDelegate<Unit, CalculateApValueAction.Result>(
+    logger = logger,
+    transform = transform,
+    operationStepRepository = operationStepRepository
+) {
+
+    override fun parameters(parameterContainer: ParameterContainer): Result<Unit, Fail.Incident.Bpmn.Parameter> =
+        success(Unit)
+
+    override suspend fun execute(
+        commandId: CommandId,
+        context: CamundaGlobalContext,
+        parameters: Unit
+    ): Result<Reply<CalculateApValueAction.Result>, Fail.Incident> {
+        val processInfo = context.processInfo
+        return accessClient.calculateApValueAction(
+            id = commandId,
+            params = CalculateApValueAction.Params(
+                cpid = processInfo.cpid,
+                ocid = processInfo.ocid
+            )
+        )
+    }
+
+    override fun updateGlobalContext(
+        context: CamundaGlobalContext,
+        parameters: Unit,
+        result: Option<CalculateApValueAction.Result>
+    ): MaybeFail<Fail.Incident> {
+
+        val data = result.orNull
+            ?: return MaybeFail.fail(
+                Fail.Incident.Response.Empty(
+                    service = ExternalServiceName.ACCESS,
+                    action = AccessCommands.CalculateApValue
+                )
+            )
+
+        val receivedValue = data.tender.value
+            .let { value ->
+                Value(
+                    amount = value.amount,
+                    currency = value.currency
+                )
+            }
+        val tender = context.tender ?: Tender()
+
+        val updatedTender = tender.copy(
+            value = tender.value?.updateBy(receivedValue)
+                ?: receivedValue
+        )
+
+        context.tender = updatedTender
+
+        return MaybeFail.none()
+    }
+}
