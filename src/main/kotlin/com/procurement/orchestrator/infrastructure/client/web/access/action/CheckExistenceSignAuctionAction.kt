4b636447d61d839a48053afdb7b50c66dbec33fc
Evgen Kopach <zkopach@gmail.com>
2020-10-16 11:49:25 +0300

    ES-4469 Created params DTO for command 'checkExistenceSignAuction'.

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/action/CheckExistenceSignAuctionAction.kt
@@ -0,0 +1,23 @@
+package com.procurement.orchestrator.infrastructure.client.web.access.action
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.application.service.ProceduralAction
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.tender.ProcurementMethodModality
+import com.procurement.orchestrator.infrastructure.model.Version
+
+abstract class CheckExistenceSignAuctionAction : ProceduralAction<CheckExistenceSignAuctionAction.Params> {
+    override val version: Version = Version.parse("2.0.0")
+    override val name: String = "checkExistenceSignAuction"
+
+    class Params(
+        @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: Cpid,
+        @field:JsonProperty("ocid") @param:JsonProperty("ocid") var ocid: Ocid,
+        @field:JsonProperty("tender") @param:JsonProperty("tender") var tender: Tender
+    ) {
+        data class Tender(
+            @field:JsonProperty("procurementMethodModalities") @param:JsonProperty("procurementMethodModalities") val procurementMethodModalities: List<ProcurementMethodModality>
+        )
+    }
+}
