0e1e3e382a47b7a55e56bc2179467e5def333009
Evgen Kopach <zkopach@gmail.com>
2020-10-16 11:29:50 +0300

    ES-4468 Created delegate for 'checkTenderState' at related process tender.

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/access/AccessCheckRelatedTenderStateDelegate.kt
@@ -0,0 +1,53 @@
+package com.procurement.orchestrator.infrastructure.bpms.delegate.access
+
+import com.procurement.orchestrator.application.CommandId
+import com.procurement.orchestrator.application.client.AccessClient
+import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
+import com.procurement.orchestrator.application.service.Logger
+import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.domain.functional.Result.Companion.success
+import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
+import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
+import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
+import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.access.action.CheckTenderStateAction
+import org.springframework.stereotype.Component
+
+@Component
+class AccessCheckRelatedTenderStateDelegate(
+    logger: Logger,
+    private val accessClient: AccessClient,
+    operationStepRepository: OperationStepRepository,
+    transform: Transform
+) : AbstractExternalDelegate<Unit, Unit>(
+    logger = logger,
+    transform = transform,
+    operationStepRepository = operationStepRepository
+) {
+
+    override fun parameters(parameterContainer: ParameterContainer): Result<Unit, Fail.Incident.Bpmn.Parameter> =
+        success(Unit)
+
+    override suspend fun execute(
+        commandId: CommandId,
+        context: CamundaGlobalContext,
+        parameters: Unit
+    ): Result<Reply<Unit>, Fail.Incident> {
+        val processInfo = context.processInfo
+        val requestInfo = context.requestInfo
+        val relatedProcess = processInfo.relatedProcess!!
+
+        return accessClient.checkTenderState(
+            id = commandId,
+            params = CheckTenderStateAction.Params(
+                cpid = relatedProcess.cpid,
+                ocid = relatedProcess.ocid!!,
+                country = requestInfo.country,
+                pmd = processInfo.pmd,
+                operationType = processInfo.operationType
+            )
+        )
+    }
+}
