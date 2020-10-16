857db41882170f097f1db4a79171e7cf60941b29
katherine <kate.shaidurova@gmail.com>
2020-10-16 18:19:23 +0300

    ES-4493 Added PublishInvitationsInPcr to SubmissionCommands.kt

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/submission/SubmissionCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/submission/SubmissionCommands.kt
@@ -2,9 +2,10 @@ package com.procurement.orchestrator.infrastructure.client.web.submission
 
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.CheckAbsenceActiveInvitationsAction
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.DoInvitationsAction
-import com.procurement.orchestrator.infrastructure.client.web.submission.action.ValidateTenderPeriodAction
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.PublishInvitationsAction
+import com.procurement.orchestrator.infrastructure.client.web.submission.action.PublishInvitationsInPcrAction
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.SetTenderPeriodAction
+import com.procurement.orchestrator.infrastructure.client.web.submission.action.ValidateTenderPeriodAction
 
 object SubmissionCommands {
 
@@ -17,4 +18,6 @@ object SubmissionCommands {
     object ValidateTenderPeriod : ValidateTenderPeriodAction()
 
     object SetTenderPeriod: SetTenderPeriodAction()
+
+    object PublishInvitationsInPcr: PublishInvitationsInPcrAction()
 }
