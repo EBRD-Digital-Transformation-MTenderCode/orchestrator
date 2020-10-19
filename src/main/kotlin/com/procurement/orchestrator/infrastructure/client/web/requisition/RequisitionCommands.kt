00f34697c0a937d64ee47dab973a46e6dd0a2d48
Evgen Kopach <zkopach@gmail.com>
2020-10-19 18:35:56 +0300

    ES-4471 Created delegate for 'validatePcrData' command.

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/RequisitionCommands.kt
@@ -0,0 +1,8 @@
+package com.procurement.orchestrator.infrastructure.client.web.requisition
+
+import com.procurement.orchestrator.infrastructure.client.web.requisition.action.ValidatePcrDataAction
+
+object RequisitionCommands {
+
+    object ValidatePcrData : ValidatePcrDataAction()
+}
