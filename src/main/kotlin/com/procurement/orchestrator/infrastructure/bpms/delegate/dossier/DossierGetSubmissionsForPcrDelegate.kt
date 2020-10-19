2800fb74c80723df02ca5938c55f86b7cd95f750
katherine <kate.shaidurova@gmail.com>
2020-10-19 12:10:18 +0300

    ES-4497 Finished updateGlobalContext()

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/dossier/DossierGetSubmissionsForPcrDelegate.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/dossier/DossierGetSubmissionsForPcrDelegate.kt
@@ -10,19 +10,15 @@ import com.procurement.orchestrator.domain.functional.MaybeFail
 import com.procurement.orchestrator.domain.functional.Option
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.domain.functional.Result.Companion.success
-import com.procurement.orchestrator.domain.model.address.Address
-import com.procurement.orchestrator.domain.model.address.AddressDetails
-import com.procurement.orchestrator.domain.model.address.country.CountryDetails
-import com.procurement.orchestrator.domain.model.address.locality.LocalityDetails
-import com.procurement.orchestrator.domain.model.address.region.RegionDetails
-import com.procurement.orchestrator.domain.model.identifier.Identifier
-import com.procurement.orchestrator.domain.model.identifier.Identifiers
-import com.procurement.orchestrator.domain.model.party.Party
+import com.procurement.orchestrator.domain.model.party.Parties
+import com.procurement.orchestrator.domain.model.party.PartyRole
+import com.procurement.orchestrator.domain.model.party.PartyRoles
 import com.procurement.orchestrator.infrastructure.bpms.delegate.AbstractExternalDelegate
 import com.procurement.orchestrator.infrastructure.bpms.delegate.ParameterContainer
 import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.FindSubmissionsForOpeningAction
+import com.procurement.orchestrator.infrastructure.client.web.dossier.action.convertToParty
 import org.springframework.stereotype.Component
 
 @Component
@@ -69,65 +65,12 @@ class DossierGetSubmissionsForPcrDelegate(
         val data = result.orNull
             ?: return MaybeFail.none()
 
-        val parties = data.map { submission ->
-            submission.candidates.map { candidate ->
-                Party(
-                    id = candidate.id,
-                    name = candidate.name,
-                    additionalIdentifiers = candidate.additionalIdentifiers
-                        ?.map { additionalIdentifier ->
-                            Identifier(
-                                id = additionalIdentifier.id,
-                                scheme = additionalIdentifier.scheme,
-                                legalName = additionalIdentifier.legalName,
-                                uri = additionalIdentifier.uri
-                            )
+        val parties = data.flatMap { submission -> submission.candidates }
+            .map { it.convertToParty() }
+            .map { it.copy(roles = PartyRoles(PartyRole.INVITED_TENDERER)) }
+            .let { Parties(it) }
 
-                        }.orEmpty()
-                        .let { Identifiers(it) },
-                    address = candidate.address
-                        .let { address ->
-                            Address(
-                                streetAddress = address.streetAddress,
-                                postalCode = address.postalCode,
-                                addressDetails = address.addressDetails
-                                    .let { addressDetails ->
-                                        AddressDetails(
-                                            country = addressDetails.country
-                                                .let { country ->
-                                                    CountryDetails(
-                                                        id = country.id,
-                                                        description = country.description,
-                                                        scheme = country.scheme,
-                                                        uri = country.uri
-                                                    )
-                                                },
-                                            region = addressDetails.region
-                                                .let { region ->
-                                                    RegionDetails(
-                                                        id = region.id,
-                                                        description = region.description,
-                                                        scheme = region.scheme,
-                                                        uri = region.uri
-                                                    )
-                                                },
-                                            locality = addressDetails.locality
-                                                .let { locality ->
-                                                    LocalityDetails(
-                                                        id = locality.id,
-                                                        description = locality.description,
-                                                        scheme = locality.scheme,
-                                                        uri = locality.uri
-                                                    )
-                                                }
-                                        )
-                                    }
-                            )
-                        },
-                    contactPoint = 
-                    )
-            }
-        }
+        context.parties = context.parties updateBy parties
 
         return MaybeFail.none()
     }
