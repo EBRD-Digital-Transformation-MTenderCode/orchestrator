6f493218d83b12a3e160d7124f46ee73743b58d9
Evgen Kopach <zkopach@gmail.com>
2020-10-19 15:10:24 +0300

    ES-4483 Added extension for tender to get non empty items.

--- src/main/kotlin/com/procurement/orchestrator/application/model/context/extension/TenderExtension.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/model/context/extension/TenderExtension.kt
@@ -8,6 +8,7 @@ import com.procurement.orchestrator.domain.functional.asSuccess
 import com.procurement.orchestrator.domain.model.amendment.Amendment
 import com.procurement.orchestrator.domain.model.document.Document
 import com.procurement.orchestrator.domain.model.enquiry.Enquiry
+import com.procurement.orchestrator.domain.model.item.Item
 import com.procurement.orchestrator.domain.model.lot.Lot
 import com.procurement.orchestrator.domain.model.tender.Tender
 
@@ -29,6 +30,9 @@ fun Tender.getLotIfOnlyOne(): Result<Lot, Fail.Incident.Bpms.Context> =
 
 fun Tender.getLotsIfNotEmpty(): Result<List<Lot>, Fail.Incident.Bpms.Context> =
     this.lots.getIfNotEmpty(name = "lots", path = PATH)
+
+fun Tender.getItemsIfNotEmpty(): Result<List<Item>, Fail.Incident.Bpms.Context> =
+    this.items.getIfNotEmpty(name = "items", path = PATH)
 
 fun Tender.getDocumentsIfNotEmpty(): Result<List<Document>, Fail.Incident.Bpms.Context> =
     this.documents.getIfNotEmpty(name = "documents", path = PATH)
