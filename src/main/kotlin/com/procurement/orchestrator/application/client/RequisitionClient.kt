46ce357c48e0f958e18192e862d4d553b3528e69
katherine <kate.shaidurova@gmail.com>
2020-10-20 12:17:29 +0300

    ES-4508 Added getTenderState() to RequisitionClient.kt

--- src/main/kotlin/com/procurement/orchestrator/application/client/RequisitionClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/client/RequisitionClient.kt
@@ -4,6 +4,7 @@ import com.procurement.orchestrator.application.CommandId
 import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
+import com.procurement.orchestrator.infrastructure.client.web.requisition.action.GetTenderStateAction
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.ValidatePcrDataAction
 
 interface RequisitionClient {
@@ -12,4 +13,9 @@ interface RequisitionClient {
         id: CommandId,
         params: ValidatePcrDataAction.Params
     ): Result<Reply<Unit>, Fail.Incident>
+
+    suspend fun getTenderState(
+        id: CommandId,
+        params: GetTenderStateAction.Params
+    ): Result<Reply<GetTenderStateAction.Result>, Fail.Incident>
 }
