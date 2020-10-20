05ca0bd356a475891be48a4320647b75a526bea8
katherine <kate.shaidurova@gmail.com>
2020-10-20 12:56:11 +0300

    ES-4510 Added CreateRelationToContractProcessStage to RequisitionCommands.kt

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/RequisitionCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/RequisitionCommands.kt
@@ -1,8 +1,11 @@
 package com.procurement.orchestrator.infrastructure.client.web.requisition
 
+import com.procurement.orchestrator.infrastructure.client.web.requisition.action.CreateRelationToContractProcessStageAction
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.ValidatePcrDataAction
 
 object RequisitionCommands {
 
     object ValidatePcrData : ValidatePcrDataAction()
+
+    object CreateRelationToContractProcessStage : CreateRelationToContractProcessStageAction()
 }
