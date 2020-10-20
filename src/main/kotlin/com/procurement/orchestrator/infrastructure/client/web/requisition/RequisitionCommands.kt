783a9234f9e28a668864a6c4524ddf2a17690871
Evgen Kopach <zkopach@gmail.com>
2020-10-20 18:34:22 +0300

    ES-4491 Created delegate for 'createPcr' command.

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/RequisitionCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/RequisitionCommands.kt
@@ -1,8 +1,11 @@
 package com.procurement.orchestrator.infrastructure.client.web.requisition
 
+import com.procurement.orchestrator.infrastructure.client.web.requisition.action.CreatePcrAction
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.ValidatePcrDataAction
 
 object RequisitionCommands {
 
     object ValidatePcrData : ValidatePcrDataAction()
+
+    object CreatePcr : CreatePcrAction()
 }
