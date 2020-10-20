88668da0203d74af3974bd8fcf85dc648b5fc820
katherine <kate.shaidurova@gmail.com>
2020-10-20 16:05:26 +0300

    ES-4506 Added save() by ocid

--- src/main/kotlin/com/procurement/orchestrator/application/repository/OldProcessContextRepository.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/repository/OldProcessContextRepository.kt
@@ -3,10 +3,13 @@ package com.procurement.orchestrator.application.repository
 import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
 
 interface OldProcessContextRepository {
 
     fun save(cpid: Cpid, context: String): Result<Boolean, Fail.Incident.Database.Access>
+
+    fun save(ocid: Ocid, context: String): Result<Boolean, Fail.Incident.Database.Access>
 
     fun load(cpid: Cpid): Result<String?, Fail.Incident.Database>
 }
