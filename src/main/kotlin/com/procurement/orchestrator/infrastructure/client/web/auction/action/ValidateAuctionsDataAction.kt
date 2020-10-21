683589103a4805f787047d680cb9e73823de7445
katherine <kate.shaidurova@gmail.com>
2020-10-21 14:16:29 +0300

    ES-4479 Added ValidateAuctionsDataAction.kt

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/auction/action/ValidateAuctionsDataAction.kt
@@ -0,0 +1,54 @@
+package com.procurement.orchestrator.infrastructure.client.web.auction.action
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.application.service.ProceduralAction
+import com.procurement.orchestrator.domain.model.lot.LotId
+import com.procurement.orchestrator.infrastructure.model.Version
+
+abstract class ValidateAuctionsDataAction : ProceduralAction<ValidateAuctionsDataAction.Params> {
+    override val version: Version = Version.parse("2.0.0")
+    override val name: String = "validateAuctionsData"
+
+    class Params(
+        @param:JsonProperty("tender") @field:JsonProperty("tender") val tender: Tender
+    ) {
+        data class Tender(
+            @param:JsonProperty("electronicAuctions") @field:JsonProperty("electronicAuctions") val electronicAuctions: ElectronicAuctions,
+            @param:JsonProperty("lots") @field:JsonProperty("lots") val lots: List<Lot>,
+            @param:JsonProperty("value") @field:JsonProperty("value") val value: Value
+        ) {
+            data class ElectronicAuctions(
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @param:JsonProperty("details") @field:JsonProperty("details") val details: List<Detail>?
+            ) {
+                data class Detail(
+                    @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+
+                    @JsonInclude(JsonInclude.Include.NON_NULL)
+                    @param:JsonProperty("relatedLot") @field:JsonProperty("relatedLot") val relatedLot: LotId?,
+
+                    @param:JsonProperty("electronicAuctionModalities") @field:JsonProperty("electronicAuctionModalities") val electronicAuctionModalities: List<ElectronicAuctionModality>
+                ) {
+                    data class ElectronicAuctionModality(
+                        @param:JsonProperty("eligibleMinimumDifference") @field:JsonProperty("eligibleMinimumDifference") val eligibleMinimumDifference: EligibleMinimumDifference
+                    ) {
+                        data class EligibleMinimumDifference(
+                            @JsonInclude(JsonInclude.Include.NON_NULL)
+                            @param:JsonProperty("currency") @field:JsonProperty("currency") val currency: String?
+                        )
+                    }
+                }
+            }
+
+            data class Lot(
+                @param:JsonProperty("id") @field:JsonProperty("id") val id: LotId
+            )
+
+            data class Value(
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @param:JsonProperty("currency") @field:JsonProperty("currency") val currency: String?
+            )
+        }
+    }
+}
