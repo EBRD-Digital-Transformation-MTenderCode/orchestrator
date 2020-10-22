614d5f35477ee38eb83967e6be94bf3e26f0e234
katherine <kate.shaidurova@gmail.com>
2020-10-22 16:01:40 +0300

    ES-4522 Added CheckPeriod to commands

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/submission/SubmissionCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/submission/SubmissionCommands.kt
@@ -1,10 +1,11 @@
 package com.procurement.orchestrator.infrastructure.client.web.submission
 
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.CheckAbsenceActiveInvitationsAction
+import com.procurement.orchestrator.infrastructure.client.web.submission.action.CheckPeriodAction
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.DoInvitationsAction
-import com.procurement.orchestrator.infrastructure.client.web.submission.action.ValidateTenderPeriodAction
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.PublishInvitationsAction
 import com.procurement.orchestrator.infrastructure.client.web.submission.action.SetTenderPeriodAction
+import com.procurement.orchestrator.infrastructure.client.web.submission.action.ValidateTenderPeriodAction
 
 object SubmissionCommands {
 
@@ -17,4 +18,6 @@ object SubmissionCommands {
     object ValidateTenderPeriod : ValidateTenderPeriodAction()
 
     object SetTenderPeriod: SetTenderPeriodAction()
+
+    object CheckPeriod : CheckPeriodAction()
 }
