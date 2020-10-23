c98cd9ab174e03fc28ed9323e99f59f21f794a1e
Evgen Kopach <zkopach@gmail.com>
2020-10-23 16:20:07 +0300

    ES-4562 Created delegate RequisitionSetLotsUnsuccessful

--- src/main/java/com/procurement/orchestrator/domain/commands/RequisitionCommandType.java
+++ src/main/java/com/procurement/orchestrator/domain/commands/RequisitionCommandType.java
@@ -10,7 +10,8 @@ import java.util.Map;
 
 public enum RequisitionCommandType {
 
-    GET_ACTIVE_LOTS("getActiveLots");
+    GET_ACTIVE_LOTS("getActiveLots"),
+    SET_LOTS_UNSUCCESSFUL("setLotsStatusUnsuccessful");
 
     private static final Map<String, RequisitionCommandType> CONSTANTS = new HashMap<>();
     private final String value;
