a7ddebf05503bb7b7682e503b60ad60eb0e2cf8a
katherine <kate.shaidurova@gmail.com>
2020-10-24 14:45:37 +0300

    ES-4532 Added GetCurrency to commands

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/RequisitionCommands.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/RequisitionCommands.kt
@@ -1,11 +1,14 @@
 package com.procurement.orchestrator.infrastructure.client.web.requisition
 
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.CheckTenderStateAction
+import com.procurement.orchestrator.infrastructure.client.web.requisition.action.GetCurrencyAction
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.ValidatePcrDataAction
 
 object RequisitionCommands {
 
     object ValidatePcrData : ValidatePcrDataAction()
 
-    object CheckTenderState: CheckTenderStateAction()
+    object CheckTenderState : CheckTenderStateAction()
+
+    object GetCurrency : GetCurrencyAction()
 }
