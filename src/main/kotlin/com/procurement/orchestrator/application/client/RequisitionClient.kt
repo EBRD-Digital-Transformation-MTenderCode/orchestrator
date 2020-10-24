7e2ca9811c610daa0c9e2a46592e02e0316a3d6c
katherine <kate.shaidurova@gmail.com>
2020-10-24 14:45:46 +0300

    ES-4532 Added GetCurrency to RequisitionClient.kt

--- src/main/kotlin/com/procurement/orchestrator/application/client/RequisitionClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/client/RequisitionClient.kt
@@ -5,6 +5,7 @@ import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.CheckTenderStateAction
+import com.procurement.orchestrator.infrastructure.client.web.requisition.action.GetCurrencyAction
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.ValidatePcrDataAction
 
 interface RequisitionClient {
@@ -18,4 +19,9 @@ interface RequisitionClient {
         id: CommandId,
         params: CheckTenderStateAction.Params
     ): Result<Reply<Unit>, Fail.Incident>
+
+    suspend fun getCurrency(
+        id: CommandId,
+        params: GetCurrencyAction.Params
+    ): Result<Reply<GetCurrencyAction.Result>, Fail.Incident>
 }
--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/HttpRequisitionClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/HttpRequisitionClient.kt
@@ -7,6 +7,7 @@ import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
 import com.procurement.orchestrator.infrastructure.client.web.WebClient
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.CheckTenderStateAction
+import com.procurement.orchestrator.infrastructure.client.web.requisition.action.GetCurrencyAction
 import com.procurement.orchestrator.infrastructure.client.web.requisition.action.ValidatePcrDataAction
 import com.procurement.orchestrator.infrastructure.configuration.property.ComponentProperties
 import java.net.URL
@@ -30,5 +31,14 @@ class HttpRequisitionClient(private val webClient: WebClient, properties: Compon
     ): Result<Reply<Unit>, Fail.Incident> = webClient.call(
         url = url,
         command = RequisitionCommands.CheckTenderState.build(id = id, params = params)
+    )
+
+    override suspend fun getCurrency(
+        id: CommandId,
+        params: GetCurrencyAction.Params
+    ): Result<Reply<GetCurrencyAction.Result>, Fail.Incident> = webClient.call(
+        url = url,
+        command = RequisitionCommands.GetCurrency.build(id = id, params = params),
+        target = RequisitionCommands.GetCurrency.target
     )
 }
