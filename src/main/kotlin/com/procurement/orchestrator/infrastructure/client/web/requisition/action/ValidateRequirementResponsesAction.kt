868a8245123e7970ad27fcdad8fc685ba6d509b4
katherine <kate.shaidurova@gmail.com>
2020-10-24 17:51:49 +0300

    ES-4536 Added action

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/requisition/action/ValidateRequirementResponsesAction.kt
@@ -0,0 +1,67 @@
+package com.procurement.orchestrator.infrastructure.client.web.requisition.action
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.application.service.ProceduralAction
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.bid.BidId
+import com.procurement.orchestrator.domain.model.lot.LotId
+import com.procurement.orchestrator.domain.model.requirement.RequirementId
+import com.procurement.orchestrator.domain.model.requirement.RequirementResponseValue
+import com.procurement.orchestrator.domain.model.requirement.response.RequirementResponseId
+import com.procurement.orchestrator.infrastructure.model.Version
+import java.time.LocalDateTime
+
+abstract class ValidateRequirementResponsesAction : ProceduralAction<ValidateRequirementResponsesAction.Params> {
+    override val version: Version = Version.parse("2.0.0")
+    override val name: String = "validateRequirementResponses"
+
+    class Params(
+        @param:JsonProperty("cpid") @field:JsonProperty("cpid") val cpid: Cpid,
+        @param:JsonProperty("ocid") @field:JsonProperty("ocid") val ocid: Ocid,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        @param:JsonProperty("tender") @field:JsonProperty("tender") val tender: Tender?,
+
+        @param:JsonProperty("bids") @field:JsonProperty("bids") val bids: Bids
+    ) {
+        data class Tender(
+            @param:JsonProperty("requirementResponses") @field:JsonProperty("requirementResponses") val requirementResponses: List<RequirementResponse>
+        ) {
+            data class RequirementResponse(
+                @param:JsonProperty("id") @field:JsonProperty("id") val id: RequirementResponseId,
+
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @param:JsonProperty("value") @field:JsonProperty("value") val value: RequirementResponseValue?,
+
+                @param:JsonProperty("requirement") @field:JsonProperty("requirement") val requirement: Requirement,
+
+                @JsonInclude(JsonInclude.Include.NON_NULL)
+                @param:JsonProperty("period") @field:JsonProperty("period") val period: Period?
+            ) {
+                data class Requirement(
+                    @JsonInclude(JsonInclude.Include.NON_NULL)
+                    @param:JsonProperty("id") @field:JsonProperty("id") val id: RequirementId?
+                )
+
+                data class Period(
+                    @JsonInclude(JsonInclude.Include.NON_NULL)
+                    @param:JsonProperty("startDate") @field:JsonProperty("startDate") val startDate: LocalDateTime?,
+
+                    @JsonInclude(JsonInclude.Include.NON_NULL)
+                    @param:JsonProperty("endDate") @field:JsonProperty("endDate") val endDate: LocalDateTime?
+                )
+            }
+        }
+
+        data class Bids(
+            @param:JsonProperty("details") @field:JsonProperty("details") val details: List<Detail>
+        ) {
+            data class Detail(
+                @param:JsonProperty("id") @field:JsonProperty("id") val id: BidId,
+                @param:JsonProperty("relatedLots") @field:JsonProperty("relatedLots") val relatedLots: List<LotId>
+            )
+        }
+    }
+}
