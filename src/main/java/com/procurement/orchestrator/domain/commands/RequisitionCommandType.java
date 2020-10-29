4990ffc59bbae517c726d810103ba31a46ef13f1
Evgen Kopach <zkopach@gmail.com>
2020-10-29 11:39:29 +0200

    ES-4584 Resolved merge conflicts.

--- src/main/java/com/procurement/orchestrator/domain/commands/RequisitionCommandType.java
+++ src/main/java/com/procurement/orchestrator/domain/commands/RequisitionCommandType.java
@@ -11,9 +11,9 @@ import java.util.Map;
 public enum RequisitionCommandType {
 
     GET_ACTIVE_LOTS("getActiveLots"),
+    GET_LOTS_AUCTION("getLotsAuction"),
     SET_LOTS_UNSUCCESSFUL("setLotsStatusUnsuccessful"),
-    SET_TENDER_UNSUCCESSFUL("setTenderUnsuccessful"),
-    GET_LOTS_AUCTION("getLotsAuction");
+    SET_TENDER_UNSUCCESSFUL("setTenderUnsuccessful");
 
     private static final Map<String, RequisitionCommandType> CONSTANTS = new HashMap<>();
     private final String value;
