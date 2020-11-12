7e65e9618598626f0a2162437c767df1a787aaa7
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-11-12 16:18:37 +0200

    ES-4829 Added building context for command 'savePeriod' of 'Submission'.

--- src/main/java/com/procurement/orchestrator/delegate/submission/SubmissionSavePeriod.java
+++ src/main/java/com/procurement/orchestrator/delegate/submission/SubmissionSavePeriod.java
@@ -2,6 +2,7 @@ package com.procurement.orchestrator.delegate.submission;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
 import com.procurement.orchestrator.domain.entity.OperationStepEntity;
 import com.procurement.orchestrator.rest.SubmissionRestClient;
 import com.procurement.orchestrator.service.OperationService;
@@ -11,9 +12,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 import static com.procurement.orchestrator.domain.commands.SubmissionCommandType.SAVE_PERIOD;
 
@@ -48,20 +48,37 @@ public class SubmissionSavePeriod implements JavaDelegate {
         final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
         final String processId = execution.getProcessInstanceId();
         final String taskId = execution.getCurrentActivityId();
+
+        final Context commandContext = new Context.Builder()
+                .setCpid(context.getCpid())
+                .setOcid(getOcid(context))
+                .build();
+
         final JsonNode tenderPeriod = processService.getTenderPeriod(jsonData, processId);
-        final JsonNode commandMessage = processService.getCommandMessage(SAVE_PERIOD, context, tenderPeriod);
-        JsonNode responseData = processService.processResponse(
-                submissionRestClient.execute(commandMessage),
-                context,
-                processId,
-                taskId,
-                commandMessage);
-        if (Objects.nonNull(responseData)) {
-            operationService.saveOperationStep(
-                    execution,
-                    entity,
-                    commandMessage,
-                    jsonData);
+        final JsonNode commandMessage = processService.getCommandMessage(SAVE_PERIOD, commandContext, tenderPeriod);
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
         }
+
+        final ResponseEntity<ResponseDto> response = submissionRestClient.execute(commandMessage);
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+        }
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+        }
+
+        if (responseData != null) {
+            operationService.saveOperationStep(execution, entity, commandMessage, jsonData);
+        }
+    }
+
+    public String getOcid(final Context context) {
+        if (context.getOcidCn() != null)
+            return context.getOcidCn();
+        else
+            return context.getOcid();
     }
 }
