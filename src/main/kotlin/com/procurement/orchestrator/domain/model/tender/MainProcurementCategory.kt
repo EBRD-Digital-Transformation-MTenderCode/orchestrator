f6c2ae397c642d1d752fee99ce1a9a8e7c0edaf4
katherine <kate.shaidurova@gmail.com>
2020-11-12 14:13:50 +0200

    ES-4818 Added MainProcurementCategory to tender

---
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/tender/MainProcurementCategory.kt
@@ -0,0 +1,21 @@
+package com.procurement.orchestrator.domain.model.tender
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+import com.procurement.orchestrator.domain.EnumElementProvider
+
+enum class MainProcurementCategory(@JsonValue override val key: String) : EnumElementProvider.Key {
+
+    GOODS("goods"),
+    WORKS("works"),
+    SERVICES("services");
+
+    override fun toString(): String = key
+
+    companion object : EnumElementProvider<MainProcurementCategory>(info = info()) {
+
+        @JvmStatic
+        @JsonCreator
+        fun creator(name: String) = orThrow(name)
+    }
+}
