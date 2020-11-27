71bf03fe0e79882b2e6c8d39868cebc37e5a2d63
katherine <kate.shaidurova@gmail.com>
2020-11-27 15:04:10 +0200

    ES-4948 Added tests

--- src/test/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/action/AccessActionsTest.kt
+++ src/test/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/action/AccessActionsTest.kt
@@ -508,4 +508,17 @@ class AccessActionsTest {
             }
         }
     }
+
+
+    @Nested
+    inner class CheckLotsState {
+
+        @Nested
+        inner class Params {
+            @Test
+            fun fully() {
+                testingBindingAndMapping<CheckLotsStateAction.Params>("json/client/access/check_lots_state_params_full.json")
+            }
+        }
+    }
 }
