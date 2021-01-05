6230a30083dabc8fcffbe9cb77d65cded6fa521c
katherine <kate.shaidurova@gmail.com>
2021-01-05 13:33:21 +0200

    ES-5231 Fixed updateBy

--- src/main/kotlin/com/procurement/orchestrator/domain/model/mdm/ProcessMasterData.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/mdm/ProcessMasterData.kt
@@ -2,16 +2,16 @@ package com.procurement.orchestrator.domain.model.mdm
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.procurement.orchestrator.domain.model.IdentifiableObject
-import com.procurement.orchestrator.domain.model.or
+import com.procurement.orchestrator.domain.model.ComplexObject
+import com.procurement.orchestrator.domain.model.updateBy
 import java.io.Serializable
 
 data class ProcessMasterData(
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
     @field:JsonProperty("mdm") @param:JsonProperty("mdm") val mdm: Mdm?
-) : IdentifiableObject<ProcessMasterData>, Serializable {
+) : ComplexObject<ProcessMasterData>, Serializable {
 
     override fun updateBy(src: ProcessMasterData) = ProcessMasterData(
-        mdm = src.mdm or mdm
+        mdm = mdm updateBy src.mdm
     )
 }
