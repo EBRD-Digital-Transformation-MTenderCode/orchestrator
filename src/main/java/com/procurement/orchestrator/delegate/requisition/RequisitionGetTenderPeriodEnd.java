428e13dac45cd21fd38acc33a8356fc14476bef3
katherine <kate.shaidurova@gmail.com>
2021-01-05 13:23:10 +0200

    ES-5237 Added RequisitionGetTenderPeriodEnd

---
+++ src/main/java/com/procurement/orchestrator/delegate/requisition/RequisitionGetTenderPeriodEnd.java
@@ -0,0 +1,84 @@
+package com.procurement.orchestrator.delegate.requisition;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.procurement.orchestrator.domain.Context;
+import com.procurement.orchestrator.domain.dto.command.ResponseDto;
+import com.procurement.orchestrator.domain.entity.OperationStepEntity;
+import com.procurement.orchestrator.rest.RequisitionRestClient;
+import com.procurement.orchestrator.service.OperationService;
+import com.procurement.orchestrator.service.ProcessService;
+import com.procurement.orchestrator.utils.JsonUtil;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+import static com.procurement.orchestrator.domain.commands.RequisitionCommandType.GET_TENDER_PERIOD_END;
+
+@Component
+public class RequisitionGetTenderPeriodEnd implements JavaDelegate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RequisitionGetTenderPeriodEnd.class);
+
+    private final RequisitionRestClient requisitionClient;
+    private final OperationService operationService;
+    private final ProcessService processService;
+    private final JsonUtil jsonUtil;
+
+    public RequisitionGetTenderPeriodEnd(final RequisitionRestClient requisitionClient,
+                                         final OperationService operationService,
+                                         final ProcessService processService,
+                                         final JsonUtil jsonUtil) {
+        this.requisitionClient = requisitionClient;
+        this.operationService = operationService;
+        this.processService = processService;
+        this.jsonUtil = jsonUtil;
+    }
+
+    @Override
+    public void execute(final DelegateExecution execution) throws Exception {
+        LOG.info(execution.getCurrentActivityId());
+        final OperationStepEntity entity = operationService.getPreviousOperationStep(execution);
+        final Context context = jsonUtil.toObject(Context.class, entity.getContext());
+        final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
+        final String processId = execution.getProcessInstanceId();
+        final String taskId = execution.getCurrentActivityId();
+
+        final JsonNode commandMessage = processService.getCommandMessage(GET_TENDER_PERIOD_END, context, jsonUtil.empty());
+        if (LOG.isDebugEnabled())
+            LOG.debug("COMMAND ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(commandMessage));
+
+        final ResponseEntity<ResponseDto> response = requisitionClient.execute(commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE FROM SERVICE ({}): '{}'.", context.getOperationId(), jsonUtil.toJson(response.getBody()));
+
+        final JsonNode responseData = processService.processResponse(response, context, processId, taskId, commandMessage);
+        if (LOG.isDebugEnabled())
+            LOG.debug("RESPONSE AFTER PROCESSING ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(responseData));
+
+        if (Objects.nonNull(responseData)) {
+            final JsonNode step = addTenderPeriodEnd(jsonData, responseData, processId);
+            if (LOG.isDebugEnabled())
+                LOG.debug("STEP FOR SAVE ({}): '{}'.", context.getOperationId(), jsonUtil.toJsonOrEmpty(step));
+
+            operationService.saveOperationStep(execution, entity, context, commandMessage, step);
+        }
+    }
+
+    private JsonNode addTenderPeriodEnd(final JsonNode jsonData, final JsonNode responseData, final String processId) {
+        try {
+            JsonNode responseTenderPeriod = responseData.get("tender").get("tenderPeriod");
+            ((ObjectNode) jsonData.get("tender")).replace("tenderPeriod", responseTenderPeriod);
+            return jsonData;
+        } catch (Exception e) {
+            processService.terminateProcess(processId, e.getMessage());
+            return null;
+        }
+    }
+}
+
