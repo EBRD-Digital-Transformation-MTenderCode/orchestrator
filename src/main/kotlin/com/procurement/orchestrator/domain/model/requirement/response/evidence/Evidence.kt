2d7a6b65b83a01e3c54bbf5bc73c01aec0b4386c
katherine <kate.shaidurova@gmail.com>
2021-01-13 11:59:45 +0200

    ES-5296 Added Evidence

---
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/requirement/response/evidence/Evidence.kt
@@ -0,0 +1,27 @@
+package com.procurement.orchestrator.domain.model.requirement.response.evidence
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.domain.model.IdentifiableObject
+import com.procurement.orchestrator.domain.model.document.DocumentReference
+import com.procurement.orchestrator.domain.model.or
+import java.io.Serializable
+
+data class Evidence(
+    @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
+    @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("description") @param:JsonProperty("description") val description: String? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("relatedDocument") @param:JsonProperty("relatedDocument") val relatedDocument: DocumentReference? = null
+) : IdentifiableObject<Evidence>, Serializable {
+
+    override fun updateBy(src: Evidence) = Evidence(
+        id = id,
+        title = src.title,
+        description = src.description or description,
+        relatedDocument = src.relatedDocument or relatedDocument
+    )
+}
