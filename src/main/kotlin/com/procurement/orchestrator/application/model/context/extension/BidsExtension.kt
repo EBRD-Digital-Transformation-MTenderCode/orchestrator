5328a5edbaa61818c89e671c755f79e85a7fbe08
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-01-19 14:20:57 +0200

    ES-5347 Changed 'AccessCheckLotsState' delegate (added parameter 'location').

--- src/main/kotlin/com/procurement/orchestrator/application/model/context/extension/BidsExtension.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/model/context/extension/BidsExtension.kt
@@ -7,20 +7,24 @@ import com.procurement.orchestrator.domain.functional.asSuccess
 import com.procurement.orchestrator.domain.model.bid.Bid
 import com.procurement.orchestrator.domain.model.bid.Bids
 import com.procurement.orchestrator.domain.model.bid.BidsDetails
+import com.procurement.orchestrator.domain.model.lot.LotId
 
 private const val PATH = "#/bids"
 
 fun Bids?.getBidsDetailIfOnlyOne(): Result<Bid, Fail.Incident.Bpms.Context> =
     this?.details
-        ?.getElementIfOnlyOne(name = PATH)
-        ?: Result.failure(Fail.Incident.Bpms.Context.Missing(name = "bids"))
+            ?.getElementIfOnlyOne(name = PATH)
+            ?: Result.failure(Fail.Incident.Bpms.Context.Missing(name = PATH))
 
 fun Bids?.getBidsDetailIfNotEmpty(): Result<BidsDetails, Fail.Incident.Bpms.Context> =
     this?.details
-        ?.getIfNotEmpty(name = PATH)
-        ?: Result.failure(Fail.Incident.Bpms.Context.Missing(name = "details"))
+            ?.getIfNotEmpty(name = PATH)
+            ?: Result.failure(Fail.Incident.Bpms.Context.Missing(name = "$PATH/details"))
+
+fun Bid.getRelatedLotsIfOnlyOne(): Result<LotId, Fail.Incident.Bpms.Context> =
+    this.relatedLots.getElementIfOnlyOne(name = "$PATH/details/relatedLots")
 
 fun GlobalContext.tryGetBids(): Result<Bids, Fail.Incident.Bpms.Context> =
     this.bids
-        ?.asSuccess()
-        ?: Result.failure(Fail.Incident.Bpms.Context.Missing(name = "bids"))
+            ?.asSuccess()
+            ?: Result.failure(Fail.Incident.Bpms.Context.Missing(name = PATH))
