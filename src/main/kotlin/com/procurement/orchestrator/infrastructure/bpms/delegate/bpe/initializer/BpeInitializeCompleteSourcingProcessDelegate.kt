eab799e9fe24cc3840207b299e5862a9fa3094be
katherine <kate.shaidurova@gmail.com>
2021-01-27 15:17:43 +0200

    ES-5413 Added BpeInitializeCompleteSourcingProcessDelegate.kt

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/bpe/initializer/BpeInitializeCompleteSourcingProcessDelegate.kt
@@ -0,0 +1,34 @@
+package com.procurement.orchestrator.infrastructure.bpms.delegate.bpe.initializer
+
+import com.procurement.orchestrator.application.model.context.CamundaContext
+import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
+import com.procurement.orchestrator.application.repository.ProcessInitializerRepository
+import com.procurement.orchestrator.application.service.Logger
+import com.procurement.orchestrator.application.service.Transform
+import com.procurement.orchestrator.domain.fail.Fail
+import com.procurement.orchestrator.domain.functional.MaybeFail
+import com.procurement.orchestrator.domain.model.lot.Lot
+import com.procurement.orchestrator.domain.model.lot.LotId
+import com.procurement.orchestrator.domain.model.lot.Lots
+import com.procurement.orchestrator.domain.model.tender.Tender
+import com.procurement.orchestrator.infrastructure.bpms.repository.OperationStepRepository
+import org.springframework.stereotype.Component
+
+@Component
+class BpeInitializeCompleteSourcingProcessDelegate(
+    val logger: Logger,
+    transform: Transform,
+    operationStepRepository: OperationStepRepository,
+    processInitializerRepository: ProcessInitializerRepository
+) : AbstractInitializeProcessDelegate(logger, transform, operationStepRepository, processInitializerRepository) {
+
+    override fun updateGlobalContext(
+        camundaContext: CamundaContext,
+        globalContext: CamundaGlobalContext
+    ): MaybeFail<Fail> {
+        val lotId = LotId.create(camundaContext.request.id!!)
+        globalContext.tender = Tender(lots = Lots(Lot(id = lotId)))
+
+        return MaybeFail.none()
+    }
+}
