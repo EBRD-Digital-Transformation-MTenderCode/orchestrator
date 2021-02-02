bf234150fdd2b76646371e2e42a97907b5929cd3
Evgen Kopach <zkopach@gmail.com>
2021-02-02 11:26:48 +0200

    ES-5429 Added request and response DTO

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/contracting/action/AddSupplierReferencesInFCAction.kt
@@ -0,0 +1,46 @@
+package com.procurement.orchestrator.infrastructure.client.web.contracting.action
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.application.service.FunctionalAction
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.contract.ContractId
+import com.procurement.orchestrator.infrastructure.client.web.Target
+import com.procurement.orchestrator.infrastructure.model.Version
+import java.io.Serializable
+import java.time.LocalDateTime
+
+abstract class AddSupplierReferencesInFCAction : FunctionalAction<AddSupplierReferencesInFCAction.Params, AddSupplierReferencesInFCAction.Result> {
+
+    override val version: Version = Version.parse("2.0.0")
+    override val name: String = "addSupplierReferencesInFC"
+    override val target: Target<Result> = Target.single()
+
+    data class Params(
+        @param:JsonProperty("cpid") @field:JsonProperty("cpid") val cpid: Cpid,
+        @param:JsonProperty("ocid") @field:JsonProperty("ocid") val ocid: Ocid,
+        @param:JsonProperty("parties") @field:JsonProperty("parties") val parties: List<Party>
+    ) {
+        data class Party(
+            @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+            @param:JsonProperty("name") @field:JsonProperty("name") val name: String
+        )
+    }
+
+    data class Result(
+        @param:JsonProperty("id") @field:JsonProperty("id") val id: ContractId,
+        @param:JsonProperty("status") @field:JsonProperty("status") val status: String,
+        @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: String,
+        @param:JsonProperty("date") @field:JsonProperty("date") val date: LocalDateTime,
+        @param:JsonProperty("suppliers") @field:JsonProperty("suppliers") val suppliers: List<Supplier>,
+        @get:JsonProperty("isFrameworkOrDynamic") @param:JsonProperty("isFrameworkOrDynamic") val isFrameworkOrDynamic: Boolean
+    ) : Serializable {
+
+        data class Supplier(
+            @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+            @param:JsonProperty("name") @field:JsonProperty("name") val name: String
+        ) : Serializable
+    }
+}
+
+
