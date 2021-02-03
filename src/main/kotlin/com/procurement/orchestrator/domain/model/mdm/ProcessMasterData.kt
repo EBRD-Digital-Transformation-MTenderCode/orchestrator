25aff6d5fd63fcfeec6e9b57941978ffb9b09fec
Evgen Kopach <zkopach@gmail.com>
2021-02-03 13:01:22 +0200

    ES-5443 Fixed registrationSchemes attribute location

--- src/main/kotlin/com/procurement/orchestrator/domain/model/mdm/ProcessMasterData.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/mdm/ProcessMasterData.kt
@@ -3,20 +3,15 @@ package com.procurement.orchestrator.domain.model.mdm
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.orchestrator.domain.model.ComplexObject
-import com.procurement.orchestrator.domain.model.scheme.RegistrationSchemes
 import com.procurement.orchestrator.domain.model.updateBy
 import java.io.Serializable
 
 data class ProcessMasterData(
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:JsonProperty("mdm") @param:JsonProperty("mdm") val mdm: Mdm? = null,
-
-    @field:JsonInclude(JsonInclude.Include.NON_EMPTY)
-    @field:JsonProperty("registrationSchemes") @param:JsonProperty("registrationSchemes") val registrationSchemes: RegistrationSchemes = RegistrationSchemes()
+    @field:JsonProperty("mdm") @param:JsonProperty("mdm") val mdm: Mdm? = null
 ) : ComplexObject<ProcessMasterData>, Serializable {
 
     override fun updateBy(src: ProcessMasterData) = ProcessMasterData(
-        mdm = mdm updateBy src.mdm,
-        registrationSchemes = registrationSchemes combineBy src.registrationSchemes
+        mdm = mdm updateBy src.mdm
     )
 }
