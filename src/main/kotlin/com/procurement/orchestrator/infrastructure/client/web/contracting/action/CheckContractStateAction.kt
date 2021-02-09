dd3182eb813e097a4d98e04130cd6b75d9beda65
katherine <kate.shaidurova@gmail.com>
2021-02-09 13:37:54 +0200

    ES-5508 Added action

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/contracting/action/CheckContractStateAction.kt
@@ -0,0 +1,23 @@
+package com.procurement.orchestrator.infrastructure.client.web.contracting.action
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.application.model.process.OperationTypeProcess
+import com.procurement.orchestrator.application.service.ProceduralAction
+import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
+import com.procurement.orchestrator.domain.model.ProcurementMethodDetails
+import com.procurement.orchestrator.infrastructure.model.Version
+
+abstract class CheckContractStateAction : ProceduralAction<CheckContractStateAction.Params> {
+
+    override val version: Version = Version.parse("2.0.0")
+    override val name: String = "checkContractState"
+
+    data class Params(
+        @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: Cpid,
+        @field:JsonProperty("ocid") @param:JsonProperty("ocid") val ocid: Ocid,
+        @field:JsonProperty("pmd") @param:JsonProperty("pmd") val pmd: ProcurementMethodDetails,
+        @field:JsonProperty("country") @param:JsonProperty("country") val country: String,
+        @field:JsonProperty("operationType") @param:JsonProperty("operationType") val operationType: OperationTypeProcess
+    )
+}
