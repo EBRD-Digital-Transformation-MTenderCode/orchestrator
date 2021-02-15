f5f1660ca395af82004b3f01471c9b5160b751ef
katherine <kate.shaidurova@gmail.com>
2021-02-15 15:04:48 +0200

    ES-5574 Updated Renewal

--- src/main/kotlin/com/procurement/orchestrator/domain/model/lot/Renewal.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/lot/Renewal.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.orchestrator.domain.model.ComplexObject
 import com.procurement.orchestrator.domain.model.or
+import com.procurement.orchestrator.domain.model.period.Period
+import com.procurement.orchestrator.domain.model.updateBy
 import java.io.Serializable
 
 data class Renewal(
@@ -14,12 +16,28 @@ data class Renewal(
     @field:JsonProperty("maxNumber") @param:JsonProperty("maxNumber") val maxNumber: Int? = null,
 
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:JsonProperty("renewalConditions") @param:JsonProperty("renewalConditions") val renewalConditions: String? = null
+    @field:JsonProperty("renewalConditions") @param:JsonProperty("renewalConditions") val renewalConditions: String? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @param:JsonProperty("description") @field:JsonProperty("description") val description: String?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @param:JsonProperty("minimumRenewals") @field:JsonProperty("minimumRenewals") val minimumRenewals: Long?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @param:JsonProperty("maximumRenewals") @field:JsonProperty("maximumRenewals") val maximumRenewals: Long?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @param:JsonProperty("period") @field:JsonProperty("period") val period: Period?
 ) : ComplexObject<Renewal>, Serializable {
 
     override fun updateBy(src: Renewal) = Renewal(
         hasRenewals = src.hasRenewals or hasRenewals,
         maxNumber = src.maxNumber or maxNumber,
-        renewalConditions = src.renewalConditions or renewalConditions
+        renewalConditions = src.renewalConditions or renewalConditions,
+        period = period updateBy src.period,
+        description = src.description or description,
+        maximumRenewals = src.maximumRenewals or maximumRenewals,
+        minimumRenewals = src.minimumRenewals or minimumRenewals
     )
 }
