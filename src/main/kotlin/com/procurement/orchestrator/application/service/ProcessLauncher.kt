8004f26a5eb6d2354b4fc4fbc42b8959b3be9979
Evgen Kopach <zkopach@gmail.com>
2021-02-23 18:16:20 +0200

    ES-5627 Updated consumer for 'document-generator-out' topic. Added handling 'documentGenerated' event.

--- src/main/kotlin/com/procurement/orchestrator/application/service/ProcessLauncher.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/service/ProcessLauncher.kt
@@ -1,6 +1,8 @@
 package com.procurement.orchestrator.application.service
 
+import com.procurement.orchestrator.application.model.OperationId
 import com.procurement.orchestrator.application.model.RequestId
+import com.procurement.orchestrator.application.model.Stage
 import com.procurement.orchestrator.application.model.context.CamundaContext
 import com.procurement.orchestrator.application.model.context.CamundaGlobalContext
 import com.procurement.orchestrator.application.model.context.container.DefaultPropertyContainer
@@ -8,12 +10,16 @@ import com.procurement.orchestrator.application.model.context.members.ProcessInf
 import com.procurement.orchestrator.application.model.context.members.RequestInfo
 import com.procurement.orchestrator.application.model.process.LatestProcessContext
 import com.procurement.orchestrator.application.repository.RuleRepository
+import com.procurement.orchestrator.application.service.events.DocumentGeneratorEvent
+import com.procurement.orchestrator.application.service.events.DocumentGeneratorEvent.DocumentGenerated
 import com.procurement.orchestrator.domain.extension.date.nowDefaultUTC
 import com.procurement.orchestrator.domain.fail.Fail
 import com.procurement.orchestrator.domain.fail.error.BpeErrors
 import com.procurement.orchestrator.domain.fail.error.RequestErrors
 import com.procurement.orchestrator.domain.functional.MaybeFail
 import com.procurement.orchestrator.domain.functional.Result
+import com.procurement.orchestrator.domain.functional.Result.Companion.success
+import com.procurement.orchestrator.domain.functional.asFailure
 import com.procurement.orchestrator.domain.functional.asSuccess
 import com.procurement.orchestrator.infrastructure.bpms.repository.RequestRecord
 import com.procurement.orchestrator.infrastructure.bpms.repository.RequestRepository
@@ -22,6 +28,7 @@ interface ProcessLauncher {
     fun launchWithContextByCpid(request: PlatformRequest): MaybeFail<Fail>
     fun launchWithContextByOcid(request: PlatformRequest): MaybeFail<Fail>
     fun launch(event: ChronographEvent): MaybeFail<Fail>
+    fun launch(event: DocumentGeneratorEvent): MaybeFail<Fail>
 }
 
 class ProcessLauncherImpl(
@@ -200,6 +207,112 @@ class ProcessLauncherImpl(
 
         val variables: Map<String, Any> = propertyContainer.toMap()
         processService.launchProcess(processDefinitionKey, event.operationId, variables)
+        return MaybeFail.none()
+    }
+
+    override fun launch(event: DocumentGeneratorEvent): MaybeFail<Fail> {
+        when (event) {
+            is DocumentGenerated -> {
+                val prevProcessContext = loadContext(event).orReturnFail { return MaybeFail.fail(it) }
+                startAddingGeneratedDocument(event, prevProcessContext)
+            }
+        }
+
+        return MaybeFail.none()
+    }
+
+    private fun loadContext(event: DocumentGenerated): Result<LatestProcessContext, Fail> {
+        val latestProcessContext = when (event.ocid.stage) {
+            Stage.FE,
+            Stage.EV,
+            Stage.NP,
+            Stage.TP -> {
+                processService.getProcessContext(cpid = event.cpid)
+                    .orForwardFail { return it }
+                    ?: return Fail.Incident.Bpe(description = "The process context by cpid '${event.cpid}' does not found.").asFailure()
+            }
+
+            Stage.AC,
+            Stage.PC -> {
+                processService.getProcessContext(ocid = event.ocid)
+                    .orForwardFail { return it }
+                    ?: return Fail.Incident.Bpe(description = "The process context by ocid '${event.ocid}' does not found.").asFailure()
+            }
+
+            Stage.AP,
+            Stage.EI,
+            Stage.FS,
+            Stage.PN -> return Fail.Incident.Bpe(description = "Process not implemented for stage '${event.ocid.stage}'").asFailure()
+        }
+
+        return success(latestProcessContext)
+    }
+
+    private fun startAddingGeneratedDocument(event: DocumentGenerated, prevProcessContext: LatestProcessContext): MaybeFail<Fail> {
+        val operationId = OperationId.generate()
+        val countryId = prevProcessContext.country
+        val pmd = prevProcessContext.pmd
+        val processDefinitionKey = processService
+            .getProcessDefinitionKey(countryId = countryId, pmd = pmd, processName = event.processName)
+            .orReturnFail { return MaybeFail.fail(it) }
+        val prevStage = prevProcessContext.stage
+        val prevPhase = prevProcessContext.phase
+        val rule = ruleRepository
+            .load(
+                countryId = countryId,
+                pmd = pmd,
+                processDefinitionKey = processDefinitionKey,
+                stageFrom = prevStage,
+                phaseFrom = prevPhase
+            )
+            .orReturnFail { return MaybeFail.fail(it) }
+            ?: return MaybeFail.fail(
+                BpeErrors.ImpossibleOperation(
+                    description = "Operation by country: '$countryId', pmd: '$pmd', process definition key: '$processDefinitionKey', stage: '$prevStage', phase: '$prevPhase' is impossible."
+                )
+            )
+
+        val propertyContainer = DefaultPropertyContainer()
+        CamundaGlobalContext(propertyContainer)
+            .apply {
+                requestInfo = RequestInfo(
+                    country = countryId,
+                    language = prevProcessContext.language,
+                    timestamp = nowDefaultUTC(),
+                    operationId = operationId,
+                    requestId = RequestId.generate(),
+                    platformId = prevProcessContext.owner,
+                    owner = prevProcessContext.owner
+                )
+
+                processInfo = ProcessInfo(
+                    stage = rule.stageTo,
+                    prevStage = prevStage,
+                    processDefinitionKey = processDefinitionKey,
+                    operationType = rule.operationType,
+                    phase = rule.phaseTo,
+                    pmd = pmd,
+                    ocid = event.ocid,
+                    cpid = event.cpid,
+                    documentInitiator = event.documentInitiator,
+                    isAuction = prevProcessContext.isAuction,
+                    mainProcurementCategory = null,
+                    awardCriteria = null,
+                    relatedProcess = null,
+                    entityId = null
+                )
+            }
+
+        CamundaContext(propertyContainer)
+            .apply {
+                this.request = CamundaContext.Request(
+                    id = event.documents.first().id,
+                    payload = ""
+                )
+            }
+
+        val variables: Map<String, Any> = propertyContainer.toMap()
+        processService.launchProcess(processDefinitionKey, operationId, variables)
         return MaybeFail.none()
     }
 
