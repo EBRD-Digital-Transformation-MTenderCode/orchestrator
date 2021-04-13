d7ed215f1fadf7a14b7e54124cf6805eeda05194
katherine <kate.shaidurova@gmail.com>
2021-04-13 12:42:36 +0300

    ES-5955 Added checkPresenceCandidateInOneSubmission to client

--- src/main/kotlin/com/procurement/orchestrator/application/client/DossierClient.kt
+++ src/main/kotlin/com/procurement/orchestrator/application/client/DossierClient.kt
@@ -6,6 +6,7 @@ import com.procurement.orchestrator.domain.functional.Result
 import com.procurement.orchestrator.infrastructure.client.reply.Reply
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CheckAccessToSubmissionAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CheckPeriodAction
+import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CheckPresenceCandidateInOneSubmissionAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.CreateSubmissionAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.FinalizeSubmissionsAction
 import com.procurement.orchestrator.infrastructure.client.web.dossier.action.FindSubmissionsAction
@@ -90,4 +91,9 @@ interface DossierClient {
         id: CommandId,
         params: GetSubmissionsForTenderingAction.Params
     ): Result<Reply<GetSubmissionsForTenderingAction.Result>, Fail.Incident>
+
+    suspend fun checkPresenceCandidateInOneSubmission(
+        id: CommandId,
+        params: CheckPresenceCandidateInOneSubmissionAction.Params
+    ): Result<Reply<Unit>, Fail.Incident>
 }
