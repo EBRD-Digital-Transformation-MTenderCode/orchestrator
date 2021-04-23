03647a0203c30648578afda310c4b1feeacff288
katherine <kate.shaidurova@gmail.com>
2021-04-23 19:08:01 +0300

    ES-5986 Added Access, Dossier, Submission to ProcessMasterData.kt

--- src/main/kotlin/com/procurement/orchestrator/domain/model/mdm/ProcessMasterData.kt
+++ src/main/kotlin/com/procurement/orchestrator/domain/model/mdm/ProcessMasterData.kt
@@ -8,10 +8,23 @@ import java.io.Serializable
 
 data class ProcessMasterData(
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:JsonProperty("mdm") @param:JsonProperty("mdm") val mdm: Mdm? = null
+    @field:JsonProperty("mdm") @param:JsonProperty("mdm") val mdm: Mdm? = null,
+
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("access") @param:JsonProperty("access") val access: Access? = null,
+
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("dossier") @param:JsonProperty("dossier") val dossier: Dossier? = null,
+
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("submission") @param:JsonProperty("submission") val submission: Submission? = null
+
 ) : ComplexObject<ProcessMasterData>, Serializable {
 
     override fun updateBy(src: ProcessMasterData) = ProcessMasterData(
-        mdm = mdm updateBy src.mdm
+        mdm = mdm updateBy src.mdm,
+        access = access updateBy src.access,
+        dossier = dossier updateBy src.dossier,
+        submission = submission updateBy src.submission
     )
 }
