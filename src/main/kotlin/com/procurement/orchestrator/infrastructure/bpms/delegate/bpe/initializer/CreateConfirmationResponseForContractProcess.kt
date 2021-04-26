aaf37ebfe546da62f6bbb3bcaa45c6fee46048a9
katherine <kate.shaidurova@gmail.com>
2021-04-26 15:43:27 +0300

    ES-5996 Added CreateConfirmationResponseForContractProcess.kt

---
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/bpms/delegate/bpe/initializer/CreateConfirmationResponseForContractProcess.kt
@@ -0,0 +1,65 @@
+package com.procurement.orchestrator.infrastructure.bpms.delegate.bpe.initializer
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.orchestrator.domain.model.contract.confirmation.response.ConfirmationResponseType
+import com.procurement.orchestrator.domain.model.contract.confirmation.response.ConfirmationResponseValue
+import com.procurement.orchestrator.domain.model.document.DocumentId
+import com.procurement.orchestrator.domain.model.document.DocumentType
+import com.procurement.orchestrator.domain.model.organization.person.BusinessFunctionId
+import com.procurement.orchestrator.domain.model.organization.person.BusinessFunctionType
+import java.time.LocalDateTime
+
+object CreateConfirmationResponseForContractProcess {
+
+    class Request {
+        class Payload(
+            @param:JsonProperty("confirmationResponse") @field:JsonProperty("confirmationResponse") val confirmationResponse: ConfirmationResponse
+        ) {
+            data class ConfirmationResponse(
+                @param:JsonProperty("requestGroup") @field:JsonProperty("requestGroup") val requestGroup: String,
+                @param:JsonProperty("type") @field:JsonProperty("type") val type: ConfirmationResponseType,
+                @param:JsonProperty("value") @field:JsonProperty("value") val value: ConfirmationResponseValue,
+                @param:JsonProperty("relatedPerson") @field:JsonProperty("relatedPerson") val relatedPerson: RelatedPerson
+            ) {
+                data class RelatedPerson(
+                    @param:JsonProperty("title") @field:JsonProperty("title") val title: String,
+                    @param:JsonProperty("name") @field:JsonProperty("name") val name: String,
+                    @param:JsonProperty("identifier") @field:JsonProperty("identifier") val identifier: Identifier,
+                    @param:JsonProperty("businessFunctions") @field:JsonProperty("businessFunctions") val businessFunctions: List<BusinessFunction>
+                ) {
+                    data class Identifier(
+                        @param:JsonProperty("scheme") @field:JsonProperty("scheme") val scheme: String,
+                        @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+
+                        @JsonInclude(JsonInclude.Include.NON_NULL)
+                        @param:JsonProperty("uri") @field:JsonProperty("uri") val uri: String?
+                    )
+
+                    data class BusinessFunction(
+                        @param:JsonProperty("id") @field:JsonProperty("id") val id: BusinessFunctionId,
+                        @param:JsonProperty("type") @field:JsonProperty("type") val type: BusinessFunctionType,
+                        @param:JsonProperty("jobTitle") @field:JsonProperty("jobTitle") val jobTitle: String,
+                        @param:JsonProperty("period") @field:JsonProperty("period") val period: Period,
+
+                        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+                        @param:JsonProperty("documents") @field:JsonProperty("documents") val documents: List<Document>?
+                    ) {
+                        data class Period(
+                            @param:JsonProperty("startDate") @field:JsonProperty("startDate") val startDate: LocalDateTime
+                        )
+
+                        data class Document(
+                            @param:JsonProperty("id") @field:JsonProperty("id") val id: DocumentId,
+                            @param:JsonProperty("documentType") @field:JsonProperty("documentType") val documentType: DocumentType,
+                            @param:JsonProperty("title") @field:JsonProperty("title") val title: String,
+
+                            @JsonInclude(JsonInclude.Include.NON_NULL)
+                            @param:JsonProperty("description") @field:JsonProperty("description") val description: String?
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
