fb51c53032c40cca37a4d04ac00a0b04a3248d13
Fatkullin Nikolay <fatkullinmykola@gmail.com>
2021-06-15 17:32:07 +0300

    ES-6319 added parameter "ocid" to the CheckExistenceFaAction.Params

--- src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/action/CheckExistenceFaAction.kt
+++ src/main/kotlin/com/procurement/orchestrator/infrastructure/client/web/access/action/CheckExistenceFaAction.kt
@@ -3,6 +3,7 @@ package com.procurement.orchestrator.infrastructure.client.web.access.action
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.orchestrator.application.service.ProceduralAction
 import com.procurement.orchestrator.domain.model.Cpid
+import com.procurement.orchestrator.domain.model.Ocid
 import com.procurement.orchestrator.infrastructure.model.Version
 
 abstract class CheckExistenceFaAction : ProceduralAction<CheckExistenceFaAction.Params> {
@@ -10,7 +11,8 @@ abstract class CheckExistenceFaAction : ProceduralAction<CheckExistenceFaAction.
     override val name: String = "checkExistenceFA"
 
     class Params(
-        @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: Cpid
+        @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: Cpid,
+        @field:JsonProperty("ocid") @param:JsonProperty("ocid") val ocid: Ocid
     )
 
 }
