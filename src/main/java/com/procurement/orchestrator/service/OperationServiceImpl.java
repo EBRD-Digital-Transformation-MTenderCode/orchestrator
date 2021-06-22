31e838ba997f440e0d80f771421d483efe9a2dd1
Fatkullin Nikolay <fatkullinmykola@gmail.com>
2021-06-22 16:25:54 +0300

    ES-6337 in "if" statement changed Object.nonNull to comparison

--- src/main/java/com/procurement/orchestrator/service/OperationServiceImpl.java
+++ src/main/java/com/procurement/orchestrator/service/OperationServiceImpl.java
@@ -181,7 +181,7 @@ public class OperationServiceImpl implements OperationService {
     @Override
     public void saveContext(final Context context) {
         final ContextEntity contextEntity = new ContextEntity();
-        if (Objects.nonNull(context.getOcid())) {
+        if (context.getOcid() != null) {
             contextEntity.setCpId(context.getOcid());
         } else {
             contextEntity.setCpId(context.getCpid());
