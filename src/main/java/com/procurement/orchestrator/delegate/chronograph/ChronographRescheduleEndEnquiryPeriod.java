635d65668426b8a47cf22735bde4eb7db2a9c9a8
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-07-19 11:50:23 +0300

    ES-6446 Added attribute 'ocid' to context for the task of the chronograph.

--- src/main/java/com/procurement/orchestrator/delegate/chronograph/ChronographRescheduleEndEnquiryPeriod.java
+++ src/main/java/com/procurement/orchestrator/delegate/chronograph/ChronographRescheduleEndEnquiryPeriod.java
@@ -57,6 +57,7 @@ public class ChronographRescheduleEndEnquiryPeriod implements JavaDelegate {
         contextChronograph.setProcessType("enquiryPeriodEnd");
         contextChronograph.setOperationId(uuid);
         contextChronograph.setRequestId(uuid);
+        contextChronograph.setOcid(context.getOcid());
         contextChronograph.setIsAuction(context.getIsAuction());
 
         final JsonNode jsonData = jsonUtil.toJsonNode(entity.getResponseData());
