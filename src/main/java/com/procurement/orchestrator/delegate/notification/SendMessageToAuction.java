7aab251d09da4a87789734ce399a9d07b4163c03
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-08-09 15:50:11 +0300

    ES-6530 Removed modification auction period.

--- src/main/java/com/procurement/orchestrator/delegate/notification/SendMessageToAuction.java
+++ src/main/java/com/procurement/orchestrator/delegate/notification/SendMessageToAuction.java
@@ -55,8 +55,7 @@ public class SendMessageToAuction implements JavaDelegate {
         final Context context = jsonUtil.toObject(Context.class, entity.getContext());
         final String processId = execution.getProcessInstanceId();
         final JsonNode rqData = processService.getAuctionLaunchData(jsonData, processId);
-        final JsonNode testRqData = getTestAuctionLaunchData(rqData);
-        final CommandMessage commandMessage = notificationService.getCommandMessage(LAUNCH, context, testRqData);
+        final CommandMessage commandMessage = notificationService.getCommandMessage(LAUNCH, context, rqData);
         messageProducer.sendToAuction(commandMessage);
         operationService.saveOperationStep(
                 execution,
@@ -64,19 +63,6 @@ public class SendMessageToAuction implements JavaDelegate {
                 context,
                 jsonUtil.toJsonNode(commandMessage),
                 jsonData);
-    }
-
-    private JsonNode getTestAuctionLaunchData(JsonNode rqData) {
-        final ArrayNode lotsArray = (ArrayNode) rqData.get("tender").get("lots");
-        if (lotsArray.size() > 0) {
-            for (final JsonNode lotNode : lotsArray) {
-                ObjectNode auctionPeriodNode = (ObjectNode) lotNode.get("auctionPeriod");
-                final LocalDateTime startDate = dateUtil.localDateTimeNowUTC().plusMinutes(2);
-                final String startDateString = dateUtil.format(startDate);
-                auctionPeriodNode.put("startDate", startDateString);
-            }
-        }
-        return rqData;
     }
 }
 
